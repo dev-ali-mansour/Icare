e37a8c21098f80cf246a0e398814cc06b2ede8c2
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-01 14:23:09 +0200

    feat: Create AuthRepository Interface.
    Create register() for user registration.
    Create signInWithEmailAndPassword() for signing in using user's email and password.
    Create sendRecoveryEmail() for sending recovery password email message.
    Create getUserInfo() for getting current user details.
    Create signOut() for signing user out.
    Create deleteAccount() for deleting user account from the system.

---
+++ app/src/main/java/eg/edu/cu/csds/icare/domain/repository/AuthRepository.kt
@@ -0,0 +1,21 @@
+package eg.edu.cu.csds.icare.domain.repository
+
+import eg.edu.cu.csds.icare.domain.model.Resource
+import eg.edu.cu.csds.icare.domain.model.User
+import kotlinx.coroutines.flow.Flow
+
+interface AuthRepository {
+    fun register(
+        name: String,
+        email: String,
+        nationalId: String,
+        phone: String,
+        password: String,
+    ): Flow<Resource<Nothing?>>
+
+    fun signInWithEmailAndPassword(email: String, password: String): Flow<Resource<Boolean>>
+    fun sendRecoveryEmail(email: String): Flow<Resource<Nothing?>>
+    fun getUserInfo(forceUpdate: Boolean): Flow<Resource<User>>
+    fun signOut(): Flow<Resource<Nothing?>>
+    fun deleteAccount(): Flow<Resource<Nothing?>>
+}
