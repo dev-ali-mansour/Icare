60e50abacf511496b767f1575a08c7ffe09a4202
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-01 14:12:47 +0200

    feat: Create Resource Util Data Class To Act As Wrapper For Retrieved Data From The Backend.

---
+++ app/src/main/java/eg/edu/cu/csds/icare/domain/model/Resource.kt
@@ -0,0 +1,11 @@
+package eg.edu.cu.csds.icare.domain.model
+
+sealed class Resource<T>(
+    val data: T? = null,
+    val error: Throwable? = null
+) {
+    class Unspecified<T>(data: T? = null) : Resource<T>(data)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(throwable: Throwable?) : Resource<T>(null, throwable)
+}
