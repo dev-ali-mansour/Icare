8badfb18d0cb8ecc2821110b116d78b6e86a0044
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-01 12:47:44 +0200

    chore: Add Koin Dependency As a Dependency Injection Framework.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
+    implementation(platform(libs.koin.bom))
+    implementation(libs.bundles.koin)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
