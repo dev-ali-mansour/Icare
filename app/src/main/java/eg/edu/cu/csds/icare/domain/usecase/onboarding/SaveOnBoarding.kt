b4d61bd7ddf350282d7313d771edc796546ace39
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-01 15:35:47 +0200

    feat: Create SaveOnBoarding UseCase.

---
+++ app/src/main/java/eg/edu/cu/csds/icare/domain/usecase/onboarding/SaveOnBoarding.kt
@@ -0,0 +1,11 @@
+package eg.edu.cu.csds.icare.domain.usecase.onboarding
+
+import eg.edu.cu.csds.icare.domain.repository.AppRepository
+
+class SaveOnBoarding(private val appRepository: AppRepository) {
+
+    suspend operator fun invoke(completed: Boolean) {
+        appRepository.saveOnBoardingState(completed = completed)
+    }
+
+}
