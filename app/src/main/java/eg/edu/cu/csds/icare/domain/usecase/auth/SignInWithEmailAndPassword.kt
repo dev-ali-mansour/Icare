3d7c9d7c077f58d61fa50b143309673a2c570131
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-01 14:25:34 +0200

    feat: Create SignInWithEmailAndPassword UseCase.

---
+++ app/src/main/java/eg/edu/cu/csds/icare/domain/usecase/auth/SignInWithEmailAndPassword.kt
@@ -0,0 +1,10 @@
+package eg.edu.cu.csds.icare.domain.usecase.auth
+
+import eg.edu.cu.csds.icare.domain.model.Resource
+import eg.edu.cu.csds.icare.domain.repository.AuthRepository
+import kotlinx.coroutines.flow.Flow
+
+class SignInWithEmailAndPassword (private val repository: AuthRepository) {
+    operator fun invoke(email: String, password: String): Flow<Resource<Boolean>> =
+        repository.signInWithEmailAndPassword(email, password)
+}
