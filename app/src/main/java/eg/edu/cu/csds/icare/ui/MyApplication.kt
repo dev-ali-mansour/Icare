859609841347c08f4b74da71c65c8c0b7c381d7d
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-01 15:26:40 +0200

    feat: Create MyApplication Class And Set It As The Application Class In AndroidManifest.xml.
    Start Koin in MyApplication.

---
+++ app/src/main/java/eg/edu/cu/csds/icare/ui/MyApplication.kt
@@ -0,0 +1,24 @@
+package eg.edu.cu.csds.icare.ui
+
+import androidx.multidex.MultiDexApplication
+import eg.edu.cu.csds.icare.di.appModule
+import eg.edu.cu.csds.icare.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+
+class MyApplication : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@MyApplication)
+            modules(
+                appModule,
+                viewModelModule
+            )
+        }
+    }
+}
