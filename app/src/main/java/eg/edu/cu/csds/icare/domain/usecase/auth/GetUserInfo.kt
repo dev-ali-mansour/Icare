2fcab0e2d8ab88700850fcdd01cb484f09fd3132
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-01 14:24:24 +0200

    feat: Create GetUserInfo UseCase.

---
+++ app/src/main/java/eg/edu/cu/csds/icare/domain/usecase/auth/GetUserInfo.kt
@@ -0,0 +1,11 @@
+package eg.edu.cu.csds.icare.domain.usecase.auth
+
+import eg.edu.cu.csds.icare.domain.model.Resource
+import eg.edu.cu.csds.icare.domain.model.User
+import eg.edu.cu.csds.icare.domain.repository.AuthRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetUserInfo(private val repository: AuthRepository) {
+    operator fun invoke(forceUpdate: Boolean): Flow<Resource<User>> =
+        repository.getUserInfo(forceUpdate = forceUpdate)
+}
