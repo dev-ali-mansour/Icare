79f47abc4d44d88869729648d62e10c30d678543
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-01 14:11:17 +0200

    feat: Create User Model Data Class.

---
+++ app/src/main/java/eg/edu/cu/csds/icare/domain/model/User.kt
@@ -0,0 +1,16 @@
+package eg.edu.cu.csds.icare.domain.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(
+    val id: Long = 0,
+    val roleId: Short = 0,
+    val phoneNumber: String = "",
+    val dateOfBirth: Long = System.currentTimeMillis(),
+    val gender: Char = 'M',
+    val address: String = "",
+    val nationalId: String = "",
+    val photoPath: String = "",
+    val permissions: List<Short> = listOf(),
+)
