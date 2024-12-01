07d4d16d7d5250ba91dd7fb89ead764f2ac4f64d
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-01 15:35:27 +0200

    feat: Create ReadOnBoarding UseCase.

---
+++ app/src/main/java/eg/edu/cu/csds/icare/domain/usecase/onboarding/ReadOnBoarding.kt
@@ -0,0 +1,10 @@
+package eg.edu.cu.csds.icare.domain.usecase.onboarding
+
+import eg.edu.cu.csds.icare.domain.model.Resource
+import eg.edu.cu.csds.icare.domain.repository.AppRepository
+import kotlinx.coroutines.flow.Flow
+
+class ReadOnBoarding (private val appRepository: AppRepository) {
+
+    operator fun invoke(): Flow<Resource<Boolean>> = appRepository.getOnBoardingState()
+}
