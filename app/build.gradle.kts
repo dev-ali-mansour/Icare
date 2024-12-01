1d0e038fd382791eaaae00ab9eae415535b7dfe9
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-01 12:56:10 +0200

    chore: Add SplashScreen Dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(platform(libs.koin.bom))
     implementation(libs.bundles.koin)
+    implementation(libs.splashScreen)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
