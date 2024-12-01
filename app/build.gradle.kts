e2d4a245a6afde187cd7914484b98109e1e2fa3f
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-01 13:01:58 +0200

    chore: Add Packaging Exclude Flags In app build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,11 @@ android {
     composeCompiler{
         enableStrongSkippingMode = true
     }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
