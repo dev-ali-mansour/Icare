d2c9c082d9e3a468fb7d9cd76e89c3e9b35890f2
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-05 08:16:08 +0200

    refactor: Replace Deprecated Import Of "org.koin.androidx.viewmodel.dsl.viewModel" In ViewModelModule.kt With The New "org.koin.core.module.dsl.viewModel".

--- app/src/main/java/eg/edu/cu/csds/icare/di/ViewModelModule.kt
+++ app/src/main/java/eg/edu/cu/csds/icare/di/ViewModelModule.kt
@@ -1,7 +1,7 @@
 package eg.edu.cu.csds.icare.di
 
 import eg.edu.cu.csds.icare.ui.screen.auth.AuthViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
