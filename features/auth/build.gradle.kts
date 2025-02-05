b27b5eddcfb2df648a87c66d12eb5c9071ed969c
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-05 08:10:44 +0200

    chore: Add Signing Configurations And Build Flavors To features:auth Module.

--- features/auth/build.gradle.kts
+++ features/auth/build.gradle.kts
@@ -1,4 +1,10 @@
+import build.Build
 import build.BuildConfig
+import build.BuildDimensions
+import build.BuildTypes
+import extensions.getLocalProperty
+import flavors.FlavorTypes
+import signing.SigningTypes
 import test.TestBuildConfig
 
 plugins {
@@ -19,18 +25,63 @@ android {
         testInstrumentationRunner = TestBuildConfig.TEST_INSTRUMENTATION_RUNNER
         consumerProguardFiles("consumer-rules.pro")
     }
-
+    signingConfigs {
+        create(SigningTypes.RELEASE) {
+            storeFile = file(project.getLocalProperty("release_key.store"))
+            storePassword = project.getLocalProperty("release_key.store_password")
+            keyAlias = project.getLocalProperty("release_key.alias")
+            keyPassword = project.getLocalProperty("release_key.key_password")
+            enableV1Signing = true
+            enableV2Signing = true
+        }
+        create(SigningTypes.RELEASE_EXTERNAL_QA) {
+            storeFile = file(project.getLocalProperty("qa_key.store"))
+            storePassword = project.getLocalProperty("qa_key.store_password")
+            keyAlias = project.getLocalProperty("qa_key.alias")
+            keyPassword = project.getLocalProperty("qa_key.key_password")
+            enableV1Signing = true
+            enableV2Signing = true
+        }
+        getByName(SigningTypes.DEBUG) {
+            storeFile = File(project.rootProject.rootDir, "debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+            enableV1Signing = true
+            enableV2Signing = true
+        }
+    }
     buildTypes {
-        release {
-            isMinifyEnabled = false
+        getByName(BuildTypes.RELEASE) {
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            isMinifyEnabled = Build.Release.isMinifyEnabled
+            enableUnitTestCoverage = Build.Release.enableUnitTestCoverage
+            signingConfig = signingConfigs.getByName(BuildTypes.RELEASE)
+        }
+
+        getByName(BuildTypes.DEBUG) {
+            isMinifyEnabled = Build.Debug.isMinifyEnabled
+            enableUnitTestCoverage = Build.Debug.enableUnitTestCoverage
+            signingConfig = signingConfigs.getByName(BuildTypes.DEBUG)
+        }
+
+        create(BuildTypes.RELEASE_EXTERNAL_QA) {
+            isMinifyEnabled = Build.ReleaseExternalQA.isMinifyEnabled
+            enableUnitTestCoverage = Build.ReleaseExternalQA.enableUnitTestCoverage
+            signingConfig = signingConfigs.getByName(BuildTypes.RELEASE_EXTERNAL_QA)
         }
     }
-    buildFeatures {
-        compose = true
+    flavorDimensions.add(BuildDimensions.APP)
+    productFlavors {
+        create(FlavorTypes.PATIENT) {
+            dimension = BuildDimensions.APP
+        }
+        create(FlavorTypes.DOCTOR) {
+            dimension = BuildDimensions.APP
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
@@ -38,6 +89,9 @@ android {
     }
     kotlinOptions {
         JavaVersion.VERSION_21
+    }
+    buildFeatures {
+        compose = true
     }
     packaging {
         resources {
