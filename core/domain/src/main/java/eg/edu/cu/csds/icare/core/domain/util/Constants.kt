3c99391bcd8f34853fdf82b0891082abb8b710d7
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-12 13:05:09 +0200

    refactor: Create Constants.kt Util File In Core Domain Module Containing Constants.

---
+++ core/domain/src/main/java/eg/edu/cu/csds/icare/core/domain/util/Constants.kt
@@ -0,0 +1,16 @@
+package eg.edu.cu.csds.icare.core.domain.util
+
+object Constants {
+    const val ON_BOARDING_PAGE_COUNT = 3
+    const val LAST_ON_BOARDING_PAGE = 2
+    const val CHANGE_LANGUAGE_CODE = 1
+    const val SHARE_CODE = 3
+    const val RATE_CODE = 4
+    const val CONTACT_CODE = 5
+    const val ERROR_REPORTING_CODE = 6
+    const val APPS_CODE = 7
+    const val POLICY_CODE = 8
+    const val ABOUT_CODE = 9
+    const val SHORT_DELAY: Long = 30
+    const val IMAGE_QUALITY = 100
+}
