4c36db315e2c7c6584ac4ece3a8fbe75932d1a90
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-12 13:56:14 +0200

    refactor: Add MainViewModel To ViewModelModule.kt.

--- app/src/main/java/eg/edu/cu/csds/icare/di/ViewModelModule.kt
+++ app/src/main/java/eg/edu/cu/csds/icare/di/ViewModelModule.kt
@@ -1,19 +1,14 @@
 package eg.edu.cu.csds.icare.di
 
 import eg.edu.cu.csds.icare.auth.screen.AuthViewModel
+import eg.edu.cu.csds.icare.core.ui.MainViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule =
     module {
         viewModel {
-            AuthViewModel(
-                get(),
-                get(),
-                get(),
-                get(),
-                get(),
-                get(),
-            )
+            MainViewModel(get(), get())
+            AuthViewModel(get(), get(), get(), get(), get(), get())
         }
     }
