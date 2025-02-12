68b283c1cfc9294fad242e06e5463a069a323cde
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-12 12:59:33 +0200

    refactor: Create Exceptions.kt File In Core Domain Containing Project Custom Exceptions.

---
+++ core/domain/src/main/java/eg/edu/cu/csds/icare/core/domain/model/Exceptions.kt
@@ -0,0 +1,9 @@
+package eg.edu.cu.csds.icare.core.domain.model
+
+class UserNotAuthenticatedException : Throwable()
+
+class EmailVerificationException : Throwable()
+
+class AccessTokenExpiredException : Throwable()
+
+class UserNotAuthorizedException : Throwable()
