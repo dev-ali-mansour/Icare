52db038006e06381bdc9ca32557eaa00e5ad70f3
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-15 14:19:22 +0200

    chore: Use Operating System To Set KeyStore Path In Features Auth Module build.gradle.kts.

--- features/auth/build.gradle.kts
+++ features/auth/build.gradle.kts
@@ -4,6 +4,7 @@ import build.BuildConfig
 import build.BuildDimensions
 import build.BuildTypes
 import extensions.getLocalProperty
+import extensions.osFamily
 import flavors.FlavorTypes
 import signing.SigningTypes
 import test.TestBuildConfig
@@ -30,7 +31,7 @@ android {
 
     signingConfigs {
         create(SigningTypes.RELEASE) {
-            storeFile = file(project.getLocalProperty("release_key.store"))
+            storeFile = file(project.getLocalProperty("release_key.store.${project.osFamily}"))
             storePassword = project.getLocalProperty("release_key.store_password")
             keyAlias = project.getLocalProperty("release_key.alias")
             keyPassword = project.getLocalProperty("release_key.key_password")
@@ -38,7 +39,7 @@ android {
             enableV2Signing = true
         }
         create(SigningTypes.RELEASE_EXTERNAL_QA) {
-            storeFile = file(project.getLocalProperty("qa_key.store"))
+            storeFile = file(project.getLocalProperty("qa_key.store.${project.osFamily}"))
             storePassword = project.getLocalProperty("qa_key.store_password")
             keyAlias = project.getLocalProperty("qa_key.alias")
             keyPassword = project.getLocalProperty("qa_key.key_password")
