513cbb4367a9bdd483a59548a82dda8f1b9e5f24
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-15 15:49:57 +0200

    feat: Implement Room Module for Local Database.
    This commit introduces the Room module for managing the local database within the application.
    It includes the necessary configurations for database setup, including a passphrase for secure storage and support for debug builds.
    The module defines dependencies for the database, `DbPassPhrase`, `SupportFactory`, and `SettingsDao`, enabling interaction with the local database.
    The module is also responsible for instantiating the `AppDatabase`.

---
+++ data/src/main/java/eg/edu/cu/csds/icare/data/di/module/RoomModule.kt
@@ -0,0 +1,47 @@
+package eg.edu.cu.csds.icare.data.di.module
+
+import android.content.Context
+import androidx.room.Room
+import eg.edu.cu.csds.icare.data.BuildConfig
+import eg.edu.cu.csds.icare.data.local.db.AppDatabase
+import eg.edu.cu.csds.icare.data.local.db.DbPassPhrase
+import eg.edu.cu.csds.icare.data.local.db.dao.SettingsDao
+import net.sqlcipher.database.SupportFactory
+import org.koin.dsl.module
+
+val roomModule =
+    module {
+        single { provideDbPassPhrase(get()) }
+        single { provideSupportFactory(get()) }
+        single { provideAppDatabase(get(), get()) }
+        single { provideSettingsDao(get()) }
+    }
+
+fun provideDbPassPhrase(context: Context): DbPassPhrase = DbPassPhrase(context)
+
+fun provideSupportFactory(passphrase: DbPassPhrase): SupportFactory = SupportFactory(passphrase.getPassphrase())
+
+fun provideAppDatabase(
+    context: Context,
+    supportFactory: SupportFactory,
+): AppDatabase =
+    when {
+        BuildConfig.DEBUG ->
+            Room
+                .databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "ICare",
+                ).build()
+
+        else ->
+            Room
+                .databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "ICare",
+                ).openHelperFactory(supportFactory)
+                .build()
+    }
+
+fun provideSettingsDao(db: AppDatabase): SettingsDao = db.settingsDao()
