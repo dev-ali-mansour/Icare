9c57f7c3ef055a4077793bbe5cd99767dbba4b39
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-15 15:47:41 +0200

    feat: Create SettingsEntity for Room database.

---
+++ data/src/main/java/eg/edu/cu/csds/icare/data/local/db/entity/SettingsEntity.kt
@@ -0,0 +1,11 @@
+package eg.edu.cu.csds.icare.data.local.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "settings")
+data class SettingsEntity(
+    @PrimaryKey
+    val id: Short,
+    val onBoardingCompleted: Boolean,
+)
