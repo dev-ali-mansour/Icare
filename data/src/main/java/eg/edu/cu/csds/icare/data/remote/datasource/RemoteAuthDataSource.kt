e82b292eed1342bf43caa5f3bcc5ae9d99ed7152
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-15 15:43:02 +0200

    feat: Add RemoteAuthDataSource Interface for Authentication Operations

---
+++ data/src/main/java/eg/edu/cu/csds/icare/data/remote/datasource/RemoteAuthDataSource.kt
@@ -0,0 +1,26 @@
+package eg.edu.cu.csds.icare.data.remote.datasource
+
+import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.User
+import kotlinx.coroutines.flow.Flow
+
+interface RemoteAuthDataSource {
+    fun getUserInfo(): Flow<Resource<User>>
+
+    fun register(
+        name: String,
+        email: String,
+        address: String,
+        phone: String,
+        password: String,
+    ): Flow<Resource<Nothing?>>
+
+    fun signInWithEmailAndPassword(
+        email: String,
+        password: String,
+    ): Flow<Resource<Boolean>>
+
+    fun sendRecoveryEmail(email: String): Flow<Resource<Nothing?>>
+
+    fun deleteAccount(): Flow<Resource<Nothing?>>
+}
