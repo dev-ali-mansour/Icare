5dfba355d9a4702277bbc3e2c41a6632fac581d2
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-16 07:52:26 +0200

    feat: Add Data Modules As Koin Modules
    Adds room, local data source, network, remote data source, repository, and use case modules as Koin modules to the application.

--- app/src/main/java/eg/edu/cu/csds/icare/ui/MyApplication.kt
+++ app/src/main/java/eg/edu/cu/csds/icare/ui/MyApplication.kt
@@ -1,6 +1,12 @@
 package eg.edu.cu.csds.icare.ui
 
 import androidx.multidex.MultiDexApplication
+import eg.edu.cu.csds.icare.data.di.module.localDataSourceModules
+import eg.edu.cu.csds.icare.data.di.module.networkModule
+import eg.edu.cu.csds.icare.data.di.module.remoteDataSourceModule
+import eg.edu.cu.csds.icare.data.di.module.repositoryModule
+import eg.edu.cu.csds.icare.data.di.module.roomModule
+import eg.edu.cu.csds.icare.data.di.module.useCaseModule
 import eg.edu.cu.csds.icare.di.appModule
 import eg.edu.cu.csds.icare.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
@@ -15,6 +21,12 @@ class MyApplication : MultiDexApplication() {
             androidContext(this@MyApplication)
             modules(
                 appModule,
+                roomModule,
+                localDataSourceModules,
+                networkModule,
+                remoteDataSourceModule,
+                repositoryModule,
+                useCaseModule,
                 viewModelModule,
             )
         }
