cb5170200d8fd19e0a437051f3130e6f1fecaec8
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-16 15:57:43 +0200

    feat: Implement splash screen with custom theme and update MainActivity

--- app/src/main/java/eg/edu/cu/csds/icare/MainActivity.kt
+++ app/src/main/java/eg/edu/cu/csds/icare/MainActivity.kt
@@ -12,12 +12,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import eg.edu.cu.csds.icare.core.ui.theme.IcareTheme
 import eg.edu.cu.csds.icare.data.BuildConfig
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             IcareTheme {
