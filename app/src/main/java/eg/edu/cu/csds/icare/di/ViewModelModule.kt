356b2caaf54bf5f1b142a36f4a0c6aa78681e3b2
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-16 15:56:42 +0200

    refactor: Simplify ViewModelModule by reordering viewModel declarations

--- app/src/main/java/eg/edu/cu/csds/icare/di/ViewModelModule.kt
+++ app/src/main/java/eg/edu/cu/csds/icare/di/ViewModelModule.kt
@@ -7,8 +7,6 @@ import org.koin.dsl.module
 
 val viewModelModule =
     module {
-        viewModel {
-            MainViewModel(get(), get())
-            AuthViewModel(get(), get(), get(), get(), get(), get())
-        }
+        viewModel { AuthViewModel(get(), get(), get(), get(), get(), get()) }
+        viewModel { MainViewModel(get(), get()) }
     }
