a7bf6f0f540f53ddab0b2a6119ec45ab7a08cfa1
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-16 07:51:01 +0200

    feat: Implement AppRepository for onboarding state management

---
+++ data/src/main/java/eg/edu/cu/csds/icare/data/repository/AppRepositoryImpl.kt
@@ -0,0 +1,16 @@
+package eg.edu.cu.csds.icare.data.repository
+
+import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.repository.AppRepository
+import eg.edu.cu.csds.icare.data.local.datasource.LocalSettingsDataSource
+import kotlinx.coroutines.flow.Flow
+
+class AppRepositoryImpl(
+    private val settingsDataSource: LocalSettingsDataSource,
+) : AppRepository {
+    override suspend fun saveOnBoardingState(completed: Boolean) {
+        settingsDataSource.saveOnBoardingState(completed = completed)
+    }
+
+    override fun getOnBoardingState(): Flow<Resource<Boolean>> = settingsDataSource.getOnBoardingState()
+}
