16d44e29bcd568830e1f9a4e38ed91d3c8584e16
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-17 10:18:53 +0200

    feat: Add GifImageLoader component for loading GIF images

---
+++ core/ui/src/main/java/eg/edu/cu/csds/icare/core/ui/view/GifImageLoader.kt
@@ -0,0 +1,50 @@
+package eg.edu.cu.csds.icare.core.ui.view
+
+import android.os.Build
+import android.os.Build.VERSION.SDK_INT
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
+import eg.edu.cu.csds.icare.core.ui.R
+
+@Composable
+fun GifImageLoader(
+    modifier: Modifier = Modifier,
+    @DrawableRes
+    drawableRes: Int = R.drawable.success,
+) {
+    val context = LocalContext.current
+    val imageLoader =
+        ImageLoader
+            .Builder(context)
+            .components {
+                if (SDK_INT >= Build.VERSION_CODES.P) {
+                    add(ImageDecoderDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
+            }.build()
+    Image(
+        painter =
+            rememberAsyncImagePainter(
+                ImageRequest
+                    .Builder(context)
+                    .data(data = drawableRes)
+                    .apply(block = {
+                        size(coil.size.Size.ORIGINAL)
+                    })
+                    .build(),
+                imageLoader = imageLoader,
+            ),
+        contentDescription = null,
+        modifier = modifier.fillMaxWidth(),
+    )
+}
