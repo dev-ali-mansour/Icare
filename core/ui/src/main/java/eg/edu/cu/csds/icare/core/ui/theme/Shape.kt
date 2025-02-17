77b4972b8a5837e7b6c26115927b95e6698096e0
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-17 09:45:52 +0200

    feat: Add custom shape definitions for Material3 theme

---
+++ core/ui/src/main/java/eg/edu/cu/csds/icare/core/ui/theme/Shape.kt
@@ -0,0 +1,13 @@
+package eg.edu.cu.csds.icare.core.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+
+val Shapes =
+    Shapes(
+        extraSmall = RoundedCornerShape(M_PADDING),
+        small = RoundedCornerShape(M_PADDING),
+        medium = RoundedCornerShape(M_PADDING),
+        large = RoundedCornerShape(M_PADDING),
+        extraLarge = RoundedCornerShape(M_PADDING),
+    )
