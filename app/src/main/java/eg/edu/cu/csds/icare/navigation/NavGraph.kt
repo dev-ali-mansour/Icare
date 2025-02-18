53bb52b52f5085a270289f04a7b77dca25ef19e9
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-18 12:32:06 +0200

    feat: Implement navigation graph with splash, welcome, and authentication screens

---
+++ app/src/main/java/eg/edu/cu/csds/icare/navigation/NavGraph.kt
@@ -0,0 +1,182 @@
+package eg.edu.cu.csds.icare.navigation
+
+import android.content.Context
+import android.content.Intent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.google.firebase.auth.FirebaseAuth
+import eg.edu.cu.csds.icare.MainActivity
+import eg.edu.cu.csds.icare.SplashScreen
+import eg.edu.cu.csds.icare.auth.navigation.authenticationRoute
+import eg.edu.cu.csds.icare.auth.screen.AuthViewModel
+import eg.edu.cu.csds.icare.core.domain.model.UserNotAuthenticatedException
+import eg.edu.cu.csds.icare.core.ui.MainViewModel
+import eg.edu.cu.csds.icare.core.ui.navigation.Screen
+import eg.edu.cu.csds.icare.core.ui.util.getErrorMessage
+import eg.edu.cu.csds.icare.core.ui.view.DialogWithIcon
+import kotlinx.coroutines.delay
+import kotlin.system.exitProcess
+
+@Suppress("LongMethod", "UnusedParameter")
+@Composable
+fun SetupNavGraph(
+    firebaseAuth: FirebaseAuth,
+    navController: NavHostController,
+    mainViewModel: MainViewModel,
+    authViewModel: AuthViewModel,
+    showAppSettings: () -> Unit,
+    context: Context = LocalContext.current,
+) {
+    val onBoardingRes by mainViewModel.onBoardingCompleted.collectAsStateWithLifecycle()
+    val alertMessage = remember { mutableStateOf("") }
+    val showAlert = remember { mutableStateOf(false) }
+    val exitApp = remember { mutableStateOf(false) }
+
+    if (showAlert.value) {
+        DialogWithIcon(text = alertMessage.value) {
+            showAlert.value = false
+            if (exitApp.value) exitProcess(0)
+        }
+    }
+
+    onBoardingRes.data?.let { onBoardingCompleted ->
+        val startScreen: Screen =
+            when {
+                onBoardingCompleted -> Screen.Splash
+                else -> Screen.Welcome
+            }
+        NavHost(
+            navController = navController,
+            startDestination = startScreen,
+        ) {
+            composable<Screen.Splash> {
+                SplashScreen(
+                    firebaseAuth = firebaseAuth,
+                    mainViewModel = mainViewModel,
+                    navigateToHome = {
+                        navController.navigate(Screen.Home) {
+                            popUpTo(navController.graph.id) { inclusive = true }
+                        }
+                    },
+                    navigateToLogin = {
+                        navController.navigate(Screen.Login) {
+                            popUpTo(navController.graph.id) { inclusive = true }
+                        }
+                    },
+                    onError = { error ->
+                        exitApp.value = true
+                        handleError(
+                            error,
+                            exitApp,
+                            context,
+                            authViewModel,
+                            navController,
+                            alertMessage,
+                            showAlert,
+                        )
+                    },
+                )
+            }
+
+            welcomeRoute(onCompleted = {
+                navController.navigate(Screen.Login) {
+                    popUpTo(navController.graph.id) { inclusive = true }
+                }
+            })
+
+            authenticationRoute(
+                context = context,
+                intent = Intent(context, MainActivity::class.java),
+                firebaseAuth = firebaseAuth,
+                mainViewModel = mainViewModel,
+                authViewModel = authViewModel,
+                onRecoveryClicked = { navController.navigate(Screen.PasswordRecovery) },
+                onCreateAccountClicked = { navController.navigate(Screen.Register) },
+                onLoginClicked = {
+                    navController.navigate(Screen.Login) {
+                        popUpTo(navController.graph.id) {
+                            inclusive = true
+                        }
+                    }
+                },
+                onRecoveryCompleted = {
+                    navController.navigate(Screen.Login) {
+                        popUpTo(navController.graph.id) {
+                            inclusive = true
+                        }
+                    }
+                },
+                onRegisterCompleted = {
+                    navController.navigate(Screen.Home) {
+                        popUpTo(navController.graph.id) {
+                            inclusive = true
+                        }
+                    }
+                },
+                onError = { error ->
+                    exitApp.value = false
+                    handleError(
+                        error,
+                        exitApp,
+                        context,
+                        authViewModel,
+                        navController,
+                        alertMessage,
+                        showAlert,
+                    )
+                },
+            )
+        }
+    }
+}
+
+fun NavGraphBuilder.welcomeRoute(onCompleted: () -> Unit) {
+    composable<Screen.Welcome> {
+        onCompleted()
+    }
+}
+
+@Suppress("UnusedPrivateMember")
+private fun NavHostController.navigateUpSafely() {
+    if (previousBackStackEntry == null) {
+        navigate(Screen.Home)
+    } else {
+        navigateUp()
+    }
+}
+
+private suspend fun handleError(
+    error: Throwable?,
+    exitApp: MutableState<Boolean>,
+    context: Context,
+    authViewModel: AuthViewModel,
+    navController: NavHostController,
+    alertMessage: MutableState<String>,
+    showAlert: MutableState<Boolean>,
+) {
+    when (error) {
+        is UserNotAuthenticatedException -> {
+            authViewModel.onLogOutClick()
+            navController.navigate(Screen.Login) {
+                popUpTo(navController.graph.id) { inclusive = true }
+            }
+        }
+
+        else -> {
+            alertMessage.value = context.getErrorMessage(error)
+            showAlert.value = true
+            delay(timeMillis = 3000)
+            showAlert.value = false
+            if (exitApp.value) exitProcess(0)
+        }
+    }
+}
