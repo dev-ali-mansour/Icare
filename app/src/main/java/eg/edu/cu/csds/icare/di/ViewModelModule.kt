6fce1cbeb33a2e28fe1a02d1cd4facbbed728601
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-18 09:56:05 +0200

    refactor: Update MainViewModel to include additional dependency

--- app/src/main/java/eg/edu/cu/csds/icare/di/ViewModelModule.kt
+++ app/src/main/java/eg/edu/cu/csds/icare/di/ViewModelModule.kt
@@ -8,5 +8,5 @@ import org.koin.dsl.module
 val viewModelModule =
     module {
         viewModel { AuthViewModel(get(), get(), get(), get(), get(), get()) }
-        viewModel { MainViewModel(get(), get()) }
+        viewModel { MainViewModel(get(), get(), get()) }
     }
