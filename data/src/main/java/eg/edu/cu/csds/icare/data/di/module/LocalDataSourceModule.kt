d88af1108f4e53b5cbb91951b01bcfe998100a65
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-18 11:05:13 +0200

    feat: Add LocalAuthDataSource and its implementation to LocalDataSourceModule

--- data/src/main/java/eg/edu/cu/csds/icare/data/di/module/LocalDataSourceModule.kt
+++ data/src/main/java/eg/edu/cu/csds/icare/data/di/module/LocalDataSourceModule.kt
@@ -1,5 +1,7 @@
 package eg.edu.cu.csds.icare.data.di.module
 
+import eg.edu.cu.csds.icare.data.local.datasource.LocalAuthDataSource
+import eg.edu.cu.csds.icare.data.local.datasource.LocalAuthDataSourceImpl
 import eg.edu.cu.csds.icare.data.local.datasource.LocalSettingsDataSource
 import eg.edu.cu.csds.icare.data.local.datasource.LocalSettingsDataSourceImpl
 import org.koin.core.module.dsl.bind
@@ -9,4 +11,5 @@ import org.koin.dsl.module
 val localDataSourceModules =
     module {
         singleOf(::LocalSettingsDataSourceImpl) { bind<LocalSettingsDataSource>() }
+        singleOf(::LocalAuthDataSourceImpl) { bind<LocalAuthDataSource>() }
     }
