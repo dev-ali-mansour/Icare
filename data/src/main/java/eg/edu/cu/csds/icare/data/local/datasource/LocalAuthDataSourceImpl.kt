2172af1c57faaf6a72f2b1693eb218961daedbc6
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-18 09:55:16 +0200

    feat: Implement LocalAuthDataSource for user and permission management

---
+++ data/src/main/java/eg/edu/cu/csds/icare/data/local/datasource/LocalAuthDataSourceImpl.kt
@@ -0,0 +1,31 @@
+package eg.edu.cu.csds.icare.data.local.datasource
+
+import eg.edu.cu.csds.icare.data.local.db.dao.UserDao
+import eg.edu.cu.csds.icare.data.local.db.entity.PermissionEntity
+import eg.edu.cu.csds.icare.data.local.db.entity.UserEntity
+
+class LocalAuthDataSourceImpl(
+    private val userDao: UserDao,
+) : LocalAuthDataSource {
+    override suspend fun getUser(): UserEntity? = userDao.getEmployee()
+
+    override suspend fun getPermissions(): List<PermissionEntity> = userDao.getPermissions()
+
+    override suspend fun saveEmployee(
+        entity: UserEntity,
+        permissions: List<PermissionEntity>,
+    ) {
+        userDao.getEmployee()?.let {
+            userDao.updateEmployee(entity)
+        } ?: run {
+            userDao.persistUser(entity)
+        }
+        userDao.clearPermissions()
+        userDao.persistPermissions(permissions)
+    }
+
+    override suspend fun clearEmployee() {
+        userDao.clearEmployee()
+        userDao.clearPermissions()
+    }
+}
