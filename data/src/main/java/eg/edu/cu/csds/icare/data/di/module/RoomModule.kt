36b74a77009fb5fde548f64614ef1b37b3977b54
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-18 11:04:53 +0200

    feat: Add UserDao to RoomModule for user data access

--- data/src/main/java/eg/edu/cu/csds/icare/data/di/module/RoomModule.kt
+++ data/src/main/java/eg/edu/cu/csds/icare/data/di/module/RoomModule.kt
@@ -6,6 +6,7 @@ import eg.edu.cu.csds.icare.data.BuildConfig
 import eg.edu.cu.csds.icare.data.local.db.AppDatabase
 import eg.edu.cu.csds.icare.data.local.db.DbPassPhrase
 import eg.edu.cu.csds.icare.data.local.db.dao.SettingsDao
+import eg.edu.cu.csds.icare.data.local.db.dao.UserDao
 import net.sqlcipher.database.SupportFactory
 import org.koin.dsl.module
 
@@ -15,6 +16,7 @@ val roomModule =
         single { provideSupportFactory(get()) }
         single { provideAppDatabase(get(), get()) }
         single { provideSettingsDao(get()) }
+        single { provideUserDao(get()) }
     }
 
 fun provideDbPassPhrase(context: Context): DbPassPhrase = DbPassPhrase(context)
@@ -45,3 +47,5 @@ fun provideAppDatabase(
     }
 
 fun provideSettingsDao(db: AppDatabase): SettingsDao = db.settingsDao()
+
+fun provideUserDao(db: AppDatabase): UserDao = db.userDao()
