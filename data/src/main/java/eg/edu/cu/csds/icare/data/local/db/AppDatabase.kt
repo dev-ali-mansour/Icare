4b4b0cddd70f1ce92c0995233bf8d487790b85a5
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-18 09:53:15 +0200

    feat: Update AppDatabase to include UserEntity and PermissionEntity

--- data/src/main/java/eg/edu/cu/csds/icare/data/local/db/AppDatabase.kt
+++ data/src/main/java/eg/edu/cu/csds/icare/data/local/db/AppDatabase.kt
@@ -3,14 +3,19 @@ package eg.edu.cu.csds.icare.data.local.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import eg.edu.cu.csds.icare.data.local.db.dao.SettingsDao
+import eg.edu.cu.csds.icare.data.local.db.dao.UserDao
+import eg.edu.cu.csds.icare.data.local.db.entity.PermissionEntity
 import eg.edu.cu.csds.icare.data.local.db.entity.SettingsEntity
+import eg.edu.cu.csds.icare.data.local.db.entity.UserEntity
 
 @Database(
-    entities = [SettingsEntity::class],
-    version = 1,
+    entities = [SettingsEntity::class, UserEntity::class, PermissionEntity::class],
+    version = 2,
     exportSchema = true,
     autoMigrations = [],
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun settingsDao(): SettingsDao
+
+    abstract fun userDao(): UserDao
 }
