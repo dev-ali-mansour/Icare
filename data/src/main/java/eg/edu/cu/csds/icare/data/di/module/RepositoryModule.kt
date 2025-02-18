aa6b91f5b34b4aa4c490a4fd0b13b82ed9f0b2a5
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-18 11:05:33 +0200

    feat: Add AuthRepository and its implementation to RepositoryModule

--- data/src/main/java/eg/edu/cu/csds/icare/data/di/module/RepositoryModule.kt
+++ data/src/main/java/eg/edu/cu/csds/icare/data/di/module/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package eg.edu.cu.csds.icare.data.di.module
 
 import eg.edu.cu.csds.icare.core.domain.repository.AppRepository
+import eg.edu.cu.csds.icare.core.domain.repository.AuthRepository
 import eg.edu.cu.csds.icare.data.repository.AppRepositoryImpl
+import eg.edu.cu.csds.icare.data.repository.AuthRepositoryImpl
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -9,4 +11,5 @@ import org.koin.dsl.module
 val repositoryModule =
     module {
         singleOf(::AppRepositoryImpl) { bind<AppRepository>() }
+        singleOf(::AuthRepositoryImpl) { bind<AuthRepository>() }
     }
