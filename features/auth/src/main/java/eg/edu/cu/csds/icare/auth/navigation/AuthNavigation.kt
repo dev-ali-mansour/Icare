fff176efb4c36b43c1f13e2e39b5ac8f2d111611
Ali Mansour <dev.ali.mansour@gmail.com>
2025-02-18 12:36:10 +0200

    feat: Add MainScreen component with navigation and bottom bar support

---
+++ features/auth/src/main/java/eg/edu/cu/csds/icare/auth/navigation/AuthNavigation.kt
@@ -0,0 +1,32 @@
+package eg.edu.cu.csds.icare.auth.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.google.firebase.auth.FirebaseAuth
+import eg.edu.cu.csds.icare.auth.screen.AuthViewModel
+import eg.edu.cu.csds.icare.core.ui.MainViewModel
+import eg.edu.cu.csds.icare.core.ui.navigation.Screen
+
+@Suppress("UnusedParameter")
+fun NavGraphBuilder.authenticationRoute(
+    firebaseAuth: FirebaseAuth,
+    mainViewModel: MainViewModel,
+    authViewModel: AuthViewModel,
+    onRecoveryClicked: () -> Unit,
+    onCreateAccountClicked: () -> Unit,
+    onLoginClicked: () -> Unit,
+    onLoginSuccess: () -> Unit,
+    onRecoveryCompleted: () -> Unit,
+    onRegisterCompleted: () -> Unit,
+    onError: suspend (Throwable?) -> Unit,
+) {
+    composable<Screen.Login> {
+    }
+    composable<Screen.PasswordRecovery> {
+    }
+    composable<Screen.Register> {
+    }
+
+    composable<Screen.Profile> {
+    }
+}
