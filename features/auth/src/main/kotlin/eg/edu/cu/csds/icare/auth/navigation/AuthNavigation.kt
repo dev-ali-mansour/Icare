1e95062acd2b568170e2d9a8fd06a9ec25ef98c8
Ali Mansour <dev.ali.mansour@gmail.com>
2025-03-10 14:32:05 +0200

    refactor: Reorder parameters in LoginScreen composable within AuthNavigation.kt
    The order of `onCreateAnAccountClicked` and `onLoginSuccess` parameters has been swapped in the `LoginScreen` composable call. This improves the readability.

--- features/auth/src/main/kotlin/eg/edu/cu/csds/icare/auth/navigation/AuthNavigation.kt
+++ features/auth/src/main/kotlin/eg/edu/cu/csds/icare/auth/navigation/AuthNavigation.kt
@@ -24,8 +24,8 @@ fun NavGraphBuilder.authenticationRoute(
     composable<Screen.Login> {
         LoginScreen(
             onRecoveryClicked = { onRecoveryClicked() },
-            onCreateAnAccountClicked = { onCreateAccountClicked() },
             onLoginSuccess = { onLoginSuccess() },
+            onCreateAnAccountClicked = { onCreateAccountClicked() },
             authViewModel = authViewModel,
         )
     }
