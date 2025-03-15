ee1799c56f2588be7941d312015277c9b8f3365b
Ali Mansour <dev.ali.mansour@gmail.com>
2025-03-15 17:15:00 +0200

    feat: Add navigation route for the Home screen
    This commit adds a navigation route for the Home screen using Jetpack Compose Navigation.
    - Created a `homeRoute` function that configures the `HomeScreen` composable.
    - `homeRoute` takes parameters for `FirebaseAuth`, `MediaHelper`, `MainViewModel`, `HomeViewModel`, navigation actions, and error handling.
    - `HomeScreen` is associated with the `Screen.Home` route.

---
+++ features/home/src/main/kotlin/eg/edu/cu/csds/icare/home/navigation/HomeNavigation.kt
@@ -0,0 +1,30 @@
+package eg.edu.cu.csds.icare.home.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.google.firebase.auth.FirebaseAuth
+import eg.edu.cu.csds.icare.core.ui.MainViewModel
+import eg.edu.cu.csds.icare.core.ui.navigation.Screen
+import eg.edu.cu.csds.icare.core.ui.util.MediaHelper
+import eg.edu.cu.csds.icare.home.HomeViewModel
+import eg.edu.cu.csds.icare.home.screen.HomeScreen
+
+fun NavGraphBuilder.homeRoute(
+    firebaseAuth: FirebaseAuth,
+    mediaHelper: MediaHelper,
+    mainViewModel: MainViewModel,
+    homeViewModel: HomeViewModel,
+    navigateToScreen: (Screen) -> Unit,
+    onError: suspend (Throwable?) -> Unit,
+) {
+    composable<Screen.Home> {
+        HomeScreen(
+            firebaseAuth = firebaseAuth,
+            mediaHelper = mediaHelper,
+            mainViewModel = mainViewModel,
+            homeViewModel = homeViewModel,
+            navigateToScreen = { navigateToScreen(it) },
+            onError = { onError(it) },
+        )
+    }
+}
