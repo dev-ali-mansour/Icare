1273ab37b2d870a79780cca2dad96aae6d07f17b
Ali Mansour <dev.ali.mansour@gmail.com>
2025-03-16 14:10:02 +0200

    feat: Provide AppUpdateManager using Koin
    The change adds a Koin module to provide an instance of `AppUpdateManager` using `AppUpdateManagerFactory.create(context)`.

--- app/src/main/kotlin/eg/edu/cu/csds/icare/di/AppModule.kt
+++ app/src/main/kotlin/eg/edu/cu/csds/icare/di/AppModule.kt
@@ -1,9 +1,15 @@
 package eg.edu.cu.csds.icare.di
 
+import android.content.Context
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import eg.edu.cu.csds.icare.core.ui.di.CoreUiModule
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
 
 @Module(includes = [CoreUiModule::class])
 @ComponentScan("eg.edu.cu.csds.icare")
-class AppModule
+class AppModule {
+    @Single
+    fun provideAppUpdateManager(context: Context) = AppUpdateManagerFactory.create(context)
+}
