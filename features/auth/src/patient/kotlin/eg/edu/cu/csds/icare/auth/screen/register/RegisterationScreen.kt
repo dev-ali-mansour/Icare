27fb9b3904ccf2bde616d73b4ad201d280844547
Thorayaa <you@example.comthoraya2005@gmail.com>
2025-03-20 15:15:40 +0200

    feat: Add Register Screen for Patient
    This commit introduces the RegisterScreen for patients.
    The following are the key changes:
    - Created RegisterScreen composable function.
    - Implemented UI elements for patient registration including fields for name, email, gender, national ID, phone, medical history, and password.
    - Implemented validation for the registration form.
    - Integrated with AuthViewModel to manage registration state and data.
    - Displayed alert dialogs for success and error messages.
    - Added navigation to the login screen.

---
+++ features/auth/src/patient/kotlin/eg/edu/cu/csds/icare/auth/screen/register/RegisterationScreen.kt
@@ -0,0 +1,204 @@
+package eg.edu.cu.csds.icare.auth.screen.register
+
+import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.firebase.auth.FirebaseAuth
+import eg.edu.cu.csds.icare.auth.R
+import eg.edu.cu.csds.icare.auth.screen.AuthViewModel
+import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.util.Constants
+import eg.edu.cu.csds.icare.core.domain.util.isValidEmail
+import eg.edu.cu.csds.icare.core.domain.util.isValidPassword
+import eg.edu.cu.csds.icare.core.ui.theme.backgroundColor
+import eg.edu.cu.csds.icare.core.ui.util.getErrorMessage
+import eg.edu.cu.csds.icare.core.ui.view.DialogWithIcon
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import eg.edu.cu.csds.icare.core.ui.R as CoreR
+
+@Composable
+internal fun RegisterScreen(
+    firebaseAuth: FirebaseAuth,
+    authViewModel: AuthViewModel,
+    onLoginClicked: () -> Unit,
+    onRegisterCompleted: () -> Unit,
+    context: Context = LocalContext.current,
+) {
+    val firstName by authViewModel.firstNameState
+    val lastName by authViewModel.lastNameState
+    val email by authViewModel.emailState
+    val phone1 by authViewModel.phone1State
+    val nationalId by authViewModel.nationalIdState
+    val password by authViewModel.passwordState
+    val chronicDiseases by authViewModel.chronicDiseasesState
+    val currentMedications by authViewModel.currentMedicationsState
+    val allergies by authViewModel.allergiesState
+    val pastSurgeries by authViewModel.pastSurgeriesState
+    val weight by authViewModel.weightState
+    var passwordVisibility by authViewModel.passwordVisibility
+    val isLoading by authViewModel.isLoading
+    val resource by authViewModel.registerResFlow.collectAsStateWithLifecycle()
+    val selectedGender by authViewModel.selectedGenderState
+    var gendersExpanded by authViewModel.gendersExpanded
+    val scope: CoroutineScope = rememberCoroutineScope()
+    var alertMessage by remember { mutableStateOf("") }
+    var showAlert by remember { mutableStateOf(false) }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) { paddingValues ->
+
+        LaunchedEffect(key1 = resource) {
+            if (resource is Resource.Success) {
+                alertMessage = context.getString(R.string.register_message)
+                showAlert = true
+                delay(timeMillis = 3000)
+                firebaseAuth.signOut()
+                showAlert = false
+                onRegisterCompleted()
+            } else if (resource is Resource.Error) {
+                scope.launch {
+                    alertMessage = context.getErrorMessage(resource.error)
+                    showAlert = true
+                    delay(timeMillis = 3000)
+                    showAlert = false
+                }
+            }
+        }
+
+        Box(
+            modifier =
+                Modifier
+                    .background(color = backgroundColor)
+                    .fillMaxSize()
+                    .padding(paddingValues),
+            contentAlignment = Alignment.BottomCenter,
+        ) {
+            RegisterContent(
+                firstName = firstName,
+                lastName = lastName,
+                email = email,
+                selectedGender = selectedGender,
+                genderExpanded = gendersExpanded,
+                nationalId = nationalId,
+                phone = phone1,
+                password = password,
+                chronicDiseases = chronicDiseases,
+                currentMedications = currentMedications,
+                allergies = allergies,
+                pastSurgeries = pastSurgeries,
+                weight = weight,
+                passwordVisibility = passwordVisibility,
+                isLoading = isLoading,
+                onFirstNameChange = { authViewModel.onFirstNameChanged(it) },
+                onLastNameChanged = { authViewModel.onLastNameChanged(it) },
+                onEmailChange = { authViewModel.onEmailChanged(it) },
+                onBirthDateChanged = { authViewModel.onBirthDateChanged(it) },
+                onGendersExpandedChange = { gendersExpanded = !gendersExpanded },
+                onGendersDismissRequest = { gendersExpanded = false },
+                onGenderClicked = {
+                    authViewModel.onGenderSelected(it)
+                    gendersExpanded = false
+                },
+                onPhoneChange = { authViewModel.onPhone1Changed(it) },
+                onNationalIdChange = { authViewModel.onNationalIdChanged(it) },
+                onPasswordChange = { authViewModel.onPasswordChanged(it) },
+                onPasswordVisibilityChange = { passwordVisibility = !passwordVisibility },
+                onRegisterButtonClicked = {
+                    scope.launch {
+                        when {
+                            firstName.isBlank() -> {
+                                alertMessage = context.getString(R.string.name_error)
+                                showAlert = true
+                                delay(timeMillis = 3000)
+                                showAlert = false
+                            }
+
+                            !email.isValidEmail -> {
+                                alertMessage = context.getString(R.string.email_error)
+                                showAlert = true
+                                delay(timeMillis = 3000)
+                                showAlert = false
+                            }
+
+                            nationalId.isBlank() || nationalId.length < Constants.NATIONAL_ID_LENGTH -> {
+                                alertMessage = context.getString(CoreR.string.national_id_error)
+                                showAlert = true
+                                delay(timeMillis = 3000)
+                                showAlert = false
+                            }
+
+                            phone1.isBlank() || phone1.length < Constants.PHONE_LENGTH -> {
+                                alertMessage = context.getString(R.string.phone_error)
+                                showAlert = true
+                                delay(timeMillis = 3000)
+                                showAlert = false
+                            }
+
+                            chronicDiseases.isBlank() -> {
+                                alertMessage = context.getString(R.string.chronic_diseases_error)
+                                showAlert = true
+                                delay(timeMillis = 3000)
+                                showAlert = false
+                            }
+
+                            currentMedications.isBlank() -> {
+                                alertMessage = context.getString(R.string.current_medications_error)
+                                showAlert = true
+                                delay(timeMillis = 3000)
+                                showAlert = false
+                            }
+
+                            allergies.isBlank() -> {
+                                alertMessage = context.getString(R.string.allergies_error)
+                                showAlert = true
+                                delay(timeMillis = 3000)
+                                showAlert = false
+                            }
+
+                            pastSurgeries.isBlank() -> {
+                                alertMessage = context.getString(R.string.past_surgeries_error)
+                                showAlert = true
+                                delay(timeMillis = 3000)
+                                showAlert = false
+                            }
+
+                            !password.isValidPassword -> {
+                                alertMessage = context.getString(R.string.password_error)
+                                showAlert = true
+                                delay(timeMillis = 3000)
+                                showAlert = false
+                            }
+
+                            else -> authViewModel.onRegisterClicked()
+                        }
+                    }
+                },
+                onLoginClicked = { onLoginClicked() },
+                onChronicDiseasesChange = { authViewModel.onChronicDiseasesChanged(it) },
+                onCurrentMedicationsChange = { authViewModel.onCurrentMedicationsChanged(it) },
+                onAllergiesChange = { authViewModel.onAllergiesChanged(it) },
+                onPastSurgeriesChange = { authViewModel.onPastSurgeriesChanged(it) },
+                onWeightChange = { authViewModel.onWeightChanged(it) },
+            )
+
+            if (showAlert) DialogWithIcon(text = alertMessage) { showAlert = false }
+        }
+    }
+}
