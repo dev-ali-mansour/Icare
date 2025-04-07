816e03032611a73e6853c65541a71622459ad756
Thorayaa <you@example.comthoraya2005@gmail.com>
2025-04-07 11:38:26 +0200

    feat: Implement use case to retrieve patient appointments
    This change introduces a new use case, `GetPatientAppointments`, which retrieves a list of appointments for a patient from the booking repository.

---
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/booking/GetPatientAppointments.kt
@@ -0,0 +1,13 @@
+package eg.edu.cu.csds.icare.core.domain.usecase.booking
+
+import eg.edu.cu.csds.icare.core.domain.model.Appointment
+import eg.edu.cu.csds.icare.core.domain.repository.BookingRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetPatientAppointments(private val repository: BookingRepository)  {
+    operator fun invoke(): Flow<List<Appointment>> {
+        return repository.getPatientAppointments()
+    }
+
+
+}
