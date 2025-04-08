de9ee5372953523889e331b80f5f26cf5f676204
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-08 14:28:03 +0200

    feat: Implement Appointments Repository with Remote Data Source
    This commit introduces the implementation of the `AppointmentsRepository` interface in `AppointmentsRepositoryImpl`. It utilizes a `RemoteAppointmentsDataSource` to fetch and manage appointment data. The methods for retrieving, booking, updating, and rescheduling appointments are now implemented, delegating these operations to the remote data source. Each function now returns a `Flow` of `Resource` wrapping a list of `Appointment` or `Nothing`.

--- data/src/main/kotlin/eg/edu/cu/csds/icare/data/repository/AppointmentsRepositoryImpl.kt
+++ data/src/main/kotlin/eg/edu/cu/csds/icare/data/repository/AppointmentsRepositoryImpl.kt
@@ -3,41 +3,33 @@ package eg.edu.cu.csds.icare.data.repository
 import eg.edu.cu.csds.icare.core.domain.model.Appointment
 import eg.edu.cu.csds.icare.core.domain.model.Resource
 import eg.edu.cu.csds.icare.core.domain.repository.AppointmentsRepository
+import eg.edu.cu.csds.icare.data.remote.datasource.RemoteAppointmentsDataSource
 import kotlinx.coroutines.flow.Flow
 import org.koin.core.annotation.Single
 
 @Single
-class AppointmentsRepositoryImpl : AppointmentsRepository {
-    override fun getPatientAppointments(): Flow<List<Appointment>> {
-        TODO("Not yet implemented")
-    }
+class AppointmentsRepositoryImpl(
+    private val remoteAppointmentsDataSource: RemoteAppointmentsDataSource,
+) : AppointmentsRepository {
+    override fun getPatientAppointments(): Flow<Resource<List<Appointment>>> = remoteAppointmentsDataSource.getPatientAppointments()
 
-    override fun getAppointments(): Flow<List<Appointment>> {
-        TODO("Not yet implemented")
-    }
+    override fun getAppointments(): Flow<Resource<List<Appointment>>> = remoteAppointmentsDataSource.getAppointments()
 
-    override fun getAppointments(statusId: Short): Flow<List<Appointment>> {
-        TODO("Not yet implemented")
-    }
+    override fun getAppointments(statusId: Short): Flow<Resource<List<Appointment>>> =
+        remoteAppointmentsDataSource.getAppointments(statusId)
 
     override fun bookAppointment(
         doctorId: Int,
         dateTime: Long,
-    ): Flow<Resource<Nothing?>> {
-        TODO("Not yet implemented")
-    }
+    ): Flow<Resource<Nothing?>> = remoteAppointmentsDataSource.bookAppointment(doctorId, dateTime)
 
     override fun updateAppointmentStatus(
         appointmentId: Long,
         statusId: Short,
-    ): Flow<Resource<Nothing?>> {
-        TODO("Not yet implemented")
-    }
+    ): Flow<Resource<Nothing?>> = remoteAppointmentsDataSource.updateAppointmentStatus(appointmentId, statusId)
 
     override fun rescheduleAppointment(
         appointmentId: Long,
         dateTime: Long,
-    ): Flow<Resource<Nothing?>> {
-        TODO("Not yet implemented")
-    }
+    ): Flow<Resource<Nothing?>> = rescheduleAppointment(appointmentId, dateTime)
 }
