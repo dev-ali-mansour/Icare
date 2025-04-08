9ec5e8599e7835c97987e66bca2e25f5fe4ed005
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-08 12:56:41 +0200

    feat: Create RemoteAppointmentsDataSource
    This commit introduces the `RemoteAppointmentsDataSource` interface, defining methods for interacting with remote appointment data. The interface includes functions for retrieving patient appointments, getting all appointments, getting appointments by status, booking a new appointment, updating an appointment's status, and rescheduling an existing appointment. Each function returns a `Flow`, representing a stream of data or a `Resource` for operations with potential success or failure.

---
+++ data/src/main/kotlin/eg/edu/cu/csds/icare/data/remote/datasource/RemoteAppointmentsDataSource.kt
@@ -0,0 +1,28 @@
+package eg.edu.cu.csds.icare.data.remote.datasource
+
+import eg.edu.cu.csds.icare.core.domain.model.Appointment
+import eg.edu.cu.csds.icare.core.domain.model.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface RemoteAppointmentsDataSource {
+    fun getPatientAppointments(): Flow<List<Appointment>>
+
+    fun getAppointments(): Flow<List<Appointment>>
+
+    fun getAppointments(statusId: Short): Flow<List<Appointment>>
+
+    fun bookAppointment(
+        doctorId: Int,
+        dateTime: Long,
+    ): Flow<Resource<Nothing?>>
+
+    fun updateAppointmentStatus(
+        appointmentId: Long,
+        statusId: Short,
+    ): Flow<Resource<Nothing?>>
+
+    fun rescheduleAppointment(
+        appointmentId: Long,
+        dateTime: Long,
+    ): Flow<Resource<Nothing?>>
+}
