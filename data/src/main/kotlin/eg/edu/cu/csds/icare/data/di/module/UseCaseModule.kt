0b3f9fe3a5ed2eda335bd4f4664cda04ece01e44
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-08 12:05:20 +0200

    feat: Add Booking Use Cases to UseCaseModule
    The change introduces new use cases related to booking functionality within the `UseCaseModule`. These include: `BookAppointment`, `GetAppointments`, `GetAppointmentsByStatus`, `GetPatientAppointments`, `RescheduleAppointment`, and `UpdateAppointmentStatus`. Each of these use cases is now provided as a singleton within the dependency injection setup.

--- data/src/main/kotlin/eg/edu/cu/csds/icare/data/di/module/UseCaseModule.kt
+++ data/src/main/kotlin/eg/edu/cu/csds/icare/data/di/module/UseCaseModule.kt
@@ -2,6 +2,7 @@ package eg.edu.cu.csds.icare.data.di.module
 
 import eg.edu.cu.csds.icare.core.domain.repository.AppRepository
 import eg.edu.cu.csds.icare.core.domain.repository.AuthRepository
+import eg.edu.cu.csds.icare.core.domain.repository.BookingRepository
 import eg.edu.cu.csds.icare.core.domain.usecase.auth.DeleteAccount
 import eg.edu.cu.csds.icare.core.domain.usecase.auth.GetUserInfo
 import eg.edu.cu.csds.icare.core.domain.usecase.auth.LinkTokenAccount
@@ -10,6 +11,11 @@ import eg.edu.cu.csds.icare.core.domain.usecase.auth.SendRecoveryMail
 import eg.edu.cu.csds.icare.core.domain.usecase.auth.SignInWithEmailAndPassword
 import eg.edu.cu.csds.icare.core.domain.usecase.auth.SignInWithGoogle
 import eg.edu.cu.csds.icare.core.domain.usecase.auth.SignOut
+import eg.edu.cu.csds.icare.core.domain.usecase.booking.BookAppointment
+import eg.edu.cu.csds.icare.core.domain.usecase.booking.GetAppointments
+import eg.edu.cu.csds.icare.core.domain.usecase.booking.GetAppointmentsByStatus
+import eg.edu.cu.csds.icare.core.domain.usecase.booking.RescheduleAppointment
+import eg.edu.cu.csds.icare.core.domain.usecase.booking.UpdateAppointmentStatus
 import eg.edu.cu.csds.icare.core.domain.usecase.onboarding.ReadOnBoarding
 import eg.edu.cu.csds.icare.core.domain.usecase.onboarding.SaveOnBoarding
 import org.koin.core.annotation.Module
@@ -46,4 +52,22 @@ class UseCaseModule {
 
     @Single
     fun provideLinkTokenAccount(repository: AuthRepository) = LinkTokenAccount(repository)
+
+    @Single
+    fun provideBookAppointment(repository: BookingRepository) = BookAppointment(repository)
+
+    @Single
+    fun provideGetAppointments(repository: BookingRepository) = GetAppointments(repository)
+
+    @Single
+    fun provideGetAppointmentsByStatus(repository: BookingRepository) = GetAppointmentsByStatus(repository)
+
+    @Single
+    fun provideGetPatientAppointments(repository: BookingRepository) = GetAppointmentsByStatus(repository)
+
+    @Single
+    fun provideRescheduleAppointment(repository: BookingRepository) = RescheduleAppointment(repository)
+
+    @Single
+    fun provideUpdateAppointmentStatus(repository: BookingRepository): UpdateAppointmentStatus = UpdateAppointmentStatus(repository)
 }
