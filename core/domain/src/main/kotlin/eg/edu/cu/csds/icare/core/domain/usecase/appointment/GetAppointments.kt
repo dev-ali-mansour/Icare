6b3977efaf281e8258202197f67a9c133ada4e5c
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-08 14:24:13 +0200

    feat: Change Appointment retrieval Use Cases to Flow of Resource
    This commit modifies the `GetAppointments`, `GetAppointmentsByStatus`, and `GetPatientAppointments` use cases to return a `Flow` of `Resource<List<Appointment>>` instead of `Flow<List<Appointment>>`. This change aligns the use cases with the resource pattern, providing a more consistent way to handle data and states.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/appointment/GetAppointments.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/appointment/GetAppointments.kt
@@ -1,11 +1,12 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.appointment
 
 import eg.edu.cu.csds.icare.core.domain.model.Appointment
+import eg.edu.cu.csds.icare.core.domain.model.Resource
 import eg.edu.cu.csds.icare.core.domain.repository.AppointmentsRepository
 import kotlinx.coroutines.flow.Flow
 
 class GetAppointments(
     private val repository: AppointmentsRepository,
 ) {
-    operator fun invoke(): Flow<List<Appointment>> = repository.getAppointments()
+    operator fun invoke(): Flow<Resource<List<Appointment>>> = repository.getAppointments()
 }
--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/appointment/GetAppointmentsByStatus.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/appointment/GetAppointmentsByStatus.kt
@@ -1,11 +1,12 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.appointment
 
 import eg.edu.cu.csds.icare.core.domain.model.Appointment
+import eg.edu.cu.csds.icare.core.domain.model.Resource
 import eg.edu.cu.csds.icare.core.domain.repository.AppointmentsRepository
 import kotlinx.coroutines.flow.Flow
 
 class GetAppointmentsByStatus(
     private val repository: AppointmentsRepository,
 ) {
-    operator fun invoke(statusId: Short): Flow<List<Appointment>> = repository.getAppointments(statusId)
+    operator fun invoke(statusId: Short): Flow<Resource<List<Appointment>>> = repository.getAppointments(statusId)
 }
