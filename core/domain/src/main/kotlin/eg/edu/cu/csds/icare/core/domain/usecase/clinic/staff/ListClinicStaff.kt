99232ba9f557905c4685eb426d110489600502fa
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-09 21:59:02 +0200

    feat: Add ListClinicStaff use case
    This commit introduces the `ListClinicStaff` use case, which fetches a list of clinic staff from the `ClinicsRepository`.
    - Added `ListClinicStaff` class with `invoke` operator function.
    - The `invoke` function takes `clinicId` as a parameter and returns `Flow` of `Resource` of `List` of `ClinicStaff`.
    - The `ListClinicStaff` use case is used to fetch a list of clinic staff from the repository.

---
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/clinic/staff/ListClinicStaff.kt
@@ -0,0 +1,12 @@
+package eg.edu.cu.csds.icare.core.domain.usecase.clinic.staff
+
+import eg.edu.cu.csds.icare.core.domain.model.ClinicStaff
+import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.repository.ClinicsRepository
+import kotlinx.coroutines.flow.Flow
+
+class ListClinicStaff(
+    private val repository: ClinicsRepository,
+) {
+    operator fun invoke(clinicId: Long): Flow<Resource<List<ClinicStaff>>> = repository.listClinicStaff(clinicId)
+}
