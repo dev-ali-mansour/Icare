f08a8f4008bae3a0da02f02e314dd65ef89915fe
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-09 21:00:14 +0200

    feat: Rename AppointmentStatus to ReferralStatus
    The `AppointmentStatus` and `AppointmentStatusesResponse` classes have been renamed to `ReferralStatus` and `ReferralStatusesResponse`, respectively.
    - Renamed `AppointmentStatus` to `ReferralStatus`.
    - Renamed `AppointmentStatusesResponse` to `ReferralStatusesResponse`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/ReferralStatus.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/ReferralStatus.kt
@@ -3,7 +3,7 @@ package eg.edu.cu.csds.icare.core.domain.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class AppointmentStatus(
+data class ReferralStatus(
     val id: Short = 0,
     val name: String = "",
     val nameAr: String = "",
