58beb188904d65acd37d3ee84abd2e5f919a78fa
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-09 23:26:29 +0200

    feat: Add BookingMethodDao to Room database
    The `BookingMethodDao` is now accessible from the Room database.
    - Added `provideBookingMethodDao` to provide an instance of `BookingMethodDao`.

--- data/src/main/kotlin/eg/edu/cu/csds/icare/data/di/module/RoomModule.kt
+++ data/src/main/kotlin/eg/edu/cu/csds/icare/data/di/module/RoomModule.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import eg.edu.cu.csds.icare.data.BuildConfig
 import eg.edu.cu.csds.icare.data.local.db.AppDatabase
 import eg.edu.cu.csds.icare.data.local.db.DbPassPhrase
+import eg.edu.cu.csds.icare.data.local.db.dao.BookingMethodDao
 import eg.edu.cu.csds.icare.data.local.db.dao.SettingsDao
 import eg.edu.cu.csds.icare.data.local.db.dao.UserDao
 import net.sqlcipher.database.SupportFactory
@@ -41,6 +42,9 @@ class RoomModule {
                     ).openHelperFactory(supportFactory)
                     .build()
         }
+
+    @Single
+    fun provideBookingMethodDao(db: AppDatabase): BookingMethodDao = db.bookingMethodDao()
 
     @Single
     fun provideSettingsDao(db: AppDatabase): SettingsDao = db.settingsDao()
