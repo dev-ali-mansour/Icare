0d718fd020d2d9e96bf01e59417ebb6b26723304
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-09 23:26:21 +0200

    feat: Add BookingMethodEntity and update database schema to version 3
    This commit introduces the `BookingMethodEntity` and its corresponding DAO, `BookingMethodDao`, to the Room database. Additionally, it updates the database version to 3 and configures an automatic migration from version 2 to 3.
    - Added `BookingMethodEntity` to represent booking methods.
    - Added `BookingMethodDao` for database operations related to booking methods.
    - Updated the `AppDatabase` to include `BookingMethodEntity` and `BookingMethodDao`.
    - Increased the database version to 3.
    - Configured an automatic migration from version 2 to 3.
    - Added `data/schemas/eg.edu.cu.csds.icare.data.local.db.AppDatabase/3.json` file for the new schema.

--- data/src/main/kotlin/eg/edu/cu/csds/icare/data/local/db/AppDatabase.kt
+++ data/src/main/kotlin/eg/edu/cu/csds/icare/data/local/db/AppDatabase.kt
@@ -3,21 +3,26 @@ package eg.edu.cu.csds.icare.data.local.db
 import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import eg.edu.cu.csds.icare.data.local.db.dao.BookingMethodDao
 import eg.edu.cu.csds.icare.data.local.db.dao.SettingsDao
 import eg.edu.cu.csds.icare.data.local.db.dao.UserDao
+import eg.edu.cu.csds.icare.data.local.db.entity.BookingMethodEntity
 import eg.edu.cu.csds.icare.data.local.db.entity.PermissionEntity
 import eg.edu.cu.csds.icare.data.local.db.entity.SettingsEntity
 import eg.edu.cu.csds.icare.data.local.db.entity.UserEntity
 
 @Database(
-    entities = [SettingsEntity::class, UserEntity::class, PermissionEntity::class],
-    version = 2,
+    entities = [BookingMethodEntity::class, SettingsEntity::class, UserEntity::class, PermissionEntity::class],
+    version = 3,
     exportSchema = true,
     autoMigrations = [
         AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 2, to = 3),
     ],
 )
 abstract class AppDatabase : RoomDatabase() {
+    abstract fun bookingMethodDao(): BookingMethodDao
+
     abstract fun settingsDao(): SettingsDao
 
     abstract fun userDao(): UserDao
