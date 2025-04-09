9d0f4cc95d099d300fc8ef2dc4986b6041abde7a
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-09 20:47:09 +0200

    feat: Create CenterStaff data class
    The `CenterStaff` data class is introduced to represent a staff member associated with a center.
    - `CenterStaff` data class includes `id`, `firstName`, `lastName`, derived `name`, `centerId`, `email`, `phone`, and `profilePictureUrl`.
    - `name` property is marked as `@Transient` and calculated based on `firstName` and `lastName`.

---
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/CenterStaff.kt
@@ -0,0 +1,17 @@
+package eg.edu.cu.csds.icare.core.domain.model
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+
+@Serializable
+data class CenterStaff(
+    val id: Long = 0,
+    val firstName: String = "",
+    val lastName: String = "",
+    @Transient
+    val name: String = "$firstName $lastName",
+    val centerId: Long = 0,
+    val email: String = "",
+    val phone: String = "",
+    val profilePictureUrl: String? = null,
+)
