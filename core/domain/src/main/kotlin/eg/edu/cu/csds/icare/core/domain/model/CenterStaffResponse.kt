d50853b75a494c7b62eabb6348b2b2d0937dd6f9
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-09 20:47:16 +0200

    feat: Create CenterStaffResponse data class
    The `CenterStaffResponse` data class was added to represent the response structure for center staff data. It includes a status code and a list of `CenterStaff`.

---
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/CenterStaffResponse.kt
@@ -0,0 +1,9 @@
+package eg.edu.cu.csds.icare.core.domain.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CenterStaffResponse(
+    val statusCode: Short,
+    val centerStaff: List<CenterStaff>,
+)
