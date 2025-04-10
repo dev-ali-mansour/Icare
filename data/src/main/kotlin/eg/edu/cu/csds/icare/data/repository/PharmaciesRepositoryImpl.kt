1733aca9f30e99173b98ef4239b67bff03c50cfa
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-10 16:12:37 +0200

    feat: Implement PharmaciesRepositoryImpl with local and remote data sources
    This commit implements the `PharmaciesRepositoryImpl` to manage pharmacy data using both local and remote data sources.
    - Implemented `listPharmacies` function to fetch pharmacies from either the local or remote data source, depending on the `forceRefresh` flag.
    - If `forceRefresh` is false, it will try to retrieve the pharmacies from `LocalPharmaciesDataSource`.
    - If `forceRefresh` is true, it will fetch data from `RemotePharmaciesDataSource`, persist it to the local database, and then retrieve it from `LocalPharmaciesDataSource`.
    - Implemented `addNewPharmacy`, `updatePharmacy`, `listPharmacists`, `addNewPharmacist`, and `updatePharmacist` functions to interact with `RemotePharmaciesDataSource`.
    - Used `distinctUntilChanged` to avoid unnecessary UI updates when the local database content is not changed.
    - Added the required dependencies to the constructor for `RemotePharmaciesDataSource` and `LocalPharmaciesDataSource`.
    - Used entity mapper functions `toEntity` and `toModel`.
    - Used `flow` and `map` to handle the data flow.

--- data/src/main/kotlin/eg/edu/cu/csds/icare/data/repository/PharmaciesRepositoryImpl.kt
+++ data/src/main/kotlin/eg/edu/cu/csds/icare/data/repository/PharmaciesRepositoryImpl.kt
@@ -4,32 +4,65 @@ import eg.edu.cu.csds.icare.core.domain.model.Pharmacist
 import eg.edu.cu.csds.icare.core.domain.model.Pharmacy
 import eg.edu.cu.csds.icare.core.domain.model.Resource
 import eg.edu.cu.csds.icare.core.domain.repository.PharmaciesRepository
+import eg.edu.cu.csds.icare.data.local.datasource.LocalPharmaciesDataSource
+import eg.edu.cu.csds.icare.data.local.db.entity.toEntity
+import eg.edu.cu.csds.icare.data.local.db.entity.toModel
+import eg.edu.cu.csds.icare.data.remote.datasource.RemotePharmaciesDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import org.koin.core.annotation.Single
 
 @Single
-class PharmaciesRepositoryImpl : PharmaciesRepository {
-    override fun listPharmacies(forceUpdate: Boolean): Flow<Resource<List<Pharmacy>>> {
-        TODO("Not yet implemented")
-    }
+class PharmaciesRepositoryImpl(
+    private val remotePharmaciesDataSource: RemotePharmaciesDataSource,
+    private val localPharmaciesDataSource: LocalPharmaciesDataSource,
+) : PharmaciesRepository {
+    override fun listPharmacies(forceRefresh: Boolean): Flow<Resource<List<Pharmacy>>> =
+        flow {
+            if (!forceRefresh) {
+                localPharmaciesDataSource
+                    .listPharmacies()
+                    .distinctUntilChanged()
+                    .collect { entities ->
+                        emit(Resource.Success(data = entities.map { it.toModel() }))
+                    }
+                return@flow
+            }
+            remotePharmaciesDataSource.fetchPharmacies().map { res ->
+                when (res) {
+                    is Resource.Unspecified -> emit(Resource.Unspecified())
 
-    override fun addNewPharmacy(center: Pharmacy): Flow<Resource<Nothing?>> {
-        TODO("Not yet implemented")
-    }
+                    is Resource.Loading -> emit(Resource.Loading())
 
-    override fun updatePharmacy(pharmacy: Pharmacy): Flow<Resource<Nothing?>> {
-        TODO("Not yet implemented")
-    }
+                    is Resource.Success -> {
+                        res.data?.let { pharmacies ->
+                            localPharmaciesDataSource.persistPharmacies(pharmacies.map { it.toEntity() })
+                        }
+                        localPharmaciesDataSource
+                            .listPharmacies()
+                            .distinctUntilChanged()
+                            .collect { entities ->
+                                emit(Resource.Success(data = entities.map { it.toModel() }))
+                            }
+                    }
 
-    override fun listPharmacists(pharmacyId: Long): Flow<Resource<List<Pharmacist>>> {
-        TODO("Not yet implemented")
-    }
+                    is Resource.Error -> emit(Resource.Error(res.error))
+                }
+            }
+        }
 
-    override fun addNewPharmacist(pharmacist: Pharmacist): Flow<Resource<Nothing?>> {
-        TODO("Not yet implemented")
-    }
+    override fun addNewPharmacy(pharmacy: Pharmacy): Flow<Resource<Nothing?>> = remotePharmaciesDataSource.addNewPharmacy(pharmacy)
 
-    override fun updatePharmacist(pharmacist: Pharmacist): Flow<Resource<Nothing?>> {
-        TODO("Not yet implemented")
-    }
+    override fun updatePharmacy(pharmacy: Pharmacy): Flow<Resource<Nothing?>> = remotePharmaciesDataSource.updatePharmacy(pharmacy)
+
+    override fun listPharmacists(pharmacyId: Long): Flow<Resource<List<Pharmacist>>> =
+        remotePharmaciesDataSource.listPharmacists(pharmacyId)
+
+    override fun addNewPharmacist(pharmacist: Pharmacist): Flow<Resource<Nothing?>> =
+        remotePharmaciesDataSource.addNewPharmacist(pharmacist)
+
+    override fun updatePharmacist(pharmacist: Pharmacist): Flow<Resource<Nothing?>> =
+        remotePharmaciesDataSource.updatePharmacist(pharmacist)
 }
