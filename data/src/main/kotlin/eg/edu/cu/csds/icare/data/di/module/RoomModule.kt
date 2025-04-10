4469541843b9d2508e7112f493831806a82934f8
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-10 17:44:12 +0200

    refactor: Add missing DAO providers to RoomModule
    This commit adds the missing DAO providers to the `RoomModule` for dependency injection.
    - Added `provideClinicDao` function to provide `ClinicDao`.
    - Added `provideDoctorDao` function to provide `DoctorDao`.
    - Added `provideCenterDao` function to provide `CenterDao`.
    - Added `providePharmacyDao` function to provide `PharmacyDao`.
    - Moved `provideBookingMethodDao` function after `provideUserDao` function.

--- data/src/main/kotlin/eg/edu/cu/csds/icare/data/di/module/RoomModule.kt
+++ data/src/main/kotlin/eg/edu/cu/csds/icare/data/di/module/RoomModule.kt
@@ -6,6 +6,10 @@ import eg.edu.cu.csds.icare.data.BuildConfig
 import eg.edu.cu.csds.icare.data.local.db.AppDatabase
 import eg.edu.cu.csds.icare.data.local.db.DbPassPhrase
 import eg.edu.cu.csds.icare.data.local.db.dao.BookingMethodDao
+import eg.edu.cu.csds.icare.data.local.db.dao.CenterDao
+import eg.edu.cu.csds.icare.data.local.db.dao.ClinicDao
+import eg.edu.cu.csds.icare.data.local.db.dao.DoctorDao
+import eg.edu.cu.csds.icare.data.local.db.dao.PharmacyDao
 import eg.edu.cu.csds.icare.data.local.db.dao.SettingsDao
 import eg.edu.cu.csds.icare.data.local.db.dao.UserDao
 import net.sqlcipher.database.SupportFactory
@@ -44,11 +48,23 @@ class RoomModule {
         }
 
     @Single
-    fun provideBookingMethodDao(db: AppDatabase): BookingMethodDao = db.bookingMethodDao()
-
-    @Single
     fun provideSettingsDao(db: AppDatabase): SettingsDao = db.settingsDao()
 
     @Single
     fun provideUserDao(db: AppDatabase): UserDao = db.userDao()
+
+    @Single
+    fun provideBookingMethodDao(db: AppDatabase): BookingMethodDao = db.bookingMethodDao()
+
+    @Single
+    fun provideClinicDao(db: AppDatabase): ClinicDao = db.clinicDao()
+
+    @Single
+    fun provideDoctorDao(db: AppDatabase): DoctorDao = db.doctorDao()
+
+    @Single
+    fun provideCenterDao(db: AppDatabase): CenterDao = db.centerDao()
+
+    @Single
+    fun providePharmacyDao(db: AppDatabase): PharmacyDao = db.pharmacyDao()
 }
