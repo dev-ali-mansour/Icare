f40cb9e1f35cf7cf556305ffc86198747490fe25
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-10 14:32:22 +0200

    feat: Update AppDatabase to version 4 and add new entities
    The `AppDatabase` has been updated to version 4 and now includes additional entities and their respective DAOs.
    - The database version is incremented to 4.
    - Added `ClinicEntity`, `CenterEntity`, `DoctorEntity`, and `PharmacyEntity`.
    - Added `ClinicDao`, `CenterDao`, `DoctorDao`, and `PharmacyDao`.
    - Updated entities list to include: `BookingMethodEntity`, `ClinicEntity`, `CenterEntity`, `DoctorEntity`, and `PharmacyEntity`.
    - Added new auto migration from version 3 to version 4.
    - Added schema file `4.json` to reflect the database update to version 4.

--- data/src/main/kotlin/eg/edu/cu/csds/icare/data/local/db/AppDatabase.kt
+++ data/src/main/kotlin/eg/edu/cu/csds/icare/data/local/db/AppDatabase.kt
@@ -4,20 +4,33 @@ import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import eg.edu.cu.csds.icare.data.local.db.dao.BookingMethodDao
+import eg.edu.cu.csds.icare.data.local.db.dao.CenterDao
+import eg.edu.cu.csds.icare.data.local.db.dao.ClinicDao
+import eg.edu.cu.csds.icare.data.local.db.dao.DoctorDao
+import eg.edu.cu.csds.icare.data.local.db.dao.PharmacyDao
 import eg.edu.cu.csds.icare.data.local.db.dao.SettingsDao
 import eg.edu.cu.csds.icare.data.local.db.dao.UserDao
 import eg.edu.cu.csds.icare.data.local.db.entity.BookingMethodEntity
+import eg.edu.cu.csds.icare.data.local.db.entity.CenterEntity
+import eg.edu.cu.csds.icare.data.local.db.entity.ClinicEntity
+import eg.edu.cu.csds.icare.data.local.db.entity.DoctorEntity
 import eg.edu.cu.csds.icare.data.local.db.entity.PermissionEntity
+import eg.edu.cu.csds.icare.data.local.db.entity.PharmacyEntity
 import eg.edu.cu.csds.icare.data.local.db.entity.SettingsEntity
 import eg.edu.cu.csds.icare.data.local.db.entity.UserEntity
 
 @Database(
-    entities = [BookingMethodEntity::class, SettingsEntity::class, UserEntity::class, PermissionEntity::class],
-    version = 3,
+    entities = [
+        SettingsEntity::class, UserEntity::class, PermissionEntity::class,
+        BookingMethodEntity::class, ClinicEntity::class, CenterEntity::class, DoctorEntity::class,
+        PharmacyEntity::class,
+    ],
+    version = 4,
     exportSchema = true,
     autoMigrations = [
         AutoMigration(from = 1, to = 2),
         AutoMigration(from = 2, to = 3),
+        AutoMigration(from = 3, to = 4),
     ],
 )
 abstract class AppDatabase : RoomDatabase() {
@@ -26,4 +39,12 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun settingsDao(): SettingsDao
 
     abstract fun userDao(): UserDao
+
+    abstract fun clinicDao(): ClinicDao
+
+    abstract fun doctorDao(): DoctorDao
+
+    abstract fun centerDao(): CenterDao
+
+    abstract fun pharmacyDao(): PharmacyDao
 }
