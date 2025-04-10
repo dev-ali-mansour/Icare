f1c17c0b89e69497f4e378350aa67c5d76b43618
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-10 13:48:45 +0200

    feat: Add API endpoints for clinics, doctors, centers, and pharmacies
    The `ApiService` now includes new endpoints for fetching data related to clinics, doctors, centers, and pharmacies.
    - Added `fetchClinics` endpoint.
    - Added `fetchDoctors` endpoint.
    - Added `fetchCenters` endpoint.
    - Added `fetchPharmacies` endpoint.

--- data/src/main/kotlin/eg/edu/cu/csds/icare/data/remote/serivce/ApiService.kt
+++ data/src/main/kotlin/eg/edu/cu/csds/icare/data/remote/serivce/ApiService.kt
@@ -3,7 +3,11 @@ package eg.edu.cu.csds.icare.data.remote.serivce
 import eg.edu.cu.csds.icare.core.domain.model.ActionResultResponse
 import eg.edu.cu.csds.icare.core.domain.model.AppointmentsResponse
 import eg.edu.cu.csds.icare.core.domain.model.BookingMethodsResponse
+import eg.edu.cu.csds.icare.core.domain.model.CentersResponse
+import eg.edu.cu.csds.icare.core.domain.model.ClinicsResponse
+import eg.edu.cu.csds.icare.core.domain.model.DoctorsResponse
 import eg.edu.cu.csds.icare.core.domain.model.LoginInfoResponse
+import eg.edu.cu.csds.icare.core.domain.model.PharmaciesResponse
 import eg.edu.cu.csds.icare.core.domain.model.RegisteredResponse
 import retrofit2.Response
 import retrofit2.http.Body
@@ -12,6 +16,18 @@ import retrofit2.http.POST
 interface ApiService {
     @POST("bookingMethods")
     suspend fun fetchBookingMethods(): Response<BookingMethodsResponse>
+
+    @POST("clinics")
+    suspend fun fetchClinics(): Response<ClinicsResponse>
+
+    @POST("doctors")
+    suspend fun fetchDoctors(): Response<DoctorsResponse>
+
+    @POST("centers")
+    suspend fun fetchCenters(): Response<CentersResponse>
+
+    @POST("pharmacies")
+    suspend fun fetchPharmacies(): Response<PharmaciesResponse>
 
     @POST("isRegistered")
     suspend fun isRegistered(
