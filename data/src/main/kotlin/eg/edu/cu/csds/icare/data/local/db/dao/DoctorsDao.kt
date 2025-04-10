cc1a7c76df9df614b1d8d6fec1ea16e03f379825
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-10 14:10:01 +0200

    feat: Create DoctorsDao to manage doctor data in the database
    The `DoctorsDao` interface provides methods for:
    - Persisting a list of `DoctorEntity` objects into the database.
    - Retrieving all doctors.
    - Retrieving doctors by `clinicId`.

---
+++ data/src/main/kotlin/eg/edu/cu/csds/icare/data/local/db/dao/DoctorsDao.kt
@@ -0,0 +1,20 @@
+package eg.edu.cu.csds.icare.data.local.db.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import eg.edu.cu.csds.icare.data.local.db.entity.DoctorEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DoctorsDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun persistDoctors(doctors: List<DoctorEntity>)
+
+    @Query("SELECT * FROM doctors")
+    fun listDoctors(): Flow<List<DoctorEntity>>
+
+    @Query("SELECT * FROM doctors WHERE clinicId=:clinicId")
+    fun listDoctors(clinicId: Long): Flow<List<DoctorEntity>>
+}
