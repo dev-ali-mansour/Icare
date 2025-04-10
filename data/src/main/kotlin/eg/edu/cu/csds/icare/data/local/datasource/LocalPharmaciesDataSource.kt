dda77ab75e2ada2682b1b481af5e444194bd9b9d
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-10 16:12:24 +0200

    feat: Rename and update LocalPharmacyDataSource to LocalPharmaciesDataSource
    This commit renames the `LocalPharmacyDataSource` interface and its implementation to `LocalPharmaciesDataSource` and `LocalPharmaciesDataSourceImpl`, respectively, and updates method names for better clarity.
    - Renamed `LocalPharmacyDataSource` to `LocalPharmaciesDataSource`.
    - Renamed `LocalPharmacyDataSourceImpl` to `LocalPharmaciesDataSourceImpl`.
    - Renamed `persistClinics` to `persistPharmacies`.

--- data/src/main/kotlin/eg/edu/cu/csds/icare/data/local/datasource/LocalPharmaciesDataSource.kt
+++ data/src/main/kotlin/eg/edu/cu/csds/icare/data/local/datasource/LocalPharmaciesDataSource.kt
@@ -3,8 +3,8 @@ package eg.edu.cu.csds.icare.data.local.datasource
 import eg.edu.cu.csds.icare.data.local.db.entity.PharmacyEntity
 import kotlinx.coroutines.flow.Flow
 
-interface LocalPharmacyDataSource {
-    fun persistClinics(pharmacies: List<PharmacyEntity>)
+interface LocalPharmaciesDataSource {
+    fun persistPharmacies(pharmacies: List<PharmacyEntity>)
 
     fun listPharmacies(): Flow<List<PharmacyEntity>>
 }
