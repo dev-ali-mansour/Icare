21e0917655dd8045c094cc7f157f0582fec499e8
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-14 13:57:42 +0200

    feat: Add status code field to ClinicStaffResponse
    This commit adds a `statusCode` field to the `ClinicStaffResponse` data class, annotated with `@SerialName("status")` for JSON serialization.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/ClinicStaffResponse.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/ClinicStaffResponse.kt
@@ -1,9 +1,11 @@
 package eg.edu.cu.csds.icare.core.domain.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ClinicStaffResponse(
+    @SerialName("status")
     val statusCode: Short,
     val staffList: List<ClinicStaff>,
 )
