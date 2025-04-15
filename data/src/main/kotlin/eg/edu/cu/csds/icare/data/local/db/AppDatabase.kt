fba6d3b00562db498e39943327ce6de405864000
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-15 15:14:08 +0200

    feat: Update AppDatabase to version 5 with schema changes
    This commit updates the `AppDatabase` to version 5 and includes schema modifications. These changes involve removing the `availability` and `rating` columns from the `doctors` table and renaming the `profilePictureUrl` column to `profilePicture`. An `AutoMigrationSpec` has been added to handle these changes during the migration process. Also a new version schema `5.json` is generated.

--- data/src/main/kotlin/eg/edu/cu/csds/icare/data/local/db/AppDatabase.kt
+++ data/src/main/kotlin/eg/edu/cu/csds/icare/data/local/db/AppDatabase.kt
@@ -2,7 +2,11 @@ package eg.edu.cu.csds.icare.data.local.db
 
 import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.DeleteColumn
+import androidx.room.RenameColumn
 import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
+import androidx.sqlite.db.SupportSQLiteDatabase
 import eg.edu.cu.csds.icare.data.local.db.dao.BookingMethodDao
 import eg.edu.cu.csds.icare.data.local.db.dao.CenterDao
 import eg.edu.cu.csds.icare.data.local.db.dao.ClinicDao
@@ -25,12 +29,13 @@ import eg.edu.cu.csds.icare.data.local.db.entity.UserEntity
         BookingMethodEntity::class, ClinicEntity::class, CenterEntity::class, DoctorEntity::class,
         PharmacyEntity::class,
     ],
-    version = 4,
+    version = 5,
     exportSchema = true,
     autoMigrations = [
         AutoMigration(from = 1, to = 2),
         AutoMigration(from = 2, to = 3),
         AutoMigration(from = 3, to = 4),
+        AutoMigration(from = 4, to = 5, spec = AppDatabase.AutoMigrationSpec5::class),
     ],
 )
 abstract class AppDatabase : RoomDatabase() {
@@ -47,4 +52,22 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun centerDao(): CenterDao
 
     abstract fun pharmacyDao(): PharmacyDao
+
+    @DeleteColumn.Entries(
+        DeleteColumn(tableName = "doctors", columnName = "availability"),
+        DeleteColumn(tableName = "doctors", columnName = "rating"),
+    )
+    @RenameColumn.Entries(
+        RenameColumn(
+            tableName = "doctors",
+            fromColumnName = "profilePictureUrl",
+            toColumnName = "profilePicture",
+        ),
+    )
+    class AutoMigrationSpec5 : AutoMigrationSpec {
+        @Override
+        override fun onPostMigrate(db: SupportSQLiteDatabase) {
+            // Invoked once auto migration is done
+        }
+    }
 }
