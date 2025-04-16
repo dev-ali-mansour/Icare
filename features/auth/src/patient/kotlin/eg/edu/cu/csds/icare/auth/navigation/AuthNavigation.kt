71906800466fb0ab117c8a91fcaff898bd5f04b9
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-16 17:16:30 +0200

    fix: Reorder composable functions in AuthNavigation
    This commit reorders the composable functions within the `AuthNavigation` in both the patient and staff modules for better organization.
    - The `PasswordRecovery` composable has been moved after the `Login` composable.
    - The `Register` composable has been moved to be after the `Profile` composable in `patient` module.

--- features/auth/src/patient/kotlin/eg/edu/cu/csds/icare/auth/navigation/AuthNavigation.kt
+++ features/auth/src/patient/kotlin/eg/edu/cu/csds/icare/auth/navigation/AuthNavigation.kt
@@ -4,6 +4,10 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.google.firebase.auth.FirebaseAuth
 import eg.edu.cu.csds.icare.auth.screen.AuthViewModel
+import eg.edu.cu.csds.icare.auth.screen.login.LoginScreen
+import eg.edu.cu.csds.icare.auth.screen.profile.ProfileScreen
+import eg.edu.cu.csds.icare.auth.screen.recovery.PasswordRecoveryScreen
+import eg.edu.cu.csds.icare.auth.screen.register.RegistrationScreen
 import eg.edu.cu.csds.icare.core.ui.MainViewModel
 import eg.edu.cu.csds.icare.core.ui.navigation.Screen
 
@@ -19,40 +23,39 @@ fun NavGraphBuilder.authenticationRoute(
     onRegisterCompleted: () -> Unit,
     onError: suspend (Throwable?) -> Unit,
 ) {
+    composable<Screen.Login> {
+        LoginScreen(
+            onRecoveryClicked = { onRecoveryClicked() },
+            onLoginSuccess = { onLoginSuccess() },
+            onCreateAnAccountClicked = { onCreateAccountClicked() },
+            authViewModel = authViewModel,
+        )
+    }
+
+    composable<Screen.PasswordRecovery> {
+        PasswordRecoveryScreen(
+            onLoginClicked = { onLoginClicked() },
+            onRecoveryCompleted = { onRecoveryCompleted() },
+            onError = { onError(it) },
+            authViewModel = authViewModel,
+        )
+    }
+
+    composable<Screen.Profile> {
+        ProfileScreen(
+            firebaseAuth = firebaseAuth,
+            mainViewModel = mainViewModel,
+            authViewModel = authViewModel,
+            onError = { onError(it) },
+        )
+    }
+
     composable<Screen.Register> {
-        composable<Screen.Login> {
-            LoginScreen(
-                onRecoveryClicked = { onRecoveryClicked() },
-                onLoginSuccess = { onLoginSuccess() },
-                onCreateAnAccountClicked = { onCreateAccountClicked() },
-                authViewModel = authViewModel,
-            )
-        }
-        composable<Screen.PasswordRecovery> {
-            PasswordRecoveryScreen(
-                onLoginClicked = { onLoginClicked() },
-                onRecoveryCompleted = { onRecoveryCompleted() },
-                onError = { onError(it) },
-                authViewModel = authViewModel,
-            )
-        }
-
-        composable<Screen.Profile> {
-            ProfileScreen(
-                firebaseAuth = firebaseAuth,
-                mainViewModel = mainViewModel,
-                authViewModel = authViewModel,
-                onError = { onError(it) },
-            )
-        }
-
-        composable<Screen.Registration> {
-            RegistrationScreen(
-                firebaseAuth = firebaseAuth,
-                onLoginClicked = { onLoginClicked() },
-                onRegisterCompleted = { onRegisterCompleted() },
-                authViewModel = authViewModel,
-            )
-        }
+        RegistrationScreen(
+            firebaseAuth = firebaseAuth,
+            onLoginClicked = { onLoginClicked() },
+            onRegisterCompleted = { onRegisterCompleted() },
+            authViewModel = authViewModel,
+        )
     }
 }
