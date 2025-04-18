9000badaa2144d87ed481d1847afc5ac6ff5e279
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-18 14:58:25 +0200

    refactor: Rename Registration Screen and Update Navigation
    This commit renames the Registration screen to Register and updates the corresponding navigation.
    - Renamed the `Registration` screen to `Register` in the `Screen` sealed class.
    - Updated the `composable` function to use `Screen.Register` for the registration route.
    - Kept `RegistrationScreen` name as is.
    - Updated the import of `RegistrationScreen` to keep it as `RegistrationScreen` after the change.

--- features/auth/src/patient/kotlin/eg/edu/cu/csds/icare/auth/navigation/AuthNavigation.kt
+++ features/auth/src/patient/kotlin/eg/edu/cu/csds/icare/auth/navigation/AuthNavigation.kt
@@ -4,6 +4,10 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.google.firebase.auth.FirebaseAuth
 import eg.edu.cu.csds.icare.auth.screen.AuthViewModel
+import eg.edu.cu.csds.icare.auth.screen.login.LoginScreen
+import eg.edu.cu.csds.icare.auth.screen.profile.ProfileScreen
+import eg.edu.cu.csds.icare.auth.screen.recovery.PasswordRecoveryScreen
+import eg.edu.cu.csds.icare.auth.screen.register.RegistrationScreen
 import eg.edu.cu.csds.icare.core.ui.MainViewModel
 import eg.edu.cu.csds.icare.core.ui.navigation.Screen
 
@@ -46,7 +50,7 @@ fun NavGraphBuilder.authenticationRoute(
         )
     }
 
-    composable<Screen.Registration> {
+    composable<Screen.Register> {
         RegistrationScreen(
             firebaseAuth = firebaseAuth,
             onLoginClicked = { onLoginClicked() },
