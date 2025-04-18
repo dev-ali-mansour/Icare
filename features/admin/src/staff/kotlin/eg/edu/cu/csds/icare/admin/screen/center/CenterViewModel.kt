861c545c24b1e2cfb4ea8932480b2ff1232905d9
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-18 13:34:04 +0200

    refactor: Remove `CenterStaffViewModel` and integrate staff management into `CenterViewModel`
    This commit removes the dedicated `CenterStaffViewModel` and integrates center staff management functionalities directly into `CenterViewModel`.
    - Deleted `CenterStaffViewModel`.
    - Added `addNewCenterStaffUseCase`, `updateCenterStaffUseCase`, and `listCenterStaffUseCase` to `CenterViewModel`.
    - Moved state variables and methods related to center staff management to `CenterViewModel`:
        - `selectedCenterStaffState`
        - `centerStaffsResFlow`
        - `addNewCenterStaff()`
        - `updateCenterStaff()`
        - `listStaff()`
        - `selectedCenterIdState`
        - `firstNameState`
        - `lastNameState`
        - `centerIdState`
        - `emailState`
        - `profilePictureState`
    - Updated `CenterViewModel` to manage center staff operations directly.

--- features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/center/CenterViewModel.kt
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/center/CenterViewModel.kt
@@ -1,15 +1,20 @@
 package eg.edu.cu.csds.icare.admin.screen.center
 
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import eg.edu.cu.csds.icare.core.domain.model.CenterStaff
 import eg.edu.cu.csds.icare.core.domain.model.LabImagingCenter
 import eg.edu.cu.csds.icare.core.domain.model.Resource
 import eg.edu.cu.csds.icare.core.domain.usecase.center.AddNewCenter
 import eg.edu.cu.csds.icare.core.domain.usecase.center.ListCenters
 import eg.edu.cu.csds.icare.core.domain.usecase.center.UpdateCenter
+import eg.edu.cu.csds.icare.core.domain.usecase.center.staff.AddNewCenterStaff
+import eg.edu.cu.csds.icare.core.domain.usecase.center.staff.ListCenterStaff
+import eg.edu.cu.csds.icare.core.domain.usecase.center.staff.UpdateCenterStaff
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -23,6 +28,9 @@ class CenterViewModel(
     private val addNewCenterUseCase: AddNewCenter,
     private val updateCenterUseCase: UpdateCenter,
     private val listCentersUseCase: ListCenters,
+    private val addNewCenterStaffUseCase: AddNewCenterStaff,
+    private val updateCenterStaffUseCase: UpdateCenterStaff,
+    private val listCenterStaffUseCase: ListCenterStaff,
 ) : ViewModel() {
     private val _actionResFlow =
         MutableStateFlow<Resource<Nothing?>>(Resource.Unspecified())
@@ -30,7 +38,13 @@ class CenterViewModel(
     private val _centersResFlow =
         MutableStateFlow<Resource<List<LabImagingCenter>>>(Resource.Unspecified())
     val centersResFlow: StateFlow<Resource<List<LabImagingCenter>>> = _centersResFlow
+    private val _centerStaffsResFlow =
+        MutableStateFlow<Resource<List<CenterStaff>>>(Resource.Unspecified())
+    val centerStaffsResFlow: StateFlow<Resource<List<CenterStaff>>> = _centerStaffsResFlow
+    var selectedCenterState: MutableState<LabImagingCenter?> = mutableStateOf(null)
+    var selectedCenterStaffState: MutableState<CenterStaff?> = mutableStateOf(null)
 
+    var selectedCenterIdState = mutableLongStateOf(0)
     var idState = mutableLongStateOf(0)
     var nameState = mutableStateOf("")
     var typeState = mutableStateOf(0.toShort())
@@ -39,6 +53,11 @@ class CenterViewModel(
     var longitudeState = mutableDoubleStateOf(0.0)
     var latitudeState = mutableDoubleStateOf(0.0)
     var contractStatusIdState = mutableStateOf(0.toShort())
+    var firstNameState = mutableStateOf("")
+    var lastNameState = mutableStateOf("")
+    var centerIdState = mutableLongStateOf(0)
+    var emailState = mutableStateOf("")
+    var profilePictureState = mutableStateOf("")
 
     fun addNewCenter() {
         viewModelScope.launch(dispatcher) {
@@ -93,6 +112,62 @@ class CenterViewModel(
             }
             listCentersUseCase().collect { result ->
                 _centersResFlow.value = result
+            }
+        }
+    }
+
+    fun addNewCenterStaff() {
+        viewModelScope.launch(dispatcher) {
+            if (_actionResFlow.value !is Resource.Unspecified) {
+                _actionResFlow.value = Resource.Unspecified()
+                delay(timeMillis = 100)
+            }
+            addNewCenterStaffUseCase(
+                CenterStaff(
+                    firstName = firstNameState.value,
+                    lastName = lastNameState.value,
+                    centerId = centerIdState.longValue,
+                    email = emailState.value,
+                    phone = phoneState.value,
+                    profilePicture = profilePictureState.value,
+                ),
+            ).collect { result ->
+                _actionResFlow.value = result
+            }
+        }
+    }
+
+    fun updateCenterStaff() {
+        viewModelScope.launch(dispatcher) {
+            if (_actionResFlow.value !is Resource.Unspecified) {
+                _actionResFlow.value = Resource.Unspecified()
+                delay(timeMillis = 100)
+            }
+
+            updateCenterStaffUseCase(
+                CenterStaff(
+                    id = idState.longValue,
+                    firstName = firstNameState.value,
+                    lastName = lastNameState.value,
+                    centerId = centerIdState.longValue,
+                    email = emailState.value,
+                    phone = phoneState.value,
+                    profilePicture = profilePictureState.value,
+                ),
+            ).collect { result ->
+                _actionResFlow.value = result
+            }
+        }
+    }
+
+    fun listStaff() {
+        viewModelScope.launch(dispatcher) {
+            if (_centerStaffsResFlow.value !is Resource.Unspecified) {
+                _centerStaffsResFlow.value = Resource.Unspecified()
+                delay(timeMillis = 100)
+            }
+            listCenterStaffUseCase(selectedCenterIdState.longValue).collect { result ->
+                _centerStaffsResFlow.value = result
             }
         }
     }
