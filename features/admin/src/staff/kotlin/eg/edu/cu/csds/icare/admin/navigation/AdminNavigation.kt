2d747ad20712949bf7432d1f11c711fba6cc18a5
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-18 13:47:22 +0200

    feat: Implement navigation for Admin screen and related entities
    This commit implements the navigation logic for the Admin screen and its associated entities.
    - Added a new `adminRoute` function to handle navigation within the Admin feature module.
    - Implemented navigation actions for the following screens: Admin, NewClinic, EditClinic, NewCenter, EditCenter, NewPharmacy, EditPharmacy, NewDoctor, EditDoctor, NewClinicStaff, EditClinicStaff, NewPharmacist, EditPharmacist, NewCenterStaff, EditCenterStaff.
    - Handled navigation to entity-specific edit screens when clicking on a clinic, doctor, clinic staff, pharmacy, pharmacist, center, or center staff.
    - Implemented navigation to the appropriate creation screen for clinics, doctors, clinic staffs, pharmacies, pharmacists, centers, and center staffs when the FAB button is clicked based on the selected category and section.
    - Added logic to refresh the list of clinics, doctors, staffs, pharmacies, pharmacists, centers, and center staffs based on selected category and section.

---
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/navigation/AdminNavigation.kt
@@ -0,0 +1,152 @@
+package eg.edu.cu.csds.icare.admin.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import eg.edu.cu.csds.icare.admin.screen.AdminScreen
+import eg.edu.cu.csds.icare.admin.screen.center.CenterViewModel
+import eg.edu.cu.csds.icare.admin.screen.clinic.ClinicViewModel
+import eg.edu.cu.csds.icare.admin.screen.pharmacy.PharmacyViewModel
+import eg.edu.cu.csds.icare.core.ui.MainViewModel
+import eg.edu.cu.csds.icare.core.ui.navigation.Screen
+
+fun NavGraphBuilder.adminRoute(
+    mainViewModel: MainViewModel,
+    clinicViewModel: ClinicViewModel,
+    centerViewModel: CenterViewModel,
+    pharmacyViewModel: PharmacyViewModel,
+    onNavigationIconClicked: () -> Unit,
+    navigateToScreen: (Screen) -> Unit,
+    onError: suspend (Throwable?) -> Unit,
+) {
+    composable<Screen.Admin> {
+        AdminScreen(
+            mainViewModel = mainViewModel,
+            clinicViewModel = clinicViewModel,
+            pharmacyViewModel = pharmacyViewModel,
+            centerViewModel = centerViewModel,
+            onNavigationIconClicked = onNavigationIconClicked,
+            onFabClicked = {
+                when (mainViewModel.selectedCategoryTabIndex.intValue) {
+                    0 ->
+                        when (mainViewModel.selectedSectionTabIndex.intValue) {
+                            0 -> navigateToScreen(Screen.NewClinic)
+                            1 -> navigateToScreen(Screen.NewDoctor)
+                            2 -> navigateToScreen(Screen.NewClinicStaff)
+                        }
+
+                    1 ->
+                        when (mainViewModel.selectedSectionTabIndex.intValue) {
+                            0 -> navigateToScreen(Screen.NewPharmacy)
+                            1 -> navigateToScreen(Screen.NewPharmacist)
+                        }
+
+                    2 ->
+                        when (mainViewModel.selectedSectionTabIndex.intValue) {
+                            0 -> navigateToScreen(Screen.NewCenter)
+                            1 -> navigateToScreen(Screen.NewCenterStaff)
+                        }
+                }
+            },
+            onCategoryTabClicked = {
+                mainViewModel.selectedSectionTabIndex.intValue = 0
+                when (mainViewModel.selectedCategoryTabIndex.intValue) {
+                    0 -> clinicViewModel.listClinics()
+                    1 -> pharmacyViewModel.listPharmacies()
+                    2 -> centerViewModel.listCenters()
+                }
+            },
+            onSectionTabClicked = {
+                when (mainViewModel.selectedCategoryTabIndex.intValue) {
+                    0 ->
+                        when (mainViewModel.selectedSectionTabIndex.intValue) {
+                            0 -> clinicViewModel.listClinics()
+                            1 -> clinicViewModel.listDoctors()
+                            2 -> clinicViewModel.listStaffs()
+                        }
+
+                    1 ->
+                        when (mainViewModel.selectedSectionTabIndex.intValue) {
+                            0 -> pharmacyViewModel.listPharmacies()
+                            1 -> pharmacyViewModel.listPharmacists()
+                        }
+
+                    2 ->
+                        when (mainViewModel.selectedSectionTabIndex.intValue) {
+                            0 -> centerViewModel.listCenters()
+                            1 -> centerViewModel.listStaff()
+                        }
+                }
+            },
+            onClinicClicked = { clinic ->
+                clinicViewModel.selectedClinicState.value = clinic
+                navigateToScreen(Screen.EditClinic)
+            },
+            onDoctorClicked = { doctor ->
+                clinicViewModel.selectedDoctorState.value = doctor
+                navigateToScreen(Screen.EditDoctor)
+            },
+            onClinicStaffClicked = { staff ->
+                clinicViewModel.selectedClinicStaffState.value = staff
+                navigateToScreen(Screen.EditClinicStaff)
+            },
+            onPharmacyClicked = { pharmacy ->
+                pharmacyViewModel.selectedPharmacyState.value = pharmacy
+                navigateToScreen(Screen.EditPharmacy)
+            },
+            onPharmacistClicked = { pharmacist ->
+                pharmacyViewModel.selectedPharmacistState.value = pharmacist
+                navigateToScreen(Screen.EditPharmacist)
+            },
+            onCenterClicked = { center ->
+                centerViewModel.selectedCenterState.value = center
+                navigateToScreen(Screen.EditCenter)
+            },
+            onCenterStaffClicked = { staff ->
+                centerViewModel.selectedCenterStaffState.value = staff
+                navigateToScreen(Screen.EditCenterStaff)
+            },
+            onError = onError,
+        )
+    }
+
+    composable<Screen.NewClinic> {
+    }
+
+    composable<Screen.EditClinic> {
+    }
+
+    composable<Screen.NewCenter> {
+    }
+
+    composable<Screen.EditCenter> {
+    }
+
+    composable<Screen.NewPharmacy> {
+    }
+
+    composable<Screen.EditPharmacy> {
+    }
+    composable<Screen.NewDoctor> {
+    }
+
+    composable<Screen.EditDoctor> {
+    }
+
+    composable<Screen.NewClinicStaff> {
+    }
+
+    composable<Screen.EditClinicStaff> {
+    }
+
+    composable<Screen.NewPharmacist> {
+    }
+
+    composable<Screen.EditPharmacist> {
+    }
+
+    composable<Screen.NewCenterStaff> {
+    }
+
+    composable<Screen.EditCenterStaff> {
+    }
+}
