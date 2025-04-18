271ed7bf2acd9ff371a4e1d6f56dc90c8f313f0d
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-18 13:34:28 +0200

    feat: Migrate Doctor and Staff functionalities to ClinicViewModel
    This commit migrates the Doctor and Clinic Staff functionalities from `ClinicStaffViewModel` to `ClinicViewModel`.
    - Removed `ClinicStaffViewModel` as its functionalities have been migrated.
    - Added the ability to add, update, and list Doctors, as well as add, update, and list clinic staffs within `ClinicViewModel`.
    - Added new state holders to `ClinicViewModel` for tracking selected Clinic, Doctor, and ClinicStaff, as well as a state holder for expanded FAB.
    - Updated `ClinicViewModel` to accept new use cases for handling doctor and clinic staff operations.

--- features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/clinic/ClinicViewModel.kt
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/clinic/ClinicViewModel.kt
@@ -1,15 +1,24 @@
 package eg.edu.cu.csds.icare.admin.screen.clinic
 
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import eg.edu.cu.csds.icare.core.domain.model.Clinic
+import eg.edu.cu.csds.icare.core.domain.model.ClinicStaff
+import eg.edu.cu.csds.icare.core.domain.model.Doctor
 import eg.edu.cu.csds.icare.core.domain.model.Resource
 import eg.edu.cu.csds.icare.core.domain.usecase.clinic.AddNewClinic
 import eg.edu.cu.csds.icare.core.domain.usecase.clinic.ListClinics
 import eg.edu.cu.csds.icare.core.domain.usecase.clinic.UpdateClinic
+import eg.edu.cu.csds.icare.core.domain.usecase.clinic.staff.AddNewClinicStaff
+import eg.edu.cu.csds.icare.core.domain.usecase.clinic.staff.ListClinicStaff
+import eg.edu.cu.csds.icare.core.domain.usecase.clinic.staff.UpdateClinicStaff
+import eg.edu.cu.csds.icare.core.domain.usecase.doctor.AddNewDoctor
+import eg.edu.cu.csds.icare.core.domain.usecase.doctor.ListDoctors
+import eg.edu.cu.csds.icare.core.domain.usecase.doctor.UpdateDoctor
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -23,6 +32,12 @@ class ClinicViewModel(
     private val addNewClinicUseCase: AddNewClinic,
     private val updateClinicUseCase: UpdateClinic,
     private val listClinicsUseCase: ListClinics,
+    private val addNewDoctorUseCase: AddNewDoctor,
+    private val updateDoctorUseCase: UpdateDoctor,
+    private val listDoctorsUseCase: ListDoctors,
+    private val addNewClinicStaffUseCase: AddNewClinicStaff,
+    private val updateClinicStaffUseCase: UpdateClinicStaff,
+    private val listClinicStaffUseCase: ListClinicStaff,
 ) : ViewModel() {
     private val _actionResFlow =
         MutableStateFlow<Resource<Nothing?>>(Resource.Unspecified())
@@ -30,7 +45,17 @@ class ClinicViewModel(
     private val _clinicsResFlow =
         MutableStateFlow<Resource<List<Clinic>>>(Resource.Unspecified())
     val clinicsResFlow: StateFlow<Resource<List<Clinic>>> = _clinicsResFlow
+    private val _doctorsResFlow =
+        MutableStateFlow<Resource<List<Doctor>>>(Resource.Unspecified())
+    val doctorsResFlow: StateFlow<Resource<List<Doctor>>> = _doctorsResFlow
+    private val _clinicStaffsResFlow =
+        MutableStateFlow<Resource<List<ClinicStaff>>>(Resource.Unspecified())
+    val clinicStaffsResFlow: StateFlow<Resource<List<ClinicStaff>>> = _clinicStaffsResFlow
+    var selectedClinicState: MutableState<Clinic?> = mutableStateOf(null)
+    var selectedDoctorState: MutableState<Doctor?> = mutableStateOf(null)
+    var selectedClinicStaffState: MutableState<ClinicStaff?> = mutableStateOf(null)
 
+    var selectedClinicIdState = mutableLongStateOf(0)
     var idState = mutableLongStateOf(0)
     var nameState = mutableStateOf("")
     var typeState = mutableStateOf("")
@@ -38,7 +63,16 @@ class ClinicViewModel(
     var addressState = mutableStateOf("")
     var longitudeState = mutableDoubleStateOf(0.0)
     var latitudeState = mutableDoubleStateOf(0.0)
+    var firstNameState = mutableStateOf("")
+    var lastNameState = mutableStateOf("")
+    var clinicIdState = mutableLongStateOf(0)
+    var emailState = mutableStateOf("")
+    var specialityState = mutableStateOf("")
+    var fromTimeState = mutableLongStateOf(0)
+    var toTimeState = mutableLongStateOf(0)
+    var profilePictureState = mutableStateOf("")
     var isOpenState = mutableStateOf(false)
+    var expandedFab = mutableStateOf(true)
 
     fun addNewClinic() {
         viewModelScope.launch(dispatcher) {
@@ -93,6 +127,122 @@ class ClinicViewModel(
             }
             listClinicsUseCase().collect { result ->
                 _clinicsResFlow.value = result
+            }
+        }
+    }
+
+    fun addNewDoctor() {
+        viewModelScope.launch(dispatcher) {
+            if (_actionResFlow.value !is Resource.Unspecified) {
+                _actionResFlow.value = Resource.Unspecified()
+                delay(timeMillis = 100)
+            }
+            addNewDoctorUseCase(
+                Doctor(
+                    firstName = firstNameState.value,
+                    lastName = lastNameState.value,
+                    clinicId = clinicIdState.longValue,
+                    email = emailState.value,
+                    phone = phoneState.value,
+                    specialty = specialityState.value,
+                    fromTime = fromTimeState.longValue,
+                    toTime = toTimeState.longValue,
+                    profilePicture = profilePictureState.value,
+                ),
+            ).collect { result ->
+                _actionResFlow.value = result
+            }
+        }
+    }
+
+    fun updateDoctor() {
+        viewModelScope.launch(dispatcher) {
+            if (_actionResFlow.value !is Resource.Unspecified) {
+                _actionResFlow.value = Resource.Unspecified()
+                delay(timeMillis = 100)
+            }
+            updateDoctorUseCase(
+                Doctor(
+                    id = idState.longValue,
+                    firstName = firstNameState.value,
+                    lastName = lastNameState.value,
+                    clinicId = clinicIdState.longValue,
+                    email = emailState.value,
+                    phone = phoneState.value,
+                    specialty = specialityState.value,
+                    fromTime = fromTimeState.longValue,
+                    toTime = toTimeState.longValue,
+                    profilePicture = profilePictureState.value,
+                ),
+            ).collect { result ->
+                _actionResFlow.value = result
+            }
+        }
+    }
+
+    fun listDoctors() {
+        viewModelScope.launch(dispatcher) {
+            if (_doctorsResFlow.value !is Resource.Unspecified) {
+                _doctorsResFlow.value = Resource.Unspecified()
+                delay(timeMillis = 100)
+            }
+            listDoctorsUseCase().collect { result ->
+                _doctorsResFlow.value = result
+            }
+        }
+    }
+
+    fun addNewStaff() {
+        viewModelScope.launch(dispatcher) {
+            if (_actionResFlow.value !is Resource.Unspecified) {
+                _actionResFlow.value = Resource.Unspecified()
+                delay(timeMillis = 100)
+            }
+            addNewClinicStaffUseCase(
+                ClinicStaff(
+                    firstName = firstNameState.value,
+                    lastName = lastNameState.value,
+                    clinicId = clinicIdState.longValue,
+                    email = emailState.value,
+                    phone = phoneState.value,
+                    profilePicture = profilePictureState.value,
+                ),
+            ).collect { result ->
+                _actionResFlow.value = result
+            }
+        }
+    }
+
+    fun updateStaff() {
+        viewModelScope.launch(dispatcher) {
+            if (_actionResFlow.value !is Resource.Unspecified) {
+                _actionResFlow.value = Resource.Unspecified()
+                delay(timeMillis = 100)
+            }
+            updateClinicStaffUseCase(
+                ClinicStaff(
+                    id = idState.longValue,
+                    firstName = firstNameState.value,
+                    lastName = lastNameState.value,
+                    clinicId = clinicIdState.longValue,
+                    email = emailState.value,
+                    phone = phoneState.value,
+                    profilePicture = profilePictureState.value,
+                ),
+            ).collect { result ->
+                _actionResFlow.value = result
+            }
+        }
+    }
+
+    fun listStaffs() {
+        viewModelScope.launch(dispatcher) {
+            if (_clinicStaffsResFlow.value !is Resource.Unspecified) {
+                _clinicStaffsResFlow.value = Resource.Unspecified()
+                delay(timeMillis = 100)
+            }
+            listClinicStaffUseCase(selectedClinicIdState.longValue).collect { result ->
+                _clinicStaffsResFlow.value = result
             }
         }
     }
