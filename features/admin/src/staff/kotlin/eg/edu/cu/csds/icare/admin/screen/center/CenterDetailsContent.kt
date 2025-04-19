08db27aa1e7058c4083d1e0e650a9a57c5c59541
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-19 11:11:41 +0200

    feat: Add center details screen
    This commit introduces the `CenterDetailsContent` composable, which allows users to input and manage details for a medical center.
    
    The following changes were made:
    
    -   Created a new composable, `CenterDetailsContent`, to handle the form for center details.
    -   Implemented input fields for name, phone, and address.
    -   Integrated a dropdown menu to select the center type (lab or imaging).
    -   Added loading, success, and error states to reflect the operation status.
    -   Added a "Proceed" button to submit the entered details.
    -   Added previews for light and dark modes, with English and Arabic locales.
    -   Implemented input validation for the phone field.
    -   Implemented UI theming and styling.

---
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/center/CenterDetailsContent.kt
@@ -0,0 +1,330 @@
+package eg.edu.cu.csds.icare.admin.screen.center
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import eg.edu.cu.csds.icare.admin.R
+import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.ui.common.CenterTypeItem
+import eg.edu.cu.csds.icare.core.ui.theme.L_PADDING
+import eg.edu.cu.csds.icare.core.ui.theme.XL_PADDING
+import eg.edu.cu.csds.icare.core.ui.theme.Yellow500
+import eg.edu.cu.csds.icare.core.ui.theme.backgroundColor
+import eg.edu.cu.csds.icare.core.ui.theme.barBackgroundColor
+import eg.edu.cu.csds.icare.core.ui.theme.buttonBackgroundColor
+import eg.edu.cu.csds.icare.core.ui.theme.contentColor
+import eg.edu.cu.csds.icare.core.ui.theme.dropDownTextColor
+import eg.edu.cu.csds.icare.core.ui.theme.helveticaFamily
+import eg.edu.cu.csds.icare.core.ui.theme.textColor
+import eg.edu.cu.csds.icare.core.ui.view.AnimatedButton
+import eg.edu.cu.csds.icare.core.ui.R as CoreR
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun CenterDetailsContent(
+    name: String,
+    typesExpanded: Boolean,
+    type: Short,
+    phone: String,
+    address: String,
+    actionResource: Resource<Nothing?>,
+    onNameChanged: (String) -> Unit,
+    onTypesExpandedChange: (Boolean) -> Unit,
+    onTypesDismissRequest: () -> Unit,
+    onTypeClicked: (Short) -> Unit,
+    onPhoneChanged: (String) -> Unit,
+    onAddressChanged: (String) -> Unit,
+    onProceedButtonClicked: () -> Unit,
+    onSuccess: () -> Unit,
+    onError: suspend (Throwable?) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val types =
+        listOf(
+            CenterTypeItem(code = 1, textResId = R.string.lab_center),
+            CenterTypeItem(code = 2, textResId = R.string.imaging_center),
+        )
+    ConstraintLayout(
+        modifier =
+            modifier
+                .fillMaxSize()
+                .padding(bottom = XL_PADDING),
+    ) {
+        val (card, loading) = createRefs()
+        Surface(
+            modifier =
+                Modifier
+                    .constrainAs(card) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                        width = Dimension.fillToConstraints
+                        height = Dimension.fillToConstraints
+                    },
+        ) {
+            Column(
+                modifier =
+                    Modifier
+                        .fillMaxSize()
+                        .background(backgroundColor)
+                        .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                TextField(
+                    value = name,
+                    onValueChange = { onNameChanged(it) },
+                    label = {
+                        Text(
+                            text = stringResource(R.string.name),
+                            fontFamily = helveticaFamily,
+                            color = textColor,
+                        )
+                    },
+                    singleLine = true,
+                    modifier =
+                        Modifier
+                            .fillMaxWidth(fraction = 0.8f)
+                            .padding(top = L_PADDING),
+                    colors =
+                        TextFieldDefaults.colors(
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedContainerColor = Color.Transparent,
+                            disabledContainerColor = Color.Transparent,
+                            cursorColor = contentColor,
+                            focusedTextColor = textColor,
+                            focusedIndicatorColor = Yellow500,
+                            unfocusedIndicatorColor = Yellow500.copy(alpha = 0.38f),
+                        ),
+                    keyboardOptions =
+                        KeyboardOptions.Default.copy(
+                            autoCorrectEnabled = false,
+                            keyboardType = KeyboardType.Text,
+                            imeAction = ImeAction.Next,
+                        ),
+                )
+
+                ExposedDropdownMenuBox(
+                    modifier = Modifier.fillMaxWidth(fraction = 0.8f),
+                    expanded = typesExpanded,
+                    onExpandedChange = {
+                        onTypesExpandedChange(it)
+                    },
+                ) {
+                    OutlinedTextField(
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .menuAnchor(MenuAnchorType.PrimaryNotEditable),
+                        readOnly = true,
+                        value =
+                            types.firstOrNull { it.code == type }?.textResId?.let {
+                                stringResource(it)
+                            } ?: "",
+                        onValueChange = { },
+                        label = {
+                            Text(
+                                text = stringResource(R.string.type),
+                                color = dropDownTextColor,
+                            )
+                        },
+                        trailingIcon = {
+                            ExposedDropdownMenuDefaults.TrailingIcon(
+                                expanded = typesExpanded,
+                            )
+                        },
+                        colors =
+                            ExposedDropdownMenuDefaults.textFieldColors(
+                                focusedTextColor = Color.White,
+                                unfocusedTextColor = Color.White,
+                                focusedContainerColor = barBackgroundColor,
+                                unfocusedContainerColor = barBackgroundColor,
+                                unfocusedTrailingIconColor = Color.White,
+                                focusedTrailingIconColor = Color.White,
+                                focusedLabelColor = Color.White,
+                                unfocusedLabelColor = Color.White,
+                            ),
+                    )
+
+                    ExposedDropdownMenu(
+                        expanded = typesExpanded,
+                        onDismissRequest = {
+                            onTypesDismissRequest()
+                        },
+                    ) {
+                        types.forEach {
+                            DropdownMenuItem(
+                                text = {
+                                    Text(
+                                        text = stringResource(it.textResId),
+                                        color = dropDownTextColor,
+                                    )
+                                },
+                                onClick = { onTypeClicked(it.code) },
+                            )
+                        }
+                    }
+                }
+
+                TextField(
+                    value = phone,
+                    onValueChange = { if (it.length < 14) onPhoneChanged(it) },
+                    label = {
+                        Text(
+                            text = stringResource(R.string.phone_number),
+                            fontFamily = helveticaFamily,
+                            color = textColor,
+                        )
+                    },
+                    singleLine = true,
+                    modifier =
+                        Modifier
+                            .fillMaxWidth(fraction = 0.8f),
+                    colors =
+                        TextFieldDefaults.colors(
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedContainerColor = Color.Transparent,
+                            disabledContainerColor = Color.Transparent,
+                            cursorColor = contentColor,
+                            focusedTextColor = textColor,
+                            focusedIndicatorColor = Yellow500,
+                            unfocusedIndicatorColor = Yellow500.copy(alpha = 0.38f),
+                        ),
+                    keyboardOptions =
+                        KeyboardOptions.Default.copy(
+                            autoCorrectEnabled = false,
+                            keyboardType = KeyboardType.Phone,
+                            imeAction = ImeAction.Next,
+                        ),
+                )
+
+                TextField(
+                    value = address,
+                    onValueChange = { onAddressChanged(it) },
+                    label = {
+                        Text(
+                            text = stringResource(R.string.address),
+                            fontFamily = helveticaFamily,
+                            color = textColor,
+                        )
+                    },
+                    singleLine = true,
+                    modifier =
+                        Modifier
+                            .fillMaxWidth(fraction = 0.8f),
+                    colors =
+                        TextFieldDefaults.colors(
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedContainerColor = Color.Transparent,
+                            disabledContainerColor = Color.Transparent,
+                            cursorColor = contentColor,
+                            focusedTextColor = textColor,
+                            focusedIndicatorColor = Yellow500,
+                            unfocusedIndicatorColor = Yellow500.copy(alpha = 0.38f),
+                        ),
+                    keyboardOptions =
+                        KeyboardOptions.Default.copy(
+                            autoCorrectEnabled = false,
+                            keyboardType = KeyboardType.Text,
+                            imeAction = ImeAction.Next,
+                        ),
+                )
+
+                Spacer(modifier = Modifier.height(L_PADDING))
+
+                AnimatedButton(
+                    modifier =
+                        Modifier
+                            .fillMaxWidth(fraction = 0.6f),
+                    text = stringResource(CoreR.string.proceed),
+                    color = buttonBackgroundColor,
+                    onClick = { onProceedButtonClicked() },
+                )
+            }
+        }
+
+        when (actionResource) {
+            is Resource.Unspecified -> {}
+            is Resource.Loading ->
+                CircularProgressIndicator(
+                    modifier =
+                        Modifier
+                            .padding(XL_PADDING)
+                            .constrainAs(loading) {
+                                top.linkTo(parent.top, margin = L_PADDING)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                                bottom.linkTo(parent.bottom)
+                            },
+                )
+
+            is Resource.Success ->
+                LaunchedEffect(key1 = Unit) {
+                    onSuccess()
+                }
+
+            is Resource.Error ->
+                LaunchedEffect(key1 = true) {
+                    onError(actionResource.error)
+                }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, locale = "ar")
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES, locale = "ar")
+@Composable
+internal fun CenterDetailsContentPreview() {
+    Box(modifier = Modifier.background(backgroundColor)) {
+        CenterDetailsContent(
+            name = "",
+            typesExpanded = false,
+            type = 1,
+            phone = "",
+            address = "",
+            actionResource = Resource.Success(null),
+            onNameChanged = {},
+            onTypesExpandedChange = {},
+            onTypesDismissRequest = {},
+            onTypeClicked = {},
+            onPhoneChanged = {},
+            onAddressChanged = {},
+            onProceedButtonClicked = {},
+            onSuccess = {},
+            onError = {},
+        )
+    }
+}
