acc0e519bb88f83845950c858615734781d6c4da
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-19 14:40:34 +0200

    Refactor: Update API Response Formats Using SerialName
    -   Update `RegisteredResponse`: Change the `errorCode` field name to `status`.
    -   Update `AppointmentsResponse`: Add `data` field name for appointments list.
    -   Update `PharmaciesResponse`: Add `status` and `data` field names.
    -   Update `ClinicsResponse`: Add `status` and `data` field names.
    -   Update `CentersResponse`: Add `status` and `data` field names.
    -   Update `DoctorsResponse`: Add `status` and `data` field names.
    -   Use `@SerialName` annotation in all response classes for consistent field naming.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/RegisteredResponse.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/RegisteredResponse.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class RegisteredResponse(
-    @SerialName("errorCode")
+    @SerialName("status")
     val statusCode: Short,
     @SerialName("registered")
     val isRegistered: Boolean,
