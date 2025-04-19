dc018434a888d48ed17ba492608216c6d65869f7
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-19 14:37:11 +0200

    Refactor: Adjust Detekt's Cyclomatic Complexity Threshold and Update Clinic Model
    - Increased the `CyclomaticComplexMethod` threshold in Detekt's configuration from 15 to 20 to allow for more complex methods.
    - Modified the `Clinic` data class to include:
      -  `token`: A transient string field.
      -  `id`: Changed to be named "clinicID" in serialized data.
      -  `name`: Changed to be named "clinicName" in serialized data.
      -  `type`: Changed to be named "clinicType" in serialized data.
      - `address`: Changed to be named "clinicLocaltion" in serialized data.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/Clinic.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/Clinic.kt
@@ -1,13 +1,21 @@
 package eg.edu.cu.csds.icare.core.domain.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 data class Clinic(
+    @Transient
+    val token: String = "",
+    @SerialName("clinicID")
     val id: Long = 0,
+    @SerialName("clinicName")
     val name: String = "",
+    @SerialName("clinicType")
     val type: String = "",
     val phone: String = "",
+    @SerialName("clinicLocaltion")
     val address: String = "",
     val isOpen: Boolean = false,
 )
