0532bd031391b946787645ec0ba5674f21d4e9dd
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-19 09:34:05 +0200

    feat: Implement Edit Pharmacy Screen
    This commit introduces the `EditPharmacyScreen`, allowing users to modify existing pharmacy details.
    
    -   Added `EditPharmacyScreen` composable function for editing pharmacy information.
    -   Implemented a top app bar with a back navigation icon.
    -   Utilized `PharmacyDetailsContent` to display and handle input fields for name, phone, and address.
    -   Implemented logic to update the selected pharmacy's data upon changes in input fields.
    -   Integrated state management with `actionResFlow` to track the status of edit operation.
    -   Added visual elements like a yellow line separator and consistent background color.
    -   Added functionality for navigation and action handling (success/error/button clicks).

---
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/EditPharmacyScreen.kt
@@ -0,0 +1,122 @@
+package eg.edu.cu.csds.icare.admin.screen.pharmacy
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import eg.edu.cu.csds.icare.admin.R
+import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.ui.theme.XS_PADDING
+import eg.edu.cu.csds.icare.core.ui.theme.Yellow500
+import eg.edu.cu.csds.icare.core.ui.theme.backgroundColor
+import eg.edu.cu.csds.icare.core.ui.theme.barBackgroundColor
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun EditPharmacyScreen(
+    pharmacyViewModel: PharmacyViewModel,
+    onNavigationIconClicked: () -> Unit,
+    onProceedButtonClicked: () -> Unit,
+    onSuccess: () -> Unit,
+    onError: suspend (Throwable?) -> Unit,
+) {
+    val actionResource by pharmacyViewModel.actionResFlow
+        .collectAsStateWithLifecycle(initialValue = Resource.Unspecified())
+    var selectedPharmacy by pharmacyViewModel.selectedPharmacyState
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.edit_pharmacy)) },
+                colors =
+                    TopAppBarDefaults.topAppBarColors(
+                        containerColor = barBackgroundColor,
+                        navigationIconContentColor = Color.White,
+                        titleContentColor = Color.White,
+                        actionIconContentColor = Color.White,
+                    ),
+                navigationIcon = {
+                    IconButton(onClick = { onNavigationIconClicked() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null,
+                            tint = Color.White,
+                        )
+                    }
+                },
+            )
+        },
+    ) { paddingValues ->
+        Surface(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+        ) {
+            ConstraintLayout(
+                modifier =
+                    Modifier
+                        .background(backgroundColor)
+                        .fillMaxWidth(),
+            ) {
+                val (line, content) = createRefs()
+                Box(
+                    modifier =
+                        Modifier
+                            .constrainAs(line) {
+                                top.linkTo(parent.top)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                            }.background(Yellow500)
+                            .fillMaxWidth()
+                            .height(XS_PADDING),
+                )
+
+                selectedPharmacy?.let { pharmacy ->
+                    PharmacyDetailsContent(
+                        modifier =
+                            Modifier.constrainAs(content) {
+                                top.linkTo(line.bottom)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                                bottom.linkTo(parent.bottom)
+                                width = Dimension.fillToConstraints
+                                height = Dimension.fillToConstraints
+                            },
+                        name = pharmacy.name,
+                        phone = pharmacy.phone,
+                        address = pharmacy.address,
+                        actionResource = actionResource,
+                        onNameChanged = { selectedPharmacy = pharmacy.copy(name = it) },
+                        onPhoneChanged = { selectedPharmacy = pharmacy.copy(phone = it) },
+                        onAddressChanged = { selectedPharmacy = pharmacy.copy(address = it) },
+                        onProceedButtonClicked = { onProceedButtonClicked() },
+                        onSuccess = { onSuccess },
+                        onError = { onError(it) },
+                    )
+                }
+            }
+        }
+    }
+}
