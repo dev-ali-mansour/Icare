4b0c00c24c4de4c839249239530e5e688135f261
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-19 18:35:11 +0200

    Refactor: Update API endpoints and data model names
    - Updated API endpoints for centers, pharmacy, and imaging centers.
    - Renamed `POST` endpoints for `addNewPharmacy` and `updatePharmacy` to `pharmacyApi/addPharmacy`.
    - Renamed `POST` endpoints for `addNewCenter` and `updateCenter` to `imagingCentersApi/add_imaging_center`.
    - Changed `POST` endpoint for `listPharmacists` to `pharmacyApi/getPharmaciest`.
    - Changed `POST` endpoint for `fetchCenters` to `imagingCentersApi/get_imaging_centers`.
    - Updated data model field names in `LabImagingCenter` and `Pharmacy` for consistency and alignment with new endpoints:
      - `LabImagingCenter`: `id` is now `centerID`, `name` is now `centerName`, `type` is now `centerType`.
      - `Pharmacy`: `id` is now `pharmacyID`, `name` is now `pharmacyName`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/LabImagingCenter.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/LabImagingCenter.kt
@@ -1,5 +1,6 @@
 package eg.edu.cu.csds.icare.core.domain.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 
@@ -7,8 +8,11 @@ import kotlinx.serialization.Transient
 data class LabImagingCenter(
     @Transient
     val token: String = "",
+    @SerialName("centerID")
     val id: Long = 0,
+    @SerialName("centerName")
     val name: String = "",
+    @SerialName("centerType")
     val type: Short = 0,
     val phone: String = "",
     val address: String = "",
