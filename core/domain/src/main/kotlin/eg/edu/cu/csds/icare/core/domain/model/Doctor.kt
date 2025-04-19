ca7d322c806692ff71fe762ab1c9e4e468055562
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-19 20:01:54 +0200

    Refactor: Modify CenterStaff data class properties
    The CenterStaff data class properties have been updated to use SerialName for JSON serialization and deserialization,  and the type of the 'id' attribute has been changed to String, in addition to adding SerialName to the 'centerId' and 'phone' attributes.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/Doctor.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/Doctor.kt
@@ -2,6 +2,7 @@ package eg.edu.cu.csds.icare.core.domain.model
 
 import eg.edu.cu.csds.icare.core.domain.util.divide
 import eg.edu.cu.csds.icare.core.domain.util.getFormattedTime
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 
@@ -9,14 +10,19 @@ import kotlinx.serialization.Transient
 data class Doctor(
     @Transient
     val token: String = "",
-    val id: Long = 0,
+    @SerialName("doctorID")
+    val id: String = "",
+    @SerialName("fName")
     val firstName: String = "",
+    @SerialName("lName")
     val lastName: String = "",
     @Transient
     val name: String = "$firstName $lastName",
     val clinicId: Long = 0,
     val email: String = "",
+    @SerialName("phoneNumber")
     val phone: String = "",
+    @SerialName("specialization")
     val specialty: String = "",
     val fromTime: Long = System.currentTimeMillis(),
     val toTime: Long = fromTime.plus(other = 5 * 60 * 60 * 1000),
