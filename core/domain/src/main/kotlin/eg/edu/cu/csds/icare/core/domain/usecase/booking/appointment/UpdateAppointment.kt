e4ec4d082fca1eff80b95754c49ef1dafd1d8874
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-20 10:14:29 +0200

    Refactor: Rename RescheduleAppointment to UpdateAppointment
    The `RescheduleAppointment` class and its corresponding file have been renamed to `UpdateAppointment`. The `invoke` method now accepts an `Appointment` object instead of individual parameters. It now calls `updateAppointment` instead of `rescheduleAppointment` in the repository.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/booking/appointment/UpdateAppointment.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/booking/appointment/UpdateAppointment.kt
@@ -1,14 +1,12 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.booking.appointment
 
+import eg.edu.cu.csds.icare.core.domain.model.Appointment
 import eg.edu.cu.csds.icare.core.domain.model.Resource
 import eg.edu.cu.csds.icare.core.domain.repository.AppointmentsRepository
 import kotlinx.coroutines.flow.Flow
 
-class RescheduleAppointment(
+class UpdateAppointment(
     private val repository: AppointmentsRepository,
 ) {
-    operator fun invoke(
-        appointmentId: Long,
-        dateTime: Long,
-    ): Flow<Resource<Nothing?>> = repository.rescheduleAppointment(appointmentId, dateTime)
+    operator fun invoke(appointment: Appointment): Flow<Resource<Nothing?>> = repository.updateAppointment(appointment)
 }
