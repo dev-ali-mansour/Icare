f887c5cc3683103278b4765170f37b2f78e92eac
Thorayaa <you@example.comthoraya2005@gmail.com>
2025-04-20 11:06:48 +0200

    feat: Create My Appointments screen
    This commit introduces the `MyAppointmentsScreen` composable, which displays the patient's appointments. It includes a top app bar with a back button, and a content area that shows the appointments list. The screen fetches appointments using `AppointmentsViewModel` and handles user interactions such as rescheduling and canceling appointments.

---
+++ features/appointments/src/patient/kotlin/eg/edu/cu/csds/icare/appointment/screen/MyAppointmentsScreen.kt
@@ -0,0 +1,126 @@
+package eg.edu.cu.csds.icare.appointment.screen
+
+import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import eg.edu.cu.csds.icare.appointment.AppointmentsViewModel
+import eg.edu.cu.csds.icare.appointment.R
+import eg.edu.cu.csds.icare.core.domain.model.Appointment
+import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.ui.theme.XS_PADDING
+import eg.edu.cu.csds.icare.core.ui.theme.Yellow500
+import eg.edu.cu.csds.icare.core.ui.theme.backgroundColor
+import eg.edu.cu.csds.icare.core.ui.theme.barBackgroundColor
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyAppointmentsScreen(
+    appointmentsViewModel: AppointmentsViewModel,
+    onNavigationIconClicked: () -> Unit,
+    onReschedule: (Appointment) -> Unit,
+    onCancel: (Appointment) -> Unit,
+    onSuccess: () -> Unit,
+    onError: suspend (Throwable?) -> Unit,
+    context: Context = LocalContext.current,
+) {
+    val actionResource by appointmentsViewModel.actionResFlow
+        .collectAsStateWithLifecycle(initialValue = Resource.Unspecified())
+    val appointmentsResource by appointmentsViewModel.appointmentsResFlow.collectAsStateWithLifecycle()
+
+    LaunchedEffect(key1 = Unit) {
+        appointmentsViewModel.getPatientAppointments(context)
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.my_appointments)) },
+                colors =
+                    TopAppBarDefaults.topAppBarColors(
+                        containerColor = barBackgroundColor,
+                        navigationIconContentColor = Color.White,
+                        titleContentColor = Color.White,
+                        actionIconContentColor = Color.White,
+                    ),
+                navigationIcon = {
+                    IconButton(onClick = { onNavigationIconClicked() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null,
+                            tint = Color.White,
+                        )
+                    }
+                },
+            )
+        },
+    ) { paddingValues ->
+        Surface(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+        ) {
+            ConstraintLayout(
+                modifier =
+                    Modifier
+                        .background(backgroundColor)
+                        .fillMaxWidth(),
+            ) {
+                val (line, content) = createRefs()
+                Box(
+                    modifier =
+                        Modifier
+                            .constrainAs(line) {
+                                top.linkTo(parent.top)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                            }.background(Yellow500)
+                            .fillMaxWidth()
+                            .height(XS_PADDING),
+                )
+
+                MyAppointmentsContent(
+                    modifier =
+                        Modifier.constrainAs(content) {
+                            top.linkTo(line.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            bottom.linkTo(parent.bottom)
+                            width = Dimension.fillToConstraints
+                            height = Dimension.fillToConstraints
+                        },
+                    appointmentsRes = appointmentsResource,
+                    actionResource = actionResource,
+                    onReschedule = { onReschedule(it) },
+                    onCancel = { onCancel(it) },
+                    onSuccess = { onSuccess() },
+                    onError = { onError(it) },
+                )
+            }
+        }
+    }
+}
