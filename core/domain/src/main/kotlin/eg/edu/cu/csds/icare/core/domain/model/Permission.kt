504d1c2716d149a44eb9ab6c041e5f55b82ede76
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-23 18:56:25 +0200

    feat: Remove Permission Model and PermissionsHelper
    This commit removes the `Permission` model and `PermissionsHelper` since they are no longer needed. The changes include deleting `Permission.kt` and `PermissionsHelper.kt`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/Permission.kt
+++
@@ -1,9 +0,0 @@
-package eg.edu.cu.csds.icare.core.domain.model
-
-sealed class Permission(
-    val code: Short,
-) {
-    data object AdminPermission : Permission(1)
-
-    data object CreatePrescriptionPermission : Permission(2)
-}
