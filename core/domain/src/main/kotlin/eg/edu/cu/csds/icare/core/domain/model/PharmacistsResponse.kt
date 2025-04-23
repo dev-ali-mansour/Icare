6f791a0f1a5fceed00222fa9c486aa729268dfc0
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-23 19:08:11 +0200

    feat: Add SerialName for Pharmacists in PharmacistsResponse
    This commit adds the `@SerialName("data")` annotation to the `pharmacists` property within the `PharmacistsResponse` data class. This ensures correct JSON serialization and deserialization by mapping the `pharmacists` field to the "data" key in the JSON payload.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/PharmacistsResponse.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/PharmacistsResponse.kt
@@ -1,9 +1,11 @@
 package eg.edu.cu.csds.icare.core.domain.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class PharmacistsResponse(
     val statusCode: Short,
+    @SerialName("data")
     val pharmacists: List<Pharmacist>,
 )
