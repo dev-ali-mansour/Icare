b48e6f5ecdb9a4644be046764923c45268748964
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-25 14:21:50 +0300

    feat: Add detailed medical record fields
    This commit enhances the `MedicalRecord` data class with additional fields for detailed medical information.
    - Added fields for `gender`, `genderValue`, `chronicDiseases`, `currentMedications`, `allergies`, `pastSurgeries`, and `weight`.
    - Marked `genderValue` as transient to exclude it from serialization.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/MedicalRecord.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/MedicalRecord.kt
@@ -1,6 +1,7 @@
 package eg.edu.cu.csds.icare.core.domain.model
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 data class MedicalRecord(
@@ -8,5 +9,13 @@ data class MedicalRecord(
     val patientId: String = "",
     val patientName: String = "",
     val patientImage: String = "",
+    val gender: Char = 'M',
+    @Transient
+    val genderValue: String = "",
+    val chronicDiseases: String = "",
+    val currentMedications: String = "",
+    val allergies: String = "",
+    val pastSurgeries: String = "",
+    val weight: Double = 0.0,
     val consultations: List<Consultation> = emptyList(),
 )
