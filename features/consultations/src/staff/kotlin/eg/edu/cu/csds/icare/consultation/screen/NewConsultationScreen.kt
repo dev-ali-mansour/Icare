192f3f50fcfae724b818cedf7932eedd05d16e6f
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-25 19:26:54 +0300

    feat: Add new consultation screen
    This commit introduces the `NewConsultationScreen` for creating new consultations.
    - Added `NewConsultationScreen` composable to handle the new consultation creation flow.
    - Implemented the UI structure for the `NewConsultationScreen`, including a top app bar with a back button.
    - Implemented `ConsultationDetailsContent` to display the new consultation data and handle input fields.
    - Added logic for managing states of input fields, such as diagnosis, medications, lab tests, imaging tests, and follow-up date.
    - Added logic to handle drop-down state of pharmacies, lab centers, imaging centers.
    - Added logic for handling user interactions, including patient card click, diagnosis changes, and pharmacy/center selections.
    - Added call backs `onProceedButtonClicked`, `onSuccess`, `onError` to handle the success/error or proceed scenarios.
    - Added constraint layout for layout.

---
+++ features/consultations/src/staff/kotlin/eg/edu/cu/csds/icare/consultation/screen/NewConsultationScreen.kt
@@ -0,0 +1,176 @@
+package eg.edu.cu.csds.icare.consultation.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import eg.edu.cu.csds.icare.admin.R
+import eg.edu.cu.csds.icare.admin.screen.center.CenterViewModel
+import eg.edu.cu.csds.icare.admin.screen.pharmacy.PharmacyViewModel
+import eg.edu.cu.csds.icare.consultation.ConsultationViewModel
+import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.ui.theme.XS_PADDING
+import eg.edu.cu.csds.icare.core.ui.theme.Yellow500
+import eg.edu.cu.csds.icare.core.ui.theme.backgroundColor
+import eg.edu.cu.csds.icare.core.ui.theme.barBackgroundColor
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun NewConsultationScreen(
+    pharmacyViewModel: PharmacyViewModel,
+    centerViewModel: CenterViewModel,
+    consultationViewModel: ConsultationViewModel,
+    onNavigationIconClicked: () -> Unit,
+    onPatientCardClick: (String) -> Unit,
+    onProceedButtonClicked: () -> Unit,
+    onSuccess: () -> Unit,
+    onError: suspend (Throwable?) -> Unit,
+) {
+    val pharmaciesRes by pharmacyViewModel.pharmaciesResFlow.collectAsStateWithLifecycle()
+    val centersRes by centerViewModel.centersResFlow.collectAsStateWithLifecycle()
+    val actionResource by consultationViewModel.actionResFlow
+        .collectAsStateWithLifecycle(initialValue = Resource.Unspecified())
+    var pharmaciesExpanded by pharmacyViewModel.pharmaciesExpandedState
+    var labCentersExpanded by consultationViewModel.labCentersExpandedState
+    var imagingCentersExpanded by consultationViewModel.imagingCentersExpandedState
+    var appointment by consultationViewModel.appointmentState
+    var diagnosis by consultationViewModel.diagnosisState
+    var pharmacyId by consultationViewModel.pharmacyIdState
+    var medications by consultationViewModel.medicationsState
+    var labCenterId by consultationViewModel.labCenterIdState
+    var labTests by consultationViewModel.labTestsState
+    var imagingCenterId by consultationViewModel.imagingCenterIdState
+    var imagingTests by consultationViewModel.imagingTestsState
+    var followUpdDate by consultationViewModel.followUpdDateState
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.new_consultation)) },
+                colors =
+                    TopAppBarDefaults.topAppBarColors(
+                        containerColor = barBackgroundColor,
+                        navigationIconContentColor = Color.White,
+                        titleContentColor = Color.White,
+                        actionIconContentColor = Color.White,
+                    ),
+                navigationIcon = {
+                    IconButton(onClick = { onNavigationIconClicked() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null,
+                            tint = Color.White,
+                        )
+                    }
+                },
+            )
+        },
+    ) { paddingValues ->
+        Surface(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+        ) {
+            ConstraintLayout(
+                modifier =
+                    Modifier
+                        .background(backgroundColor)
+                        .fillMaxWidth(),
+            ) {
+                val (line, content) = createRefs()
+                Box(
+                    modifier =
+                        Modifier
+                            .constrainAs(line) {
+                                top.linkTo(parent.top)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                            }.background(Yellow500)
+                            .fillMaxWidth()
+                            .height(XS_PADDING),
+                )
+
+                ConsultationDetailsContent(
+                    modifier =
+                        Modifier.constrainAs(content) {
+                            top.linkTo(line.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            bottom.linkTo(parent.bottom)
+                            width = Dimension.fillToConstraints
+                            height = Dimension.fillToConstraints
+                        },
+                    pharmaciesRes = pharmaciesRes,
+                    centersRes = centersRes,
+                    readOnly = false,
+                    dateTime = System.currentTimeMillis(),
+                    appointment = appointment,
+                    diagnosis = diagnosis,
+                    pharmacyId = pharmacyId,
+                    pharmaciesExpanded = pharmaciesExpanded,
+                    medications = medications,
+                    prescriptionStatusId = 1,
+                    labCenterId = labCenterId,
+                    labCentersExpanded = labCentersExpanded,
+                    labTests = labTests,
+                    labTestStatusId = 1,
+                    imagingCenterId = imagingCenterId,
+                    imagingCentersExpanded = imagingCentersExpanded,
+                    imagingTests = imagingTests,
+                    imgTestStatusId = 1,
+                    followUpdDate = followUpdDate,
+                    actionResource = actionResource,
+                    onPatientCardClick = { onPatientCardClick(it) },
+                    onDiagnosisChanged = { diagnosis = it },
+                    onMedicationsChanged = { medications = it },
+                    onLabTestsChanged = { labTests = it },
+                    onImagingTestsChanged = { imagingTests = it },
+                    onPharmaciesExpandedChange = { pharmaciesExpanded = !pharmaciesExpanded },
+                    onPharmaciesDismissRequest = { pharmaciesExpanded = false },
+                    onPharmacyClicked = {
+                        pharmacyId = it
+                        pharmaciesExpanded = false
+                    },
+                    onLabCentersExpandedChange = { labCentersExpanded = !labCentersExpanded },
+                    onLabCentersDismissRequest = { labCentersExpanded = false },
+                    onLabCenterClicked = {
+                        labCenterId = it
+                        labCentersExpanded = false
+                    },
+                    onImagingCentersExpandedChange = { imagingCentersExpanded = !imagingCentersExpanded },
+                    onImagingCentersDismissRequest = { imagingCentersExpanded = false },
+                    onImagingCenterClicked = {
+                        imagingCenterId = it
+                        imagingCentersExpanded = false
+                    },
+                    onFollowUpDateChanged = { followUpdDate = it },
+                    onProceedButtonClicked = { onProceedButtonClicked() },
+                    onSuccess = { onSuccess() },
+                    onError = { onError(it) },
+                )
+            }
+        }
+    }
+}
