0ad2b60196d35dcdcc77e41732d37d25c847a453
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-25 19:27:53 +0300

    feat: Add MedicalRecord Screen
    This commit introduces the `MedicalRecord` screen, which displays medical record information.
    - Created the `MedicalRecordScreen` composable function.
    - Added the top app bar with navigation icon for going back.
    - Added a yellow line for UI styling.
    - Added `MedicalRecordContent` for the screen content.
    - Added string resource "medical record" to the `core` module.

---
+++ features/consultations/src/main/kotlin/eg/edu/cu/csds/icare/consultation/screen/MedicalRecordScreen.kt
@@ -0,0 +1,110 @@
+package eg.edu.cu.csds.icare.consultation.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import eg.edu.cu.csds.icare.consultation.ConsultationViewModel
+import eg.edu.cu.csds.icare.core.domain.model.Consultation
+import eg.edu.cu.csds.icare.core.ui.theme.XS_PADDING
+import eg.edu.cu.csds.icare.core.ui.theme.Yellow500
+import eg.edu.cu.csds.icare.core.ui.theme.backgroundColor
+import eg.edu.cu.csds.icare.core.ui.theme.barBackgroundColor
+import eg.edu.cu.csds.icare.core.ui.R as CoreR
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun MedicalRecordScreen(
+    consultationViewModel: ConsultationViewModel,
+    onNavigationIconClicked: () -> Unit,
+    onConsultationClicked: (Consultation) -> Unit,
+    onError: suspend (Throwable?) -> Unit,
+) {
+    val medicalRecordResource by consultationViewModel.medicalRecordResFlow.collectAsStateWithLifecycle()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = CoreR.string.medical_record)) },
+                colors =
+                    TopAppBarDefaults.topAppBarColors(
+                        containerColor = barBackgroundColor,
+                        navigationIconContentColor = Color.White,
+                        titleContentColor = Color.White,
+                        actionIconContentColor = Color.White,
+                    ),
+                navigationIcon = {
+                    IconButton(onClick = { onNavigationIconClicked() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null,
+                            tint = Color.White,
+                        )
+                    }
+                },
+            )
+        },
+    ) { paddingValues ->
+        Surface(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+        ) {
+            ConstraintLayout(
+                modifier =
+                    Modifier
+                        .background(backgroundColor)
+                        .fillMaxWidth(),
+            ) {
+                val (line, content) = createRefs()
+                Box(
+                    modifier =
+                        Modifier
+                            .constrainAs(line) {
+                                top.linkTo(parent.top)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                            }.background(Yellow500)
+                            .fillMaxWidth()
+                            .height(XS_PADDING),
+                )
+
+                MedicalRecordContent(
+                    modifier =
+                        Modifier.constrainAs(content) {
+                            top.linkTo(line.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            bottom.linkTo(parent.bottom)
+                            width = Dimension.fillToConstraints
+                            height = Dimension.fillToConstraints
+                        },
+                    medicalRecordRes = medicalRecordResource,
+                    onConsultationClicked = { onConsultationClicked(it) },
+                    onError = { onError(it) },
+                )
+            }
+        }
+    }
+}
