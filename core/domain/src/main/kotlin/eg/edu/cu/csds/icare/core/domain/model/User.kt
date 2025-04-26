364623eb73013ff9109044d097f2f0498855511f
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-26 12:29:48 +0300

    feat: Add SerialName for roleId and userId to User model
    This commit introduces `userId` to the `User` data class and adds `@SerialName("roleID")` annotation to `roleId` for custom JSON serialization using kotlinx.serialization. This ensures that the `roleId` field is serialized as "roleID" in the JSON output.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/User.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/User.kt
@@ -1,9 +1,12 @@
 package eg.edu.cu.csds.icare.core.domain.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class User(
+    val userId: String = "",
+    @SerialName("roleID")
     val roleId: Short = 6,
     val nationalId: String = "",
     val birthDate: Long = 0,
