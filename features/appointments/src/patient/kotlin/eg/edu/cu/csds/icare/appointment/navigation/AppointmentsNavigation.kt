aad49bc6dd1a55d5b91ea583c40c173ebe9d6dfb
Thorayaa <you@example.comthoraya2005@gmail.com>
2025-04-28 08:07:12 +0300

    feat: Add Doctor Profile screen and integrate it with appointments flow
    This commit introduces the `DoctorProfileScreen` and integrates it into the appointments navigation flow.
    - Added `DoctorProfileScreen` to display doctor details.
    - Updated navigation to include the `DoctorProfile` route.
    - Integrated with `AppointmentViewModel` to manage doctor and patient data.
    - Implemented `onProceedButtonClicked` to book appointments.
    - Implemented `onSuccess` to handle successful appointment bookings and navigate back.
    - Implemented `onCancel` to cancel appointment.
    - Implemented `onError` to handle errors during the booking process.

--- features/appointments/src/patient/kotlin/eg/edu/cu/csds/icare/appointment/navigation/AppointmentsNavigation.kt
+++ features/appointments/src/patient/kotlin/eg/edu/cu/csds/icare/appointment/navigation/AppointmentsNavigation.kt
@@ -2,11 +2,16 @@ package eg.edu.cu.csds.icare.appointment.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import eg.edu.cu.csds.icare.admin.screen.clinic.ClinicViewModel
 import eg.edu.cu.csds.icare.appointment.AppointmentViewModel
+import eg.edu.cu.csds.icare.appointment.screen.DoctorProfileScreen
 import eg.edu.cu.csds.icare.appointment.screen.MyAppointmentsScreen
+import eg.edu.cu.csds.icare.core.ui.MainViewModel
 import eg.edu.cu.csds.icare.core.ui.navigation.Screen
 
 fun NavGraphBuilder.appointmentsRoute(
+    mainViewModel: MainViewModel,
+    clinicViewModel: ClinicViewModel,
     appointmentsViewModel: AppointmentViewModel,
     onNavigationIconClicked: () -> Unit,
     onError: suspend (Throwable?) -> Unit,
@@ -16,12 +21,34 @@ fun NavGraphBuilder.appointmentsRoute(
             appointmentViewModel = appointmentsViewModel,
             onNavigationIconClicked = { onNavigationIconClicked() },
             onReschedule = {},
-            onCancel = {},
-            onSuccess = {},
+            onCancel = {
+                appointmentsViewModel.selectedAppointment.value = it
+                appointmentsViewModel.updateAppointment()
+            },
+            onSuccess = { onNavigationIconClicked() },
             onError = { onError(it) },
         )
     }
 
     composable<Screen.Doctors> {
+    }
+
+    composable<Screen.DoctorProfile> {
+        DoctorProfileScreen(
+            mainViewModel = mainViewModel,
+            clinicViewModel = clinicViewModel,
+            appointmentViewModel = appointmentsViewModel,
+            onNavigationIconClicked = { onNavigationIconClicked() },
+            onProceedButtonClicked = { doctorId, userId ->
+                appointmentsViewModel.doctorIdState.value = doctorId
+                appointmentsViewModel.patientIdState.value = userId
+                appointmentsViewModel.bookAppointment()
+            },
+            onSuccess = {
+                appointmentsViewModel.getPatientAppointments()
+                onNavigationIconClicked()
+            },
+            onError = { onError(it) },
+        )
     }
 }
