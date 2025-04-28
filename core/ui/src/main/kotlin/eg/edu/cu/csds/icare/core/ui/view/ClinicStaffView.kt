186a615fb64c299445794f15ea11a9feadde47f7
Thorayaa <you@example.comthoraya2005@gmail.com>
2025-04-28 12:04:09 +0300

    feat: Enhance Clinic Staff View with Card layout
    This commit refactors the `ClinicStaffView` to use a Material Design `Card` for improved visual presentation and interaction.
    - Replaced the previous layout with a `Card` that includes a profile picture, name, clinic ID, phone number, and email.
    - Implemented the card to be clickable.
    - Added `AsyncImage` to load and display the clinic staff's profile picture.
    - Improved text formatting with `MaterialTheme` typography and colors.
    - Added `Icons` for phone and email to enhance visual cues.
    - Updated the preview to reflect the changes.
    - The layout now uses `Row` and `Column` for better content arrangement within the card.
    - Added `Spacer` for spacing between elements within the `Row` and `Column`.
    - Added `CardDefaults.cardColors` to customize card color.
    - Added `CardDefaults.cardElevation` to customize card elevation.
    - Added `clip` with `CircleShape` to circle the user photo.
    - Added `helveticaFamily` for the font family.
    - Added `weight(1f)` to the column that contains the texts.

--- core/ui/src/main/kotlin/eg/edu/cu/csds/icare/core/ui/view/ClinicStaffView.kt
+++ core/ui/src/main/kotlin/eg/edu/cu/csds/icare/core/ui/view/ClinicStaffView.kt
@@ -1,50 +1,105 @@
 package eg.edu.cu.csds.icare.core.ui.view
 
 import android.content.res.Configuration
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import eg.edu.cu.csds.icare.core.domain.model.ClinicStaff
-import eg.edu.cu.csds.icare.core.ui.theme.DOUBLE_THICK_BORDER_STROKE_SIZE
-import eg.edu.cu.csds.icare.core.ui.theme.PaidColor
-import eg.edu.cu.csds.icare.core.ui.theme.S_PADDING
+import eg.edu.cu.csds.icare.core.ui.theme.M_PADDING
 import eg.edu.cu.csds.icare.core.ui.theme.XS_PADDING
 import eg.edu.cu.csds.icare.core.ui.theme.backgroundColor
-import eg.edu.cu.csds.icare.core.ui.theme.contentBackgroundColor
-import eg.edu.cu.csds.icare.core.ui.theme.itemBackgroundColor
+import eg.edu.cu.csds.icare.core.ui.theme.helveticaFamily
 
 @Composable
 fun ClinicStaffView(
-    modifier: Modifier = Modifier,
-    staff: ClinicStaff,
+    clinicStaff: ClinicStaff,
+    onClick: () -> Unit,
 ) {
-    Surface(
+    Card(
+        onClick = onClick,
         modifier =
             Modifier
                 .fillMaxWidth()
-                .alpha(alpha = if (!isSystemInDarkTheme()) 0.5f else 1f),
-        color = itemBackgroundColor,
-        shape = RoundedCornerShape(S_PADDING),
-        border = BorderStroke(DOUBLE_THICK_BORDER_STROKE_SIZE, PaidColor),
+                .padding(horizontal = M_PADDING, vertical = XS_PADDING),
+        colors =
+            CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+            ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        shape = MaterialTheme.shapes.medium,
     ) {
-        ConstraintLayout(
+        Row(
             modifier =
-                modifier
-                    .fillMaxWidth()
-                    .background(contentBackgroundColor),
+                Modifier
+                    .padding(M_PADDING),
+            verticalAlignment = Alignment.CenterVertically,
         ) {
-            Text(text = staff.name)
+            AsyncImage(
+                model = clinicStaff.profilePicture,
+                contentDescription = "",
+                modifier =
+                    Modifier
+                        .size(60.dp)
+                        .clip(CircleShape)
+                        .align(Alignment.Top),
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+            ) {
+                Text(
+                    text = "${clinicStaff.firstName} ${clinicStaff.lastName}",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontFamily = helveticaFamily,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                )
+                Spacer(modifier = Modifier.height(M_PADDING))
+                Text(
+                    text = clinicStaff.clinicId.toString(),
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontFamily = helveticaFamily,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.7f),
+                )
+                Spacer(modifier = Modifier.height(M_PADDING))
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(Icons.Default.Phone, contentDescription = null)
+                    Spacer(modifier = Modifier.width(XS_PADDING))
+                    Text(text = clinicStaff.phone)
+                }
+                Spacer(modifier = Modifier.height(M_PADDING))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(Icons.Default.Email, contentDescription = null)
+                    Spacer(modifier = Modifier.width(XS_PADDING))
+                    Text(text = clinicStaff.email)
+                }
+            }
         }
     }
 }
@@ -54,13 +109,27 @@ fun ClinicStaffView(
 @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Preview(locale = "ar", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-fun ClinicStaffViewPreview() {
-    Column(
-        modifier =
-            Modifier
-                .padding(XS_PADDING)
-                .background(color = backgroundColor),
-    ) {
-        ClinicStaffView(staff = ClinicStaff())
+fun ClinicStaffCardPreview() {
+    MaterialTheme {
+        Column(
+            modifier =
+                Modifier
+                    .padding(XS_PADDING)
+                    .background(color = backgroundColor),
+        ) {
+            ClinicStaffView(
+                clinicStaff =
+                    ClinicStaff(
+                        id = "101",
+                        firstName = "Mohammed Adel",
+                        lastName = "Ahmed",
+                        profilePicture = "",
+                        clinicId = 1,
+                        email = "mAhmed@example.com",
+                        phone = "0123456789",
+                    ),
+                onClick = {},
+            )
+        }
     }
 }
