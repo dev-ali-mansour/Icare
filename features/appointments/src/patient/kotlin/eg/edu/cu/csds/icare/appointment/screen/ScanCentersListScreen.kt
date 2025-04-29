7eab04aa161edb6b66ab84c7e2af3897a7b636fc
Thorayaa <you@example.comthoraya2005@gmail.com>
2025-04-29 12:02:42 +0300

    feat: Implement Scan Centers List Screen
    This commit introduces the `ScanCentersListScreen` to display a list of available centers for scanning.
    - Added `ScanCentersListScreen` composable for displaying the list.
    - Integrated pull-to-refresh functionality.
    - Added a top app bar with a back button.
    - Added a loading indicator.
    - Displayed the center list using `ScanCentersListContent`.
    - Implement search and clear callbacks.
    - Implemented error handling callback.
    - Added `CenterViewModel` to handle data.

---
+++ features/appointments/src/patient/kotlin/eg/edu/cu/csds/icare/appointment/screen/ScanCentersListScreen.kt
@@ -0,0 +1,142 @@
+package eg.edu.cu.csds.icare.appointment.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
+import androidx.compose.material3.pulltorefresh.pullToRefresh
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import eg.edu.cu.csds.icare.admin.screen.center.CenterViewModel
+import eg.edu.cu.csds.icare.core.ui.theme.XL_PADDING
+import eg.edu.cu.csds.icare.core.ui.theme.XS_PADDING
+import eg.edu.cu.csds.icare.core.ui.theme.Yellow500
+import eg.edu.cu.csds.icare.core.ui.theme.backgroundColor
+import eg.edu.cu.csds.icare.core.ui.theme.barBackgroundColor
+import eg.edu.cu.csds.icare.core.ui.R as CoreR
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScanCentersListScreen(
+    centerViewModel: CenterViewModel,
+    onNavigationIconClicked: () -> Unit,
+    onSearch: () -> Unit,
+    onClear: () -> Unit,
+    onError: suspend (Throwable?) -> Unit,
+) {
+    val centerResource by centerViewModel.centersResFlow.collectAsStateWithLifecycle()
+    var searchQuery: String by centerViewModel.searchQueryState
+    var isRefreshing by remember { mutableStateOf(false) }
+    val state = rememberPullToRefreshState()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(CoreR.string.appointment_booking)) },
+                colors =
+                    TopAppBarDefaults.topAppBarColors(
+                        containerColor = barBackgroundColor,
+                        navigationIconContentColor = Color.White,
+                        titleContentColor = Color.White,
+                        actionIconContentColor = Color.White,
+                    ),
+                navigationIcon = {
+                    IconButton(onClick = { onNavigationIconClicked() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null,
+                            tint = Color.White,
+                        )
+                    }
+                },
+            )
+        },
+    ) { paddingValues ->
+        Surface(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .pullToRefresh(
+                        state = state,
+                        isRefreshing = isRefreshing,
+                        onRefresh = {
+                            centerViewModel.listLabCenters()
+                        },
+                    ).padding(paddingValues),
+        ) {
+            ConstraintLayout(
+                modifier =
+                    Modifier
+                        .background(backgroundColor)
+                        .fillMaxSize(),
+            ) {
+                val (line, content, refresh) = createRefs()
+
+                Box(
+                    modifier =
+                        Modifier
+                            .constrainAs(line) {
+                                top.linkTo(parent.top)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                            }.background(Yellow500)
+                            .fillMaxWidth()
+                            .height(XS_PADDING),
+                )
+
+                ScanCentersListContent(
+                    modifier =
+                        Modifier.constrainAs(content) {
+                            top.linkTo(line.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            bottom.linkTo(parent.bottom)
+                            width = Dimension.fillToConstraints
+                            height = Dimension.fillToConstraints
+                        },
+                    centersRes = centerResource,
+                    searchQuery = searchQuery,
+                    showLoading = { isRefreshing = it },
+                    onSearchQueryChange = { searchQuery = it },
+                    onSearch = { onSearch() },
+                    onClear = { onClear() },
+                    onError = { onError(it) },
+                )
+
+                Indicator(
+                    modifier =
+                        Modifier.constrainAs(refresh) {
+                            top.linkTo(parent.top, margin = XL_PADDING)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        },
+                    isRefreshing = isRefreshing,
+                    state = state,
+                )
+            }
+        }
+    }
+}
