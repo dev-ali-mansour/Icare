f5cf032edcb25ae0f04aae913a8977725a738bb5
Ali Mansour <dev.ali.mansour@gmail.com>
2025-04-29 07:32:39 +0300

    feat: Add pull-to-refresh and success dialog to EditPharmacyScreen
    This commit introduces pull-to-refresh functionality and a success dialog to the `EditPharmacyScreen`.
    - Added `PullToRefresh` composable for pull-to-refresh functionality.
    - Added a `SuccessesDialog` to display a success message.
    - Added `isRefreshing` state to handle pull-to-refresh state.
    - Added `showSuccessDialog` state to manage success dialog visibility.
    - Added a `CoroutineScope` to manage success dialog timing.
    - When the pharmacy edit is successful, a success dialog will be shown for 2 seconds.
    - Added an indicator to indicate if the refresh is happening.

--- features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/EditPharmacyScreen.kt
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/EditPharmacyScreen.kt
@@ -16,8 +16,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
+import androidx.compose.material3.pulltorefresh.pullToRefresh
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,6 +35,9 @@ import eg.edu.cu.csds.icare.core.ui.theme.XS_PADDING
 import eg.edu.cu.csds.icare.core.ui.theme.Yellow500
 import eg.edu.cu.csds.icare.core.ui.theme.backgroundColor
 import eg.edu.cu.csds.icare.core.ui.theme.barBackgroundColor
+import eg.edu.cu.csds.icare.core.ui.view.SuccessesDialog
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -44,6 +51,10 @@ internal fun EditPharmacyScreen(
     val actionResource by pharmacyViewModel.actionResFlow
         .collectAsStateWithLifecycle(initialValue = Resource.Unspecified())
     var selectedPharmacy by pharmacyViewModel.selectedPharmacyState
+    var showSuccessDialog by pharmacyViewModel.showSuccessDialog
+    var isRefreshing by pharmacyViewModel.isRefreshing
+    val state = rememberPullToRefreshState()
+    val scope = rememberCoroutineScope()
 
     Scaffold(
         topBar = {
@@ -72,6 +83,7 @@ internal fun EditPharmacyScreen(
             modifier =
                 Modifier
                     .fillMaxSize()
+                    .pullToRefresh(state = state, isRefreshing = isRefreshing, onRefresh = {})
                     .padding(paddingValues),
         ) {
             ConstraintLayout(
@@ -80,7 +92,8 @@ internal fun EditPharmacyScreen(
                         .background(backgroundColor)
                         .fillMaxWidth(),
             ) {
-                val (line, content) = createRefs()
+                val (refresh, line, content) = createRefs()
+
                 Box(
                     modifier =
                         Modifier
@@ -108,13 +121,33 @@ internal fun EditPharmacyScreen(
                         phone = pharmacy.phone,
                         address = pharmacy.address,
                         actionResource = actionResource,
+                        showLoading = { isRefreshing = it },
                         onNameChanged = { selectedPharmacy = pharmacy.copy(name = it) },
                         onPhoneChanged = { selectedPharmacy = pharmacy.copy(phone = it) },
                         onAddressChanged = { selectedPharmacy = pharmacy.copy(address = it) },
                         onProceedButtonClicked = { onProceedButtonClicked() },
-                        onSuccess = { onSuccess() },
+                        onSuccess = {
+                            scope.launch {
+                                showSuccessDialog = true
+                                delay(timeMillis = 2000)
+                                showSuccessDialog = false
+                                onSuccess()
+                            }
+                        },
                         onError = { onError(it) },
                     )
+
+                    Indicator(
+                        modifier =
+                            Modifier.constrainAs(refresh) {
+                                top.linkTo(parent.top)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                            },
+                        isRefreshing = isRefreshing,
+                        state = state,
+                    )
+                    if (showSuccessDialog) SuccessesDialog {}
                 }
             }
         }
