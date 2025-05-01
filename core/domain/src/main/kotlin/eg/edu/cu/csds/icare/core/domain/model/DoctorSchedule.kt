c53803046849d11c3e70b365ef01fc1d607dcdd4
Ali Mansour <dev.ali.mansour@gmail.com>
2025-05-01 17:20:22 +0300

    feat: Enhance DoctorSchedule and add appointment strings
    This commit introduces enhancements to the `DoctorSchedule` data class and adds new string resources for appointment related views.
    - Added `rating`, `fromTime`, and `toTime` fields to the `DoctorSchedule` data class.
    - Added `no_appointments_data` string in both English and Arabic in the `appointments` module.
    - Modified the arabic `no_doctors_available` string in the `appointments` module.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/DoctorSchedule.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/DoctorSchedule.kt
@@ -7,6 +7,9 @@ data class DoctorSchedule(
     val totalPatients: Long = 0,
     val confirmed: Long = 0,
     val price: Double = 0.0,
+    val rating: Double = 0.0,
+    val fromTime: Long = System.currentTimeMillis(),
+    val toTime: Long = System.currentTimeMillis().plus(other = 5 * 60 * 60 * 60 * 1000),
     val availableSlots: Short = 0,
     val appointments: List<Appointment>,
 )
