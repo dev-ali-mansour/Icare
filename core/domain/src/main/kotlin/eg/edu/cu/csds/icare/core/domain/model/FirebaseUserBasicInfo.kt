704d510e8bb50c7ae045ea9e377d43d4d3f282fe
Ali Mansour <dev.ali.mansour@gmail.com>
2025-05-01 13:29:56 +0300

    refactor: Remove FirebaseUserBasicInfo and FirebaseUserExt
    This commit removes the `FirebaseUserBasicInfo` data class and the `FirebaseUserExt` utility, as they are no longer needed.
    - Removed the `FirebaseUserBasicInfo` data class.
    - Removed the `FirebaseUserExt` utility that provided an extension to retrieve `FirebaseUserBasicInfo`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/FirebaseUserBasicInfo.kt
+++
@@ -1,9 +0,0 @@
-package eg.edu.cu.csds.icare.core.domain.model
-
-import android.net.Uri
-
-data class FirebaseUserBasicInfo(
-    val displayName: String?,
-    val email: String?,
-    val photoUrl: Uri?,
-)
