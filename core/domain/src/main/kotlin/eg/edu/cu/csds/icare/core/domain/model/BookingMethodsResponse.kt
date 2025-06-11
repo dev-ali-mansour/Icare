18dd937e68d759e1149266fdc34a4606bfe27802
Ali Mansour <dev.ali.mansour@gmail.com>
2025-06-11 11:49:32 +0300

    refactor: Remove BookingMethod related files
    This commit removes the following files:
    - `core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/BookingMethod.kt`
    - `data/src/main/kotlin/eg/edu/cu/csds/icare/data/local/db/entity/BookingMethodEntity.kt`
    - `core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/BookingMethodsResponse.kt`
    
    These files were related to the `BookingMethod` domain model, its corresponding database entity, and the API response structure for fetching booking methods.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/BookingMethodsResponse.kt
+++
@@ -1,9 +0,0 @@
-package eg.edu.cu.csds.icare.core.domain.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class BookingMethodsResponse(
-    val statusCode: Short,
-    val methods: List<BookingMethod>,
-)
