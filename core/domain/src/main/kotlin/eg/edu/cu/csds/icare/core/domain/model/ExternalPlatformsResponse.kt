ecf12ffc7590789b05493a2794d5a21432265b09
Ali Mansour <dev.ali.mansour@gmail.com>
2025-06-11 11:48:54 +0300

    refactor: Remove ExternalPlatform and ExternalPlatformsResponse models
    This commit removes the `ExternalPlatform` and `ExternalPlatformsResponse` data classes from the `core.domain.model` package.
    
    - Deleted `ExternalPlatform.kt`
    - Deleted `ExternalPlatformsResponse.kt`

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/ExternalPlatformsResponse.kt
+++
@@ -1,9 +0,0 @@
-package eg.edu.cu.csds.icare.core.domain.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ExternalPlatformsResponse(
-    val statusCode: Short,
-    val platforms: List<ExternalPlatform>,
-)
