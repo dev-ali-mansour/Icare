7bc774e40727ec3bb7ecf889e58bb39d28055905
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-31 23:20:24 +0300

    refactor: Remove LoginResponse.kt.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/model/LoginResponse.kt
+++
@@ -1,9 +0,0 @@
-package eg.edu.cu.csds.icare.core.domain.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class LoginResponse(
-    val statusCode: Short = 0,
-    val user: User = User(),
-)
