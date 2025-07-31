f6ac08c47069886dfba53044f2d5547036819e92
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-31 13:40:03 +0300

    chore: Update SDK versions to 36
    The `TARGET_SDK_VERSION` and `COMPILE_SDK_VERSION` in `buildSrc/src/main/kotlin/build/BuildConfig.kt` have been updated from 35 to 36.

--- buildSrc/src/main/kotlin/build/BuildConfig.kt
+++ buildSrc/src/main/kotlin/build/BuildConfig.kt
@@ -3,6 +3,6 @@ package build
 object BuildConfig {
     const val APP_ID = "eg.edu.cu.csds.icare"
     const val MIN_SDK_VERSION = 23
-    const val TARGET_SDK_VERSION = 35
-    const val COMPILE_SDK_VERSION = 35
+    const val TARGET_SDK_VERSION = 36
+    const val COMPILE_SDK_VERSION = 36
 }
