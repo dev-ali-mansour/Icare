77b607a844eab8ada6577f66c5637be351af307d
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-01 23:23:08 +0300

    Refactor: Rename Register and SignInWithEmailAndPassword to SignUpUseCase and SignInUseCase, respectively

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/auth/SignInWithGoogleUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/auth/SignInWithGoogleUseCase.kt
@@ -5,7 +5,7 @@ import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.AuthRepository
 import kotlinx.coroutines.flow.Flow
 
-class SignInWithGoogle(
+class SignInWithGoogleUseCase(
     private val repository: AuthRepository,
 ) {
     operator fun invoke(token: String): Flow<Result<Unit, DataError.Remote>> = repository.signInWithGoogle(token)
--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/auth/SignUpUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/auth/SignUpUseCase.kt
@@ -5,7 +5,7 @@ import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.AuthRepository
 import kotlinx.coroutines.flow.Flow
 
-class Register(
+class SignUpUseCase(
     private val repository: AuthRepository,
 ) {
     operator fun invoke(
