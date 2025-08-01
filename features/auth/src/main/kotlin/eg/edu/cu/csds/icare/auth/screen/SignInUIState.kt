52008feb8f8c770e9f8280d30e43ef8b01d6f651
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-01 22:51:35 +0300

    Refactor: Rename LoginUIState to SignInUIState
    This commit renames the `LoginUIState` data class and its properties to `SignInUIState` for consistency with sign-in naming conventions.
    
    - `LoginUIState` is renamed to `SignInUIState`.
    - `loginSuccess` property is renamed to `signInSuccess`.
    - `googleLoginIntent` property is renamed to `googleSignInIntent`.

--- features/auth/src/main/kotlin/eg/edu/cu/csds/icare/auth/screen/SignInUIState.kt
+++ features/auth/src/main/kotlin/eg/edu/cu/csds/icare/auth/screen/SignInUIState.kt
@@ -3,12 +3,12 @@ package eg.edu.cu.csds.icare.auth.screen
 import android.content.Intent
 import eg.edu.cu.csds.icare.core.ui.util.UiText
 
-data class LoginUIState(
+data class SignInUIState(
     val isLoading: Boolean = false,
-    val loginSuccess: Boolean = false,
+    val signInSuccess: Boolean = false,
     val email: String = "",
     val password: String = "",
     val isPasswordVisible: Boolean = false,
-    val googleLoginIntent: Intent? = null,
+    val googleSignInIntent: Intent? = null,
     val errorMessage: UiText? = null,
 )
