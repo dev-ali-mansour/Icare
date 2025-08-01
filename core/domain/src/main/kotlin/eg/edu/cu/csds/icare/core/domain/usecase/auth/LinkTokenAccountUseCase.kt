37b508d9d204816138e027c778b953e375d39a66
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-01 22:40:52 +0300

    Refactor: Rename LinkTokenAccount to LinkTokenAccountUseCase and update return type
    This commit renames the `LinkTokenAccount` class to `LinkTokenAccountUseCase` in `core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/auth/`.
    
    Additionally, the return type of the `invoke` operator function within this class has been changed:
    - From: `Flow<Resource<Nothing?>>`
    - To: `Flow<Result<Unit, DataError.Remote>>`
    
    This change aligns the use case with the updated `Result` type for handling success or `DataError.Remote`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/auth/LinkTokenAccountUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/auth/LinkTokenAccountUseCase.kt
@@ -1,14 +1,15 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.auth
 
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.DataError
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.AuthRepository
 import kotlinx.coroutines.flow.Flow
 
-class LinkTokenAccount(
+class LinkTokenAccountUseCase(
     private val repository: AuthRepository,
 ) {
     operator fun invoke(
         providerId: String,
         token: String,
-    ): Flow<Resource<Nothing?>> = repository.linkTokenAccount(providerId, token)
+    ): Flow<Result<Unit, DataError.Remote>> = repository.linkTokenAccount(providerId, token)
 }
