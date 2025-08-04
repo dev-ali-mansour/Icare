bf9d97b7a078dfaeb3b1ecfa0b37b828dc850655
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-04 18:27:47 +0300

    feat: Add ProjectExtensions for VersionCatalog and Secrets
    This commit introduces a new file `ProjectExtensions.kt` within the `build-logic/convention/src/main/kotlin/dev/alimansour/shared/plugins` directory.
    
    The file includes:
    - An extension property `libs` for `Project` to easily access the `libs` version catalog.
    - An extension function `getSecret(key: String)` for `Project` to retrieve values from `local.properties` or environment variables.
    
    These extensions aim to simplify build script configurations by providing convenient access to version catalogs and secrets.

---
+++ build-logic/convention/src/main/kotlin/dev/alimansour/shared/plugins/ProjectExtensions.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright [2025] [Ali Mansour]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This file contains modified code originally from the Now in Android project by Google.
+ * Original source: https://github.com/android/nowinandroid
+ */
+
+package dev.alimansour.shared.plugins
+
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.getByType
+import java.util.Properties
+
+val Project.libs
+    get(): VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+fun Project.getSecret(key: String): String {
+    val localProperties =
+        Properties().apply {
+            val propertiesFile = rootProject.file("local.properties")
+            if (propertiesFile.exists()) {
+                propertiesFile.inputStream().use { load(it) }
+            }
+        }
+    return localProperties.getProperty(key) ?: System.getenv(key)
+}
