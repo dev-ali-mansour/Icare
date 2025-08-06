42e82201baf6f5b0a168ec0308430ab2e5feea11
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-06 08:22:38 +0300

    chore: Update core/domain module to use JVM library convention plugin
    The `core/domain/build.gradle.kts` file has been updated to use the JVM library convention plugin.
    
    - Removed `libs.plugins.convention.android.library`, `libs.plugins.convention.android.library.jacoco`, and `libs.plugins.convention.koin` plugins.
    - Added `libs.plugins.convention.jvm.library` plugin.
    - Removed the `android` block and its `namespace` configuration as it's no longer an Android library.

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,9 +1,3 @@
 plugins {
-    alias(libs.plugins.convention.android.library)
-    alias(libs.plugins.convention.android.library.jacoco)
-    alias(libs.plugins.convention.koin)
-}
-
-android {
-    namespace = "eg.edu.cu.csds.icare.core.domain"
+    alias(libs.plugins.convention.jvm.library)
 }
