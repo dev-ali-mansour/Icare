080ec841fb96fa62e3bbd4dd6b791d980547478b
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-07 17:19:28 +0300

    feat: Create SignUpSingleEvent for patient auth
    This commit introduces a new sealed interface `SignUpSingleEvent.kt` within the patient authentication feature.
    
    This interface defines the possible single events that can occur during the sign-up process:
    - `LoginSuccess`: Indicates a successful sign-up and login.
    - `ShowInfo`: Represents an informational message to be displayed to the user, taking a `UiText` parameter for the message content.
    - `ShowError`: Represents an error message to be displayed, also taking a `UiText` parameter for the message content.

---
+++ features/auth/src/patient/kotlin/eg/edu/cu/csds/icare/auth/screen/signup/SignUpSingleEvent.kt
@@ -0,0 +1,15 @@
+package eg.edu.cu.csds.icare.auth.screen.signup
+
+import eg.edu.cu.csds.icare.core.ui.util.UiText
+
+sealed interface SignUpSingleEvent {
+    object LoginSuccess : SignUpSingleEvent
+
+    data class ShowInfo(
+        val message: UiText,
+    ) : SignUpSingleEvent
+
+    data class ShowError(
+        val message: UiText,
+    ) : SignUpSingleEvent
+}
