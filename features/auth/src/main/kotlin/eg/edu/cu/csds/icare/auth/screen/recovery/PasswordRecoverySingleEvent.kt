0e3dc8a770fd6e409f7b8ed3a99d4a6accd220ba
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-07 20:34:21 +0300

    refactor: Use UiText for PasswordRecoverySingleEvent messages
    This commit updates the `PasswordRecoverySingleEvent` sealed interface to use `UiText` for its message properties.
    
    - The `ShowInfo` data class has been added, which includes a `message` property of type `UiText`.
    - The `ShowError` data class's `message` property type has been changed from `String` to `UiText`.

--- features/auth/src/main/kotlin/eg/edu/cu/csds/icare/auth/screen/recovery/PasswordRecoverySingleEvent.kt
+++ features/auth/src/main/kotlin/eg/edu/cu/csds/icare/auth/screen/recovery/PasswordRecoverySingleEvent.kt
@@ -1,9 +1,15 @@
 package eg.edu.cu.csds.icare.auth.screen.recovery
 
+import eg.edu.cu.csds.icare.core.ui.util.UiText
+
 sealed interface PasswordRecoverySingleEvent {
     object RecoverySuccess : PasswordRecoverySingleEvent
 
+    data class ShowInfo(
+        val message: UiText,
+    ) : PasswordRecoverySingleEvent
+
     data class ShowError(
-        val message: String,
+        val message: UiText,
     ) : PasswordRecoverySingleEvent
 }
