f18df015ad61c354405572e1f28a02a8bb1e8e23
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-07 14:52:38 +0300

    refactor: Relocate SignInUIState and remove unused properties
    The `SignInUIState.kt` file has been moved from `features/auth/src/main/kotlin/eg/edu/cu/csds/icare/auth/screen` to `features/auth/src/main/kotlin/eg/edu/cu/csds/icare/auth/screen/signin`.
    
    Within the `SignInUIState` data class, the `signInSuccess` and `errorMessage` properties have been removed as they are no longer used. The import for `eg.edu.cu.csds.icare.core.ui.util.UiText` has also been removed accordingly.

--- features/auth/src/main/kotlin/eg/edu/cu/csds/icare/auth/screen/signin/SignInUIState.kt
+++ features/auth/src/main/kotlin/eg/edu/cu/csds/icare/auth/screen/signin/SignInUIState.kt
@@ -1,13 +1,9 @@
-package eg.edu.cu.csds.icare.auth.screen
-
-import eg.edu.cu.csds.icare.core.ui.util.UiText
+package eg.edu.cu.csds.icare.auth.screen.signin
 
 data class SignInUIState(
     val isLoading: Boolean = false,
-    val signInSuccess: Boolean = false,
     val email: String = "",
     val password: String = "",
     val isPasswordVisible: Boolean = false,
     val googleSignInToken: String = "",
-    val errorMessage: UiText? = null,
 )
