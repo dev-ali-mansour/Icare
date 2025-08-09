72665f082de77ef0a84bec3c16128613b9ecd07d
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-09 15:32:11 +0300

    feat: Add `UnlinkGoogleAccountUseCase`
    This commit introduces a new use case, `UnlinkGoogleAccountUseCase`, within the `core.domain.usecase.auth` package.
    
    This use case interacts with the `AuthRepository` to provide a flow for unlinking a Google account, returning a `Result` that indicates either success (`Unit`) or a `DataError.Remote`.

---
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/auth/UnlinkGoogleAccountUseCase.kt
@@ -0,0 +1,12 @@
+package eg.edu.cu.csds.icare.core.domain.usecase.auth
+
+import eg.edu.cu.csds.icare.core.domain.model.DataError
+import eg.edu.cu.csds.icare.core.domain.model.Result
+import eg.edu.cu.csds.icare.core.domain.repository.AuthRepository
+import kotlinx.coroutines.flow.Flow
+
+class UnlinkGoogleAccountUseCase(
+    private val repository: AuthRepository,
+) {
+    operator fun invoke(): Flow<Result<Unit, DataError.Remote>> = repository.unlinkGoogleAccount()
+}
