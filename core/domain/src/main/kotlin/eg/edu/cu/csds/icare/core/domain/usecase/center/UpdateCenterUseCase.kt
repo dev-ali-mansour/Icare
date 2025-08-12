f2cb00b86d75e032453590be7882d07176f2380d
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-12 10:25:00 +0300

    Refactor: Standardize UpdateCenter use case
    This commit renames the `UpdateCenter` use case to `UpdateCenterUseCase` to follow a consistent naming convention.
    
    - Renamed `UpdateCenter.kt` to `UpdateCenterUseCase.kt`.
    - The class `UpdateCenter` is now `UpdateCenterUseCase`.
    - The return type of the `invoke` operator function has been changed from `Flow<Resource<Nothing?>>` to `Flow<Result<Unit, DataError.Remote>>`.
    - Imported `eg.edu.cu.csds.icare.core.domain.model.DataError` and `eg.edu.cu.csds.icare.core.domain.model.Result`.
    - Removed import for `eg.edu.cu.csds.icare.core.domain.model.Resource`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/center/UpdateCenterUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/center/UpdateCenterUseCase.kt
@@ -1,12 +1,14 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.center
 
+import eg.edu.cu.csds.icare.core.domain.model.DataError
 import eg.edu.cu.csds.icare.core.domain.model.LabImagingCenter
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.CentersRepository
 import kotlinx.coroutines.flow.Flow
 
-class UpdateCenter(
+class UpdateCenterUseCase(
     private val repository: CentersRepository,
 ) {
-    operator fun invoke(center: LabImagingCenter): Flow<Resource<Nothing?>> = repository.updateCenter(center)
+    operator fun invoke(center: LabImagingCenter): Flow<Result<Unit, DataError.Remote>> =
+        repository.updateCenter(center)
 }
