d7b52e918d77f5dc27a425d4387bf2c3aa2e6730
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-12 10:25:33 +0300

    Refactor: Standardize UpdatePharmacy use case
    This commit renames the `UpdatePharmacy` use case to `UpdatePharmacyUseCase` for naming consistency.
    
    - Renamed `UpdatePharmacy.kt` to `UpdatePharmacyUseCase.kt`.
    - The class `UpdatePharmacy` is now `UpdatePharmacyUseCase`.
    - The return type of the `invoke` operator has been changed from `Flow<Resource<Nothing?>>` to `Flow<Result<Unit, DataError.Remote>>`.
    - Imported `DataError` and `Result` from `eg.edu.cu.csds.icare.core.domain.model`.
    - Removed the import for `eg.edu.cu.csds.icare.core.domain.model.Resource`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/pharmacy/UpdatePharmacyUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/pharmacy/UpdatePharmacyUseCase.kt
@@ -1,12 +1,14 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.pharmacy
 
+import eg.edu.cu.csds.icare.core.domain.model.DataError
 import eg.edu.cu.csds.icare.core.domain.model.Pharmacy
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.PharmaciesRepository
 import kotlinx.coroutines.flow.Flow
 
-class UpdatePharmacy(
+class UpdatePharmacyUseCase(
     private val repository: PharmaciesRepository,
 ) {
-    operator fun invoke(pharmacy: Pharmacy): Flow<Resource<Nothing?>> = repository.updatePharmacy(pharmacy)
+    operator fun invoke(pharmacy: Pharmacy): Flow<Result<Unit, DataError.Remote>> =
+        repository.updatePharmacy(pharmacy)
 }
