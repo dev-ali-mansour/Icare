18b1b634ff9f9c4bea8cb2628a28452ee2da49e1
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-12 10:27:47 +0300

    Refactor: Remove FirebaseAuth and MediaHelper from homeRoute
    This commit removes `FirebaseAuth` and `MediaHelper` dependencies from the `homeRoute` function in both staff and patient flavors of `HomeNavigation.kt`.
    
    **HomeNavigation.kt (staff & patient):**
    - Removed `firebaseAuth` and `mediaHelper` parameters from the `homeRoute` function.
    - The `HomeScreen` composable is no longer called with `firebaseAuth` or `mediaHelper`.
    - Removed unused imports for `com.google.firebase.auth.FirebaseAuth` and `eg.edu.cu.csds.icare.core.ui.util.MediaHelper`.

--- features/home/src/staff/kotlin/eg/edu/cu/csds/icare/home/navigation/HomeNavigation.kt
+++ features/home/src/staff/kotlin/eg/edu/cu/csds/icare/home/navigation/HomeNavigation.kt
@@ -2,7 +2,6 @@ package eg.edu.cu.csds.icare.home.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.google.firebase.auth.FirebaseAuth
 import eg.edu.cu.csds.icare.admin.screen.center.CenterViewModel
 import eg.edu.cu.csds.icare.admin.screen.clinic.ClinicViewModel
 import eg.edu.cu.csds.icare.admin.screen.pharmacy.PharmacyViewModel
@@ -12,13 +11,10 @@ import eg.edu.cu.csds.icare.core.ui.MainViewModel
 import eg.edu.cu.csds.icare.core.ui.common.AppointmentStatus
 import eg.edu.cu.csds.icare.core.ui.common.Role
 import eg.edu.cu.csds.icare.core.ui.navigation.Screen
-import eg.edu.cu.csds.icare.core.ui.util.MediaHelper
 import eg.edu.cu.csds.icare.home.HomeViewModel
 import eg.edu.cu.csds.icare.home.screen.HomeScreen
 
 fun NavGraphBuilder.homeRoute(
-    firebaseAuth: FirebaseAuth,
-    mediaHelper: MediaHelper,
     mainViewModel: MainViewModel,
     homeViewModel: HomeViewModel,
     clinicViewModel: ClinicViewModel,
@@ -31,8 +27,6 @@ fun NavGraphBuilder.homeRoute(
 ) {
     composable<Screen.Home> {
         HomeScreen(
-            firebaseAuth = firebaseAuth,
-            mediaHelper = mediaHelper,
             mainViewModel = mainViewModel,
             homeViewModel = homeViewModel,
             clinicViewModel = clinicViewModel,
