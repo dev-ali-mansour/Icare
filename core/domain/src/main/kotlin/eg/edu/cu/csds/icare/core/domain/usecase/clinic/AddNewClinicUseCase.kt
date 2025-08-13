3fcbc86c163f9bf5af1d22f4361dcf7c7be7f45e
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-13 18:21:52 +0300

    Refactor: Rename AddNewClinic to AddNewClinicUseCase and update return type
    This commit renames the class `AddNewClinic` to `AddNewClinicUseCase` for better clarity on its role as a use case. It also updates the return type of the `invoke` operator function.
    
    **core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/clinic/AddNewClinicUseCase.kt:**
    - Renamed class `AddNewClinic` to `AddNewClinicUseCase`.
    - Changed the return type of the `invoke` function from `Flow<Resource<Nothing?>>` to `Flow<Result<Unit, DataError.Remote>>`.
    - Updated import from `eg.edu.cu.csds.icare.core.domain.model.Resource` to `eg.edu.cu.csds.icare.core.domain.model.DataError` and `eg.edu.cu.csds.icare.core.domain.model.Result`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/clinic/AddNewClinicUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/clinic/AddNewClinicUseCase.kt
@@ -1,12 +1,14 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.clinic
 
 import eg.edu.cu.csds.icare.core.domain.model.Clinic
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.DataError
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.ClinicsRepository
 import kotlinx.coroutines.flow.Flow
 
-class AddNewClinic(
+class AddNewClinicUseCase(
     private val repository: ClinicsRepository,
 ) {
-    operator fun invoke(clinic: Clinic): Flow<Resource<Nothing?>> = repository.addNewClinic(clinic)
+    operator fun invoke(clinic: Clinic): Flow<Result<Unit, DataError.Remote>> =
+        repository.addNewClinic(clinic)
 }
