be08b5c518e8862d6417e25a74fe41fb624cc909
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 15:53:35 +0300

    Refactor: Rename GetAppointmentsByStatus to GetAppointmentsByStatusUseCase
    This commit renames the `GetAppointmentsByStatus` use case to `GetAppointmentsByStatusUseCase` to follow a consistent naming convention.
    
    - The file `GetAppointmentsByStatus.kt` has been deleted.
    - A new file `GetAppointmentsByStatusUseCase.kt` has been created with the renamed class.
    - The return type of the `invoke` operator has been updated from `Flow<Resource<List<Appointment>>>` to `Flow<Result<List<Appointment>, DataError.Remote>>`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/appointment/GetAppointmentsByStatusUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/appointment/GetAppointmentsByStatusUseCase.kt
@@ -1,12 +1,14 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.appointment
 
 import eg.edu.cu.csds.icare.core.domain.model.Appointment
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.DataError
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.AppointmentsRepository
 import kotlinx.coroutines.flow.Flow
 
-class GetAppointmentsByStatus(
+class GetAppointmentsByStatusUseCase(
     private val repository: AppointmentsRepository,
 ) {
-    operator fun invoke(statusId: Short): Flow<Resource<List<Appointment>>> = repository.getAppointments(statusId)
+    operator fun invoke(statusId: Short): Flow<Result<List<Appointment>, DataError.Remote>> =
+        repository.getAppointments(statusId)
 }
