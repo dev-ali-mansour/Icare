d7e0478396fd2a1f3de486602f22a103281515bd
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 16:01:04 +0300

    refactor: Update ListTopDoctors to ListTopDoctorsUseCase and change return type
    This commit renames `ListTopDoctors` to `ListTopDoctorsUseCase` and updates its return type.
    
    - The class `ListTopDoctors` in `eg.edu.cu.csds.icare.core.domain.usecase.doctor` is renamed to `ListTopDoctorsUseCase`.
    - The return type of the `invoke` operator function is changed from `Flow<Resource<List<Doctor>>>` to `Flow<Result<List<Doctor>, DataError.Remote>>`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/doctor/ListTopDoctorsUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/doctor/ListTopDoctorsUseCase.kt
@@ -1,12 +1,13 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.doctor
 
+import eg.edu.cu.csds.icare.core.domain.model.DataError
 import eg.edu.cu.csds.icare.core.domain.model.Doctor
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.ClinicsRepository
 import kotlinx.coroutines.flow.Flow
 
-class ListTopDoctors(
+class ListTopDoctorsUseCase(
     private val repository: ClinicsRepository,
 ) {
-    operator fun invoke(): Flow<Resource<List<Doctor>>> = repository.listTopDoctors()
+    operator fun invoke(): Flow<Result<List<Doctor>, DataError.Remote>> = repository.listTopDoctors()
 }
