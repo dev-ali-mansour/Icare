32f15b25c1c72ca693325a3c469f77adb3816fa4
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 15:59:13 +0300

    Refactor: Rename ListPharmacists to ListPharmacistsUseCase and update return type
    This commit renames the use case `ListPharmacists` to `ListPharmacistsUseCase` for consistency.
    Additionally, the return type of the `invoke` operator function has been updated from `Flow<Resource<List<Pharmacist>>>` to `Flow<Result<List<Pharmacist>, DataError.Remote>>`.
    
    **core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/pharmacist/ListPharmacists.kt:**
    - Deleted.
    
    **core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/pharmacist/ListPharmacistsUseCase.kt:**
    - Created with the new name and updated return type.
    - Imports `eg.edu.cu.csds.icare.core.domain.model.DataError` and `eg.edu.cu.csds.icare.core.domain.model.Result`.
    - The `invoke` function now returns `Flow<Result<List<Pharmacist>, DataError.Remote>>`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/pharmacist/ListPharmacistsUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/pharmacist/ListPharmacistsUseCase.kt
@@ -1,12 +1,13 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.pharmacist
 
+import eg.edu.cu.csds.icare.core.domain.model.DataError
 import eg.edu.cu.csds.icare.core.domain.model.Pharmacist
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.PharmaciesRepository
 import kotlinx.coroutines.flow.Flow
 
-class ListPharmacists(
+class ListPharmacistsUseCase(
     private val repository: PharmaciesRepository,
 ) {
-    operator fun invoke(): Flow<Resource<List<Pharmacist>>> = repository.listPharmacists()
+    operator fun invoke(): Flow<Result<List<Pharmacist>, DataError.Remote>> = repository.listPharmacists()
 }
