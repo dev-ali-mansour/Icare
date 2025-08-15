0b57f26f77ba88956bccbe929ff606fc4a6dec06
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 15:52:18 +0300

    Refactor: Update UpdateClinicStaff to UseCase naming convention
    This commit renames `UpdateClinicStaff` to `UpdateClinicStaffUseCase` to align with the project's use case naming conventions.
    
    - The file `UpdateClinicStaff.kt` has been deleted.
    - A new file `UpdateClinicStaffUseCase.kt` has been created with the renamed class.
    - The `invoke` function within `UpdateClinicStaffUseCase` now returns `Flow<Result<Unit, DataError.Remote>>` instead of `Flow<Resource<Nothing?>>`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/clinic/staff/UpdateClinicStaffUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/clinic/staff/UpdateClinicStaffUseCase.kt
@@ -1,12 +1,14 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.clinic.staff
 
 import eg.edu.cu.csds.icare.core.domain.model.ClinicStaff
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.DataError
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.ClinicsRepository
 import kotlinx.coroutines.flow.Flow
 
-class UpdateClinicStaff(
+class UpdateClinicStaffUseCase(
     private val repository: ClinicsRepository,
 ) {
-    operator fun invoke(staff: ClinicStaff): Flow<Resource<Nothing?>> = repository.updateClinicStaff(staff)
+    operator fun invoke(staff: ClinicStaff): Flow<Result<Unit, DataError.Remote>> =
+        repository.updateClinicStaff(staff)
 }
