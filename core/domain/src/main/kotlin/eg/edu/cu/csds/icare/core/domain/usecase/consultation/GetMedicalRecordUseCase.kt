1ecbf7d59ca40cb05d21d217a2cdfaf8def1eb14
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 15:55:36 +0300

    refactor: Update GetMedicalRecord to GetMedicalRecordUseCase and use Result
    This commit renames `GetMedicalRecord` to `GetMedicalRecordUseCase` and updates its return type.
    
    - The class `GetMedicalRecord` in `GetMedicalRecord.kt` is renamed to `GetMedicalRecordUseCase` and moved to `GetMedicalRecordUseCase.kt`.
    - The `invoke` operator in `GetMedicalRecordUseCase` now returns `Flow<Result<MedicalRecord, DataError.Remote>>` instead of `Flow<Resource<MedicalRecord>>`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/consultation/GetMedicalRecordUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/consultation/GetMedicalRecordUseCase.kt
@@ -1,12 +1,14 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.consultation
 
+import eg.edu.cu.csds.icare.core.domain.model.DataError
 import eg.edu.cu.csds.icare.core.domain.model.MedicalRecord
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.ConsultationsRepository
 import kotlinx.coroutines.flow.Flow
 
-class GetMedicalRecord(
+class GetMedicalRecordUseCase(
     private val repository: ConsultationsRepository,
 ) {
-    operator fun invoke(patientId: String): Flow<Resource<MedicalRecord>> = repository.getMedicalRecord(patientId)
+    operator fun invoke(patientId: String): Flow<Result<MedicalRecord, DataError.Remote>> =
+        repository.getMedicalRecord(patientId)
 }
