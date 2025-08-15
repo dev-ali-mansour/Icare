9ffdf6314b5ce68caf79f0cef7c33e33455a6820
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 15:50:19 +0300

    Refactor: Update AddNewPharmacist use case
    This commit refactors the `AddNewPharmacist` use case.
    
    - Renamed `AddNewPharmacist` to `AddNewPharmacistUseCase` for consistency.
    - Updated the return type of the `invoke` operator from `Flow<Resource<Nothing?>>` to `Flow<Result<Unit, DataError.Remote>>`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/pharmacist/AddNewPharmacistUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/pharmacist/AddNewPharmacistUseCase.kt
@@ -1,12 +1,14 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.pharmacist
 
+import eg.edu.cu.csds.icare.core.domain.model.DataError
 import eg.edu.cu.csds.icare.core.domain.model.Pharmacist
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.PharmaciesRepository
 import kotlinx.coroutines.flow.Flow
 
-class AddNewPharmacist(
+class AddNewPharmacistUseCase(
     private val repository: PharmaciesRepository,
 ) {
-    operator fun invoke(pharmacist: Pharmacist): Flow<Resource<Nothing?>> = repository.addNewPharmacist(pharmacist)
+    operator fun invoke(pharmacist: Pharmacist): Flow<Result<Unit, DataError.Remote>> =
+        repository.addNewPharmacist(pharmacist)
 }
