0d9e28ea33584e79875fcc9e40a4e3be6719f8b9
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 15:49:32 +0300

    Refactor: Rename AddNewCenterStaff and update return type
    This commit renames the use case `AddNewCenterStaff` to `AddNewCenterStaffUseCase` for consistency.
    
    Additionally, the return type of the `invoke` operator in `AddNewCenterStaffUseCase.kt` has been updated from `Flow<Resource<Nothing?>>` to `Flow<Result<Unit, DataError.Remote>>`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/center/staff/AddNewCenterStaffUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/center/staff/AddNewCenterStaffUseCase.kt
@@ -1,12 +1,14 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.center.staff
 
 import eg.edu.cu.csds.icare.core.domain.model.CenterStaff
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.DataError
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.CentersRepository
 import kotlinx.coroutines.flow.Flow
 
-class AddNewCenterStaff(
+class AddNewCenterStaffUseCase(
     private val repository: CentersRepository,
 ) {
-    operator fun invoke(staff: CenterStaff): Flow<Resource<Nothing?>> = repository.addNewCenterStaff(staff)
+    operator fun invoke(staff: CenterStaff): Flow<Result<Unit, DataError.Remote>> =
+        repository.addNewCenterStaff(staff)
 }
