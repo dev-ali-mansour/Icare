7ad1652752c7391a581939cc1867ece35e6f7313
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 16:15:44 +0300

    Refactor: Update ClinicsRepository to use Result type and add getDoctor function
    This commit refactors `ClinicsRepository` and its implementation `ClinicsRepositoryImpl` to use the `Result` type instead of `Resource` for handling data operations. Additionally, the `forceRefresh` parameter has been renamed to `forceUpdate` for clarity.
    
    **ClinicsRepository.kt:**
    - All functions now return `Flow<Result<T, DataError.Remote>>` or `Flow<Result<T, DataError.Local>>` instead of `Flow<Resource<T>>`.
    - The `forceRefresh` parameter in `listClinics` and `listDoctors` has been renamed to `forceUpdate`.
    - A new function `getDoctor(doctorId: Long): Flow<Result<Doctor, DataError.Local>>` has been added.
    
    **ClinicsRepositoryImpl.kt:**
    - Updated function signatures and implementations to align with the changes in `ClinicsRepository`.
    - Replaced `Resource` handling with `Result` handling, utilizing `onSuccess` and `onError` callbacks.
    - Renamed `forceRefresh` to `forceUpdate` in `listClinics` and `listDoctors`.
    - Implemented the new `getDoctor` function to fetch a doctor from the local data source.
    - Added Timber logging in `listClinics` for debugging purposes.
    - Simplified the logic for adding and updating clinics and doctors by leveraging the `Result` type and refreshing the list upon success.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/repository/ClinicsRepository.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/repository/ClinicsRepository.kt
@@ -2,33 +2,36 @@ package eg.edu.cu.csds.icare.core.domain.repository
 
 import eg.edu.cu.csds.icare.core.domain.model.Clinic
 import eg.edu.cu.csds.icare.core.domain.model.ClinicStaff
+import eg.edu.cu.csds.icare.core.domain.model.DataError
 import eg.edu.cu.csds.icare.core.domain.model.Doctor
 import eg.edu.cu.csds.icare.core.domain.model.DoctorSchedule
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import kotlinx.coroutines.flow.Flow
 
 interface ClinicsRepository {
-    fun listClinics(forceRefresh: Boolean): Flow<Resource<List<Clinic>>>
+    fun listClinics(forceUpdate: Boolean): Flow<Result<List<Clinic>, DataError.Remote>>
 
-    fun addNewClinic(clinic: Clinic): Flow<Resource<Nothing?>>
+    fun addNewClinic(clinic: Clinic): Flow<Result<Unit, DataError.Remote>>
 
-    fun updateClinic(clinic: Clinic): Flow<Resource<Nothing?>>
+    fun updateClinic(clinic: Clinic): Flow<Result<Unit, DataError.Remote>>
 
-    fun listDoctors(forceRefresh: Boolean): Flow<Resource<List<Doctor>>>
+    fun listDoctors(forceUpdate: Boolean): Flow<Result<List<Doctor>, DataError.Remote>>
 
-    fun getDoctorSchedule(uid: String): Flow<Resource<DoctorSchedule>>
+    fun getDoctorSchedule(uid: String): Flow<Result<DoctorSchedule, DataError.Remote>>
 
-    fun listTopDoctors(): Flow<Resource<List<Doctor>>>
+    fun listTopDoctors(): Flow<Result<List<Doctor>, DataError.Remote>>
 
-    fun listClinicDoctors(clinicId: Long): Flow<Resource<List<Doctor>>>
+    fun listClinicDoctors(clinicId: Long): Flow<Result<List<Doctor>, DataError.Remote>>
 
-    fun addNewDoctor(doctor: Doctor): Flow<Resource<Nothing?>>
+    fun addNewDoctor(doctor: Doctor): Flow<Result<Unit, DataError.Remote>>
 
-    fun updateDoctor(doctor: Doctor): Flow<Resource<Nothing?>>
+    fun getDoctor(doctorId: Long): Flow<Result<Doctor, DataError.Local>>
 
-    fun listClinicStaff(): Flow<Resource<List<ClinicStaff>>>
+    fun updateDoctor(doctor: Doctor): Flow<Result<Unit, DataError.Remote>>
 
-    fun addNewClinicStaff(staff: ClinicStaff): Flow<Resource<Nothing?>>
+    fun listClinicStaff(): Flow<Result<List<ClinicStaff>, DataError.Remote>>
 
-    fun updateClinicStaff(staff: ClinicStaff): Flow<Resource<Nothing?>>
+    fun addNewClinicStaff(staff: ClinicStaff): Flow<Result<Unit, DataError.Remote>>
+
+    fun updateClinicStaff(staff: ClinicStaff): Flow<Result<Unit, DataError.Remote>>
 }
