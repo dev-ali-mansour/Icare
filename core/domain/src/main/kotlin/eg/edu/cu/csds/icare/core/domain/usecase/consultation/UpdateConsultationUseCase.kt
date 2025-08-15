ff15cb6c0273c22936f9bdb420a5d2288f669121
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 15:52:03 +0300

    Refactor: Update UpdateConsultation to UpdateConsultationUseCase and modify return type
    This commit renames `UpdateConsultation.kt` to `UpdateConsultationUseCase.kt` and changes the return type of its `invoke` function.
    
    - The class `UpdateConsultation` is renamed to `UpdateConsultationUseCase`.
    - The `invoke` function within `UpdateConsultationUseCase` now returns `Flow<Result<Unit, DataError.Remote>>` instead of `Flow<Resource<Nothing?>>`.
    - The import for `eg.edu.cu.csds.icare.core.domain.model.Resource` is removed.
    - Imports for `eg.edu.cu.csds.icare.core.domain.model.DataError` and `eg.edu.cu.csds.icare.core.domain.model.Result` are added.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/consultation/UpdateConsultationUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/consultation/UpdateConsultationUseCase.kt
@@ -1,12 +1,14 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.consultation
 
 import eg.edu.cu.csds.icare.core.domain.model.Consultation
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.DataError
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.ConsultationsRepository
 import kotlinx.coroutines.flow.Flow
 
-class UpdateConsultation(
+class UpdateConsultationUseCase(
     private val repository: ConsultationsRepository,
 ) {
-    operator fun invoke(consultation: Consultation): Flow<Resource<Nothing?>> = repository.updateConsultation(consultation)
+    operator fun invoke(consultation: Consultation): Flow<Result<Unit, DataError.Remote>> =
+        repository.updateConsultation(consultation)
 }
