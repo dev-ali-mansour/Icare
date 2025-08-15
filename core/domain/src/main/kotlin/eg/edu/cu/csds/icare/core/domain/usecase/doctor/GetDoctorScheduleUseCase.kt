8168d314d7f8e5e5d9680fa6e53695e1176f92eb
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 15:54:08 +0300

    Refactor: Rename GetDoctorSchedule to GetDoctorScheduleUseCase and update return type
    This commit renames the use case `GetDoctorSchedule` to `GetDoctorScheduleUseCase` for consistency.
    
    Additionally, the return type of the `invoke` operator in `GetDoctorScheduleUseCase.kt` has been updated from `Flow<Resource<DoctorSchedule>>` to `Flow<Result<DoctorSchedule, DataError.Remote>>`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/doctor/GetDoctorScheduleUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/doctor/GetDoctorScheduleUseCase.kt
@@ -1,12 +1,14 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.doctor
 
+import eg.edu.cu.csds.icare.core.domain.model.DataError
 import eg.edu.cu.csds.icare.core.domain.model.DoctorSchedule
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.ClinicsRepository
 import kotlinx.coroutines.flow.Flow
 
-class GetDoctorSchedule(
+class GetDoctorScheduleUseCase(
     private val repository: ClinicsRepository,
 ) {
-    operator fun invoke(uid: String): Flow<Resource<DoctorSchedule>> = repository.getDoctorSchedule(uid)
+    operator fun invoke(uid: String): Flow<Result<DoctorSchedule, DataError.Remote>> =
+        repository.getDoctorSchedule(uid)
 }
