09b0cfc7ca9900daec025ef8afcea44ca4dfea68
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 15:49:55 +0300

    Refactor: Standardize AddNewDoctor use case and return type
    This commit refactors the `AddNewDoctor` use case.
    
    - Renamed `AddNewDoctor` to `AddNewDoctorUseCase` to follow consistent naming conventions.
    - Updated the return type of the `invoke` operator from `Flow<Resource<Nothing?>>` to `Flow<Result<Unit, DataError.Remote>>`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/doctor/AddNewDoctorUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/doctor/AddNewDoctorUseCase.kt
@@ -1,12 +1,14 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.doctor
 
+import eg.edu.cu.csds.icare.core.domain.model.DataError
 import eg.edu.cu.csds.icare.core.domain.model.Doctor
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.ClinicsRepository
 import kotlinx.coroutines.flow.Flow
 
-class AddNewDoctor(
+class AddNewDoctorUseCase(
     private val repository: ClinicsRepository,
 ) {
-    operator fun invoke(doctor: Doctor): Flow<Resource<Nothing?>> = repository.addNewDoctor(doctor)
+    operator fun invoke(doctor: Doctor): Flow<Result<Unit, DataError.Remote>> =
+        repository.addNewDoctor(doctor)
 }
