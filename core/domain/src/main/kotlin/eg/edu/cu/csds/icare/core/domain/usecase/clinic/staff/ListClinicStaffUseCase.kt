50f032ad11b5e85a4ef6ac10c98d1806e4c5fb70
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 15:58:20 +0300

    Refactor: Update ListClinicStaff to ListClinicStaffUseCase and change return type
    This commit renames `ListClinicStaff.kt` to `ListClinicStaffUseCase.kt` and modifies its return type.
    
    - The class `ListClinicStaff` is renamed to `ListClinicStaffUseCase`.
    - The return type of the `invoke` operator function has been changed from `Flow<Resource<List<ClinicStaff>>>` to `Flow<Result<List<ClinicStaff>, DataError.Remote>>`.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/clinic/staff/ListClinicStaffUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/clinic/staff/ListClinicStaffUseCase.kt
@@ -1,12 +1,13 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.clinic.staff
 
 import eg.edu.cu.csds.icare.core.domain.model.ClinicStaff
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.DataError
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.ClinicsRepository
 import kotlinx.coroutines.flow.Flow
 
-class ListClinicStaff(
+class ListClinicStaffUseCase(
     private val repository: ClinicsRepository,
 ) {
-    operator fun invoke(): Flow<Resource<List<ClinicStaff>>> = repository.listClinicStaff()
+    operator fun invoke(): Flow<Result<List<ClinicStaff>, DataError.Remote>> = repository.listClinicStaff()
 }
