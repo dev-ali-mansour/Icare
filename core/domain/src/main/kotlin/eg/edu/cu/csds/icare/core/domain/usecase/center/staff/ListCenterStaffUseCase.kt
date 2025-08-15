f2d08b7c2ef5b8bed54b64645a8b8051f64aa856
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 15:58:07 +0300

    Refactor: Rename ListCenterStaff to ListCenterStaffUseCase and update return type
    This commit renames the use case `ListCenterStaff` to `ListCenterStaffUseCase` for consistency.
    Additionally, the return type of the `invoke` operator has been updated from `Flow<Resource<List<CenterStaff>>>` to `Flow<Result<List<CenterStaff>, DataError.Remote>>`.
    
    - `ListCenterStaff.kt` has been deleted.
    - `ListCenterStaffUseCase.kt` has been created with the updated name and return type.

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/center/staff/ListCenterStaffUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/center/staff/ListCenterStaffUseCase.kt
@@ -1,12 +1,13 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.center.staff
 
 import eg.edu.cu.csds.icare.core.domain.model.CenterStaff
-import eg.edu.cu.csds.icare.core.domain.model.Resource
+import eg.edu.cu.csds.icare.core.domain.model.DataError
+import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.CentersRepository
 import kotlinx.coroutines.flow.Flow
 
-class ListCenterStaff(
+class ListCenterStaffUseCase(
     private val repository: CentersRepository,
 ) {
-    operator fun invoke(): Flow<Resource<List<CenterStaff>>> = repository.listCenterStaff()
+    operator fun invoke(): Flow<Result<List<CenterStaff>, DataError.Remote>> = repository.listCenterStaff()
 }
