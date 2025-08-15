74e717de3afaa89a6c9ed3a96ff7fe930d939735
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-15 16:23:54 +0300

    refactor: Rename `Screen` to `Route` in `BottomNavItem`
    This commit updates `BottomNavItem.kt` to use `Route` instead of `Screen` for defining navigation destinations.
    
    - The `screen` property in the `BottomNavItem` sealed class has been renamed to `route`.
    - All instances of `Screen` (e.g., `Screen.Home`) have been changed to their corresponding `Route` (e.g., `Route.Home`).

--- core/ui/src/main/kotlin/eg/edu/cu/csds/icare/core/ui/common/BottomNavItem.kt
+++ core/ui/src/main/kotlin/eg/edu/cu/csds/icare/core/ui/common/BottomNavItem.kt
@@ -3,34 +3,34 @@ package eg.edu.cu.csds.icare.core.ui.common
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import eg.edu.cu.csds.icare.core.ui.R
-import eg.edu.cu.csds.icare.core.ui.navigation.Screen
+import eg.edu.cu.csds.icare.core.ui.navigation.Route
 
 sealed class BottomNavItem(
     @param:DrawableRes var iconResId: Int,
     @param:StringRes val titleResId: Int,
-    var screen: Screen,
+    var route: Route,
 ) {
     data object Home : BottomNavItem(
         R.drawable.ic_home,
         R.string.nav_home,
-        Screen.Home,
+        Route.Home,
     )
 
     data object Notifications : BottomNavItem(
         R.drawable.ic_notifications,
         R.string.nav_notifications,
-        Screen.Notifications,
+        Route.Notifications,
     )
 
     data object Profile : BottomNavItem(
         R.drawable.ic_profile,
         R.string.nav_profile,
-        Screen.Profile,
+        Route.Profile,
     )
 
     data object Settings : BottomNavItem(
         R.drawable.ic_nav_settings,
         R.string.nav_settings,
-        Screen.Settings,
+        Route.Settings,
     )
 }
