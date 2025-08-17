b4e2e6891c90a485786ae2375fee9d8dd6e78fba
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-17 21:47:44 +0300

    feat: Introduce DoctorListState for managing doctor list UI
    This commit adds a new data class `DoctorListState.kt` to manage the state of the doctor list screen in the admin feature.
    
    **features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/clinic/doctor/list/DoctorListState.kt:**
    - Created `DoctorListState` data class with `isLoading` (Boolean) and `doctors` (List<Doctor>) properties to represent the UI state.

---
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/clinic/doctor/list/DoctorListState.kt
@@ -0,0 +1,8 @@
+package eg.edu.cu.csds.icare.admin.screen.clinic.doctor.list
+
+import eg.edu.cu.csds.icare.core.domain.model.Doctor
+
+data class DoctorListState(
+    val isLoading: Boolean = false,
+    val doctors: List<Doctor> = emptyList(),
+)
