d06d6775a5377bc93d2b7ac4b98a06cee227f355
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-18 01:12:07 +0300

    feat: Introduce SelectedClinicViewModel for managing selected clinic state
    This commit introduces a new ViewModel, `SelectedClinicViewModel`, responsible for managing the state of the currently selected clinic.
    
    **features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/clinic/SelectedClinicViewModel.kt:**
    - Created `SelectedClinicViewModel.kt`.
    - Added a `MutableStateFlow` named `_selectedClinic` to hold the selected `Clinic` object, initialized to `null`.
    - Exposed an immutable `StateFlow` named `selectedClinic` for observing changes to the selected clinic.
    - Added a function `onSelectClinic(clinic: Clinic?)` to update the `_selectedClinic` value.
    - Annotated the class with `@KoinViewModel` for dependency injection.

---
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/clinic/SelectedClinicViewModel.kt
@@ -0,0 +1,17 @@
+package eg.edu.cu.csds.icare.admin.screen.clinic
+
+import androidx.lifecycle.ViewModel
+import eg.edu.cu.csds.icare.core.domain.model.Clinic
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import org.koin.android.annotation.KoinViewModel
+
+@KoinViewModel
+class SelectedClinicViewModel : ViewModel() {
+    private val _selectedClinic = MutableStateFlow<Clinic?>(null)
+    val selectedClinic = _selectedClinic.asStateFlow()
+
+    fun onSelectClinic(clinic: Clinic?) {
+        _selectedClinic.value = clinic
+    }
+}
