e57990fd902dda7994d9903073009966d9a9b708
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-18 18:44:06 +0300

    refactor: Rename staffList to clinicians in CliniciansResponse
    This commit renames the field `staffList` to `clinicians` in the `CliniciansResponse` data class and updates its usage in `RemoteClinicsDataSourceImpl`.
    
    **CliniciansResponse.kt:**
    - Renamed `staffList` to `clinicians`.
    
    **RemoteClinicsDataSourceImpl.kt:**
    - Updated the access to the list of clinicians from `res.staffList` to `res.clinicians` when handling a successful API response for fetching clinicians.

--- core/data/src/main/kotlin/eg/edu/cu/csds/icare/core/data/dto/CliniciansResponse.kt
+++ core/data/src/main/kotlin/eg/edu/cu/csds/icare/core/data/dto/CliniciansResponse.kt
@@ -8,5 +8,5 @@ data class CliniciansResponse(
     @SerialName("status")
     val statusCode: Short,
     @SerialName("data")
-    val staffList: List<ClinicianDto>,
+    val clinicians: List<ClinicianDto>,
 )
