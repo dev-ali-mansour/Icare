b118dda31d091439c533acd371e7c5608b3e96c1
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-19 07:13:30 +0300

    feat: Create PharmacyListState data class
    This commit introduces the `PharmacyListState` data class.
    
    **features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/list/PharmacyListState.kt:**
    - Created a new data class `PharmacyListState`.
    - It contains two properties:
        - `isLoading`: A `Boolean` to indicate loading status, defaulting to `false`.
        - `pharmacies`: A `List<Pharmacy>` to hold the list of pharmacies, defaulting to an empty list.

---
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/list/PharmacyListState.kt
@@ -0,0 +1,8 @@
+package eg.edu.cu.csds.icare.admin.screen.pharmacy.list
+
+import eg.edu.cu.csds.icare.core.domain.model.Pharmacy
+
+data class PharmacyListState(
+    val isLoading: Boolean = false,
+    val pharmacies: List<Pharmacy> = emptyList(),
+)
