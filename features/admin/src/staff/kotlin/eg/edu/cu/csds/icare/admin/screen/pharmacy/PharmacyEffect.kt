d5b5011056100b08a5b34bc9064d0c7956c5b7a6
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-21 15:00:00 +0300

    refactor: Rename PharmacySingleEvent to PharmacyEffect

--- features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/PharmacyEffect.kt
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/PharmacyEffect.kt
@@ -2,10 +2,10 @@ package eg.edu.cu.csds.icare.admin.screen.pharmacy
 
 import eg.edu.cu.csds.icare.core.ui.util.UiText
 
-sealed interface PharmacySingleEvent {
-    object ShowSuccess : PharmacySingleEvent
+sealed interface PharmacyEffect {
+    object ShowSuccess : PharmacyEffect
 
     data class ShowError(
         val message: UiText,
-    ) : PharmacySingleEvent
+    ) : PharmacyEffect
 }
