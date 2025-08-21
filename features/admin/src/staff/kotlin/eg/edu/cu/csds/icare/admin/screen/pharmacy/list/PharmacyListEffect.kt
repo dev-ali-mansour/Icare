8387ee587cce5e18548bc576efe955ecb3c8b96b
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-21 15:00:00 +0300

    refactor: Rename PharmacyListSingleEvent to PharmacyListEffect

--- features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/list/PharmacyListEffect.kt
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/list/PharmacyListEffect.kt
@@ -3,16 +3,16 @@ package eg.edu.cu.csds.icare.admin.screen.pharmacy.list
 import eg.edu.cu.csds.icare.core.domain.model.Pharmacy
 import eg.edu.cu.csds.icare.core.ui.util.UiText
 
-sealed interface PharmacyListSingleEvent {
+sealed interface PharmacyListEffect {
     data class NavigateToPharmacyDetails(
         val pharmacy: Pharmacy,
-    ) : PharmacyListSingleEvent
+    ) : PharmacyListEffect
 
     data class UpdateFabExpanded(
         val isExpanded: Boolean,
-    ) : PharmacyListSingleEvent
+    ) : PharmacyListEffect
 
     data class ShowError(
         val message: UiText,
-    ) : PharmacyListSingleEvent
+    ) : PharmacyListEffect
 }
