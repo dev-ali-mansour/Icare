c069d15240e6175c0eb1046f9763b077245fd7df
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-22 15:00:00 +0300

    refactor: Add effect field to PharmacyListState

--- features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/list/PharmacyListState.kt
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/list/PharmacyListState.kt
@@ -1,8 +1,10 @@
 package eg.edu.cu.csds.icare.admin.screen.pharmacy.list
 
+import eg.edu.cu.csds.icare.admin.screen.pharmacy.PharmacyEffect
 import eg.edu.cu.csds.icare.core.domain.model.Pharmacy
 
 data class PharmacyListState(
     val isLoading: Boolean = false,
     val pharmacies: List<Pharmacy> = emptyList(),
+    val effect: PharmacyEffect? = null,
 )
