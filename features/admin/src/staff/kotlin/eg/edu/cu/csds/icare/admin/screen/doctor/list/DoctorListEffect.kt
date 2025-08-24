2fe448a6e2210456b42eb3481a433c718972d2a4
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-24 18:01:39 +0300

    refactor: Rename DoctorListSingleEvent to DoctorListEffect

--- features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/doctor/list/DoctorListEffect.kt
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/doctor/list/DoctorListEffect.kt
@@ -3,16 +3,16 @@ package eg.edu.cu.csds.icare.admin.screen.doctor.list
 import eg.edu.cu.csds.icare.core.domain.model.Doctor
 import eg.edu.cu.csds.icare.core.ui.util.UiText
 
-sealed interface DoctorListSingleEvent {
+sealed interface DoctorListEffect {
     data class NavigateToDoctorDetails(
         val doctor: Doctor,
-    ) : DoctorListSingleEvent
+    ) : DoctorListEffect
 
     data class UpdateFabExpanded(
         val isExpanded: Boolean,
-    ) : DoctorListSingleEvent
+    ) : DoctorListEffect
 
     data class ShowError(
         val message: UiText,
-    ) : DoctorListSingleEvent
+    ) : DoctorListEffect
 }
