a33eaecf6c4a688d5b4665c30fb342fa2f602933
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-24 18:01:48 +0300

    refactor: Add effect field to DoctorListState
    This commit adds a new `effect` field of type `DoctorListEffect?` to the `DoctorListState` data class. This allows the state to hold information about side effects that should be triggered in the UI.
    
    **Key changes:**
    - Added `effect: DoctorListEffect? = null` to `DoctorListState`.
    
    **Affected files:**
    - **features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/doctor/list/DoctorListState.kt:**
        - Added the `effect` field to the `DoctorListState` data class.

--- features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/doctor/list/DoctorListState.kt
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/doctor/list/DoctorListState.kt
@@ -5,4 +5,5 @@ import eg.edu.cu.csds.icare.core.domain.model.Doctor
 data class DoctorListState(
     val isLoading: Boolean = false,
     val doctors: List<Doctor> = emptyList(),
+    val effect: DoctorListEffect? = null,
 )
