fe805e9a185bb58ea0d922bf8bfe91061830a890
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-24 18:02:01 +0300

    refactor: Rename DoctorListIntent to DoctorLisEvent
    This commit renames the `DoctorListIntent` sealed interface to `DoctorLisEvent` for consistency with MVI naming conventions.
    
    **Key changes:**
    - Renamed `DoctorListIntent` to `DoctorLisEvent`.
    - Added `ConsumeEffect` object to `DoctorLisEvent`.
    
    **Affected files:**
    - `features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/doctor/list/DoctorLisEvent.kt` (renamed from `DoctorListIntent.kt`)

--- features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/doctor/list/DoctorLisEvent.kt
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/doctor/list/DoctorLisEvent.kt
@@ -2,14 +2,16 @@ package eg.edu.cu.csds.icare.admin.screen.doctor.list
 
 import eg.edu.cu.csds.icare.core.domain.model.Doctor
 
-sealed interface DoctorListIntent {
-    object Refresh : DoctorListIntent
+sealed interface DoctorLisEvent {
+    object Refresh : DoctorLisEvent
 
     data class SelectDoctor(
         val doctor: Doctor,
-    ) : DoctorListIntent
+    ) : DoctorLisEvent
 
     data class UpdateFabExpanded(
         val isExpanded: Boolean,
-    ) : DoctorListIntent
+    ) : DoctorLisEvent
+
+    object ConsumeEffect : DoctorLisEvent
 }
