aa2be6df7f6f0c492b6729bedc43d610a18a7083
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-24 17:44:35 +0300

    refactor: Update PharmacyListState effect type
    This commit updates the `effect` field in `PharmacyListState` to use the more specific `PharmacyListEffect` type instead of the generic `PharmacyEffect`.
    
    **Key changes:**
    - Changed type of `effect` from `PharmacyEffect?` to `PharmacyListEffect?` in `PharmacyListState`.
    
    **Affected files:**
    - **features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/list/PharmacyListState.kt:**
        - Updated `effect` field type.

--- features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/list/PharmacyListState.kt
+++ features/admin/src/staff/kotlin/eg/edu/cu/csds/icare/admin/screen/pharmacy/list/PharmacyListState.kt
@@ -1,10 +1,9 @@
 package eg.edu.cu.csds.icare.admin.screen.pharmacy.list
 
-import eg.edu.cu.csds.icare.admin.screen.pharmacy.PharmacyEffect
 import eg.edu.cu.csds.icare.core.domain.model.Pharmacy
 
 data class PharmacyListState(
     val isLoading: Boolean = false,
     val pharmacies: List<Pharmacy> = emptyList(),
-    val effect: PharmacyEffect? = null,
+    val effect: PharmacyListEffect? = null,
 )
