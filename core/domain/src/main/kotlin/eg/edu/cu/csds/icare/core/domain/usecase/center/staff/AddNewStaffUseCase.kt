c9371b34fb9f702b6d2cb482e005d94bc4251f6f
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-25 08:18:16 +0300

    refactor: Rename AddNewCenterStaffUseCase to AddNewStaffUseCase
    This commit renames the `AddNewCenterStaffUseCase` to `AddNewStaffUseCase` and updates its dependencies to use `Staff` instead of `CenterStaff`.
    
    **Key changes:**
    - Renamed `AddNewCenterStaffUseCase` to `AddNewStaffUseCase`.
    - Changed parameter type from `CenterStaff` to `Staff` in the `invoke` operator function.
    
    **Affected files:**
    - `core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/center/staff/AddNewStaffUseCase.kt` (renamed from `AddNewCenterStaffUseCase.kt`)

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/center/staff/AddNewStaffUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/center/staff/AddNewStaffUseCase.kt
@@ -1,14 +1,14 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.center.staff
 
-import eg.edu.cu.csds.icare.core.domain.model.CenterStaff
 import eg.edu.cu.csds.icare.core.domain.model.DataError
 import eg.edu.cu.csds.icare.core.domain.model.Result
+import eg.edu.cu.csds.icare.core.domain.model.Staff
 import eg.edu.cu.csds.icare.core.domain.repository.CentersRepository
 import kotlinx.coroutines.flow.Flow
 
-class AddNewCenterStaffUseCase(
+class AddNewStaffUseCase(
     private val repository: CentersRepository,
 ) {
-    operator fun invoke(staff: CenterStaff): Flow<Result<Unit, DataError.Remote>> =
+    operator fun invoke(staff: Staff): Flow<Result<Unit, DataError.Remote>> =
         repository.addNewCenterStaff(staff)
 }
