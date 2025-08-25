505b3b163b805659ae56781118ea908cc9556c65
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-25 08:17:57 +0300

    refactor: Rename ListCenterStaffUseCase and update model usage
    This commit renames `ListCenterStaffUseCase` to `ListStaffUseCase` and updates its usage of the `CenterStaff` model to the more generic `Staff` model.
    
    **Key changes:**
    - Renamed `ListCenterStaffUseCase` to `ListStaffUseCase`.
    - Changed the return type of the `invoke` operator function from `Flow<Result<List<CenterStaff>, DataError.Remote>>` to `Flow<Result<List<Staff>, DataError.Remote>>`.
    - Updated import from `eg.edu.cu.csds.icare.core.domain.model.CenterStaff` to `eg.edu.cu.csds.icare.core.domain.model.Staff`.
    
    **Affected files:**
    - `core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/center/staff/ListStaffUseCase.kt` (renamed from `ListCenterStaffUseCase.kt`)

--- core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/center/staff/ListStaffUseCase.kt
+++ core/domain/src/main/kotlin/eg/edu/cu/csds/icare/core/domain/usecase/center/staff/ListStaffUseCase.kt
@@ -1,13 +1,13 @@
 package eg.edu.cu.csds.icare.core.domain.usecase.center.staff
 
-import eg.edu.cu.csds.icare.core.domain.model.CenterStaff
+import eg.edu.cu.csds.icare.core.domain.model.Staff
 import eg.edu.cu.csds.icare.core.domain.model.DataError
 import eg.edu.cu.csds.icare.core.domain.model.Result
 import eg.edu.cu.csds.icare.core.domain.repository.CentersRepository
 import kotlinx.coroutines.flow.Flow
 
-class ListCenterStaffUseCase(
+class ListStaffUseCase(
     private val repository: CentersRepository,
 ) {
-    operator fun invoke(): Flow<Result<List<CenterStaff>, DataError.Remote>> = repository.listCenterStaff()
+    operator fun invoke(): Flow<Result<List<Staff>, DataError.Remote>> = repository.listCenterStaff()
 }
