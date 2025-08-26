005ca568599bcaacb1a8b8f1f382a27960b31c88
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-26 06:59:16 +0300

    refactor: Update OnBoardingScreen import path
    This commit updates the import path for `OnBoardingScreen` in the on-boarding navigation.
    
    **Key changes:**
    - Changed import from `eg.edu.cu.csds.icare.onboarding.OnBoardingScreen` to `eg.edu.cu.csds.icare.onboarding.screen.OnBoardingScreen`.
    
    **Affected files:**
    
    **features/on_boarding/src/main/kotlin/eg/edu/cu/csds/icare/onboarding/navigation/OnBoardingNavigation.kt:**
    - Updated the import statement for `OnBoardingScreen`.

--- features/on_boarding/src/main/kotlin/eg/edu/cu/csds/icare/onboarding/navigation/OnBoardingNavigation.kt
+++ features/on_boarding/src/main/kotlin/eg/edu/cu/csds/icare/onboarding/navigation/OnBoardingNavigation.kt
@@ -3,7 +3,7 @@ package eg.edu.cu.csds.icare.onboarding.navigation
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import eg.edu.cu.csds.icare.core.ui.navigation.Route
-import eg.edu.cu.csds.icare.onboarding.OnBoardingScreen
+import eg.edu.cu.csds.icare.onboarding.screen.OnBoardingScreen
 
 fun NavGraphBuilder.onBoardingRoute(onFinished: () -> Unit) {
     composable<Route.OnBoarding> {
