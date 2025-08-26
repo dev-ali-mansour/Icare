cdc59b735788f4d41c7cc6403b9768d22db9574b
Ali Mansour <dev.ali.mansour@gmail.com>
2025-08-26 06:54:05 +0300

    refactor: Use UiText for OnBoardingPage title and description
    This commit updates the `OnBoardingPage` data class to use `UiText` for its `title` and `description` fields, instead of `String`. This allows for more flexible text handling, such as using string resources.
    
    **Key changes:**
    - Changed the type of `title` from `String` to `UiText`.
    - Changed the type of `description` from `String` to `UiText`.
    
    **Affected files:**
    
    **core/ui/src/main/kotlin/eg/edu/cu/csds/icare/core/ui/common/OnBoardingPage.kt:**
    - Updated `title` and `description` fields to use `UiText`.
    - Added import for `eg.edu.cu.csds.icare.core.ui.util.UiText`.

--- core/ui/src/main/kotlin/eg/edu/cu/csds/icare/core/ui/common/OnBoardingPage.kt
+++ core/ui/src/main/kotlin/eg/edu/cu/csds/icare/core/ui/common/OnBoardingPage.kt
@@ -1,10 +1,11 @@
 package eg.edu.cu.csds.icare.core.ui.common
 
 import androidx.annotation.DrawableRes
+import eg.edu.cu.csds.icare.core.ui.util.UiText
 
 data class OnBoardingPage(
     @param:DrawableRes
     val image: Int,
-    val title: String,
-    val description: String,
+    val title: UiText,
+    val description: UiText,
 )
