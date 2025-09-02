c01248a68e45fc2573d844dbbfddd2f218f97dae
Ali Mansour <dev.ali.mansour@gmail.com>
2025-09-02 14:48:03 +0300

    feat: Modularize Koin dependency injection
    This commit introduces Koin modules for each feature to improve modularity and organization of dependency injection.
    
    **Key changes:**
    - Created Koin modules for the following features:
        - Admin
        - Appointments
        - Auth
        - Consultations
        - Home
        - Notifications
        - OnBoarding
        - Settings
    - Updated `AppModule` to include these new feature-specific modules.
    - Removed the `KOIN_DEFAULT_MODULE` argument configuration from `KoinConventionPlugin` as it's no longer needed with explicit module includes.
    
    **Affected files:**
    - **features/home/src/main/kotlin/eg/edu/cu/csds/icare/home/di/HomeModule.kt** (new file)
    - **features/consultations/src/main/kotlin/eg/edu/cu/csds/icare/consultation/di/ConsultationsModule.kt** (new file)
    - **features/settings/src/main/kotlin/eg/edu/cu/csds/icare/settings/di/SettingsModule.kt** (new file)
    - **features/auth/src/main/kotlin/eg/edu/cu/csds/icare/auth/di/AuthModule.kt** (new file)
    - **app/src/main/kotlin/eg/edu/cu/csds/icare/di/AppModule.kt:**
        - Added includes for all new feature modules.
    - **features/appointments/src/main/kotlin/eg/edu/cu/csds/icare/appointment/di/AppointmentsModule.kt** (new file)
    - **features/admin/src/main/kotlin/eg/edu/cu/csds/icare/admin/di/AdminModule.kt** (new file)
    - **features/on_boarding/src/main/kotlin/eg/edu/cu/csds/icare/onboarding/di/OnBoardingModule.kt** (new file)
    - **build-logic/convention/src/main/kotlin/KoinConventionPlugin.kt:**
        - Removed KSP argument for `KOIN_DEFAULT_MODULE`.
    - **features/notifications/src/main/kotlin/eg/edu/cu/csds/icare/notification/di/NotificationsModule.kt** (new file)

---
+++ features/admin/src/main/kotlin/eg/edu/cu/csds/icare/admin/di/AdminModule.kt
@@ -0,0 +1,8 @@
+package eg.edu.cu.csds.icare.admin.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("eg.edu.cu.csds.icare.admin")
+class AdminModule
