b53667536d2a7688fa04096cd36b5e0476279312
Ali Mansour <dev.ali.mansour@gmail.com>
2026-01-16 14:30:00 +0200

    refactor: Rename HomeModule and update navigation handling to use Navigation3

--- feature/home/src/main/kotlin/eg/edu/cu/csds/icare/feature/home/di/module/HomeModule.kt
+++ feature/home/src/main/kotlin/eg/edu/cu/csds/icare/feature/home/di/module/HomeModule.kt
@@ -1,7 +1,7 @@
-package eg.edu.cu.csds.icare.feature.home.di
+package eg.edu.cu.csds.icare.feature.home.di.module
 
 import eg.edu.cu.csds.icare.core.data.di.module.UseCaseModule
-import eg.edu.cu.csds.icare.core.ui.di.CoreUiModule
+import eg.edu.cu.csds.icare.core.ui.di.module.CoreUiModule
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 
