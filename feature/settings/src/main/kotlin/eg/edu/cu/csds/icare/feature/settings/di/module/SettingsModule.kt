41d0d35ddedbb80773d9b2dcae53747a8c2c8553
Ali Mansour <dev.ali.mansour@gmail.com>
2026-01-17 10:30:00 +0200

    refactor: Rename SettingsModule and update navigation handling to use Navigation3

--- feature/settings/src/main/kotlin/eg/edu/cu/csds/icare/feature/settings/di/module/SettingsModule.kt
+++ feature/settings/src/main/kotlin/eg/edu/cu/csds/icare/feature/settings/di/module/SettingsModule.kt
@@ -1,7 +1,7 @@
-package eg.edu.cu.csds.icare.feature.settings.di
+package eg.edu.cu.csds.icare.feature.settings.di.module
 
 import eg.edu.cu.csds.icare.core.data.di.module.UseCaseModule
-import eg.edu.cu.csds.icare.core.ui.di.CoreUiModule
+import eg.edu.cu.csds.icare.core.ui.di.module.CoreUiModule
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 
--- feature/settings/src/main/kotlin/eg/edu/cu/csds/icare/feature/settings/navigation/SettingsNavigation.kt
+++ feature/settings/src/main/kotlin/eg/edu/cu/csds/icare/feature/settings/navigation/SettingsNavigation.kt
@@ -1,23 +1,22 @@
 package eg.edu.cu.csds.icare.feature.settings.navigation
 
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.composable
+import androidx.navigation3.runtime.EntryProviderScope
 import eg.edu.cu.csds.icare.core.ui.navigation.Route
 import eg.edu.cu.csds.icare.feature.settings.screen.about.AboutScreen
 import eg.edu.cu.csds.icare.feature.settings.screen.settings.SettingsScreen
 
-fun NavGraphBuilder.settingsRoute(
-    navigateUp: () -> Unit,
+fun EntryProviderScope<Any>.settingsEntryBuilder(
+    onNavigationIconClicked: () -> Unit,
     navigateToRoute: (Route) -> Unit,
 ) {
-    composable<Route.Settings> {
+    entry<Route.Settings> {
         SettingsScreen {
             navigateToRoute(it)
         }
     }
-    composable<Route.About> {
+    entry<Route.About> {
         AboutScreen {
-            navigateUp()
+            onNavigationIconClicked()
         }
     }
 }
